3a93187f7a4a269abb9a2f3ceb8feba5b1862304
Karl-Aris <areglado291995@gmail.com>
2025-05-07 10:18:56 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,13 @@
+{
+    'name': 'Custom Sales Order Search',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': 'Allows users to paste multiple Sales Order numbers and search them.',
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/custom_sales_order_search_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
 
