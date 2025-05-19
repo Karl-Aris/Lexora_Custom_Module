fa55917e21ea4d957ca368cde9fbd2aca69c0047
Karl-Aris <areglado291995@gmail.com>
2025-05-19 11:32:52 -0400

    Delete __manifest__.py

--- __manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'Authorize.Net Surcharge',
-    'version': '1.0',
-    'category': 'Sales',
-    'depends': ['sale', 'payment', 'sale_management'],
-    'data': [],
-    'installable': True,
-}
