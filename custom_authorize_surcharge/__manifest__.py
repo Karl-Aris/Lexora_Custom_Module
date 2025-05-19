5fe9d62178340a3c93beb199b052861de098222b
Karl-Aris <areglado291995@gmail.com>
2025-05-19 11:33:21 -0400

    Add files via upload

---
+++ custom_authorize_surcharge/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Authorize.Net Surcharge",
+    "version": "1.0",
+    "summary": "Applies a 3% surcharge when using Authorize.Net payment method",
+    "description": "Automatically adds a 3% surcharge to the order total if Authorize.Net is selected as payment method.",
+    "author": "Carl Aris Areglado",
+    "category": "Accounting",
+    "depends": ["sale", "account", "payment"],
+    "data": [],
+    "installable": True,
+    "application": False
+}
---
+++ custom_authorize_surcharge/models/payment_transaction.py
@@ -0,0 +1,1 @@
+# Your payment logic goes here
