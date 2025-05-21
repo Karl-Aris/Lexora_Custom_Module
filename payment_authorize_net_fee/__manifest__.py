9b923446585e1540305b7d2f6bffc9535bb7e2bf
Karl-Aris <areglado291995@gmail.com>
2025-05-21 10:53:45 -0400

    Add files via upload

---
+++ payment_authorize_net_fee/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Authorize.Net Processing Fee",
+    "version": "1.0",
+    "category": "Accounting",
+    "summary": "Add 3% processing fee for Authorize.Net payments",
+    "author": "ChatGPT",
+    "depends": ["payment_authorize_net"],
+    "installable": True,
+    "application": False,
+}
