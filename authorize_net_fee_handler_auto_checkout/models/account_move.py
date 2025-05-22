f280a9bd5861ad1fff9e2c5b5c6e1d5fb1b7110a
Karl-Aris <areglado291995@gmail.com>
2025-05-22 11:44:47 -0400

    Update account_move.py

--- authorize_net_fee_handler_auto_checkout/models/account_move.py
+++ authorize_net_fee_handler_auto_checkout/models/account_move.py
@@ -1,30 +1,26 @@
-from odoo import models, fields, _
+from odoo import models, fields, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def apply_authorize_net_fee(self, transaction):
-        self.ensure_one()
-        if self.move_type != 'out_invoice' or self.state != 'posted':
+    def _add_authorize_net_fee_line_if_needed(self, payment):
+        # Only add the fee if payment method is Authorize.Net
+        if payment.payment_method_id.code != 'authorize':
             return
 
-        if not transaction or transaction.provider_id.code != 'authorize':
-            return
-
-        if any(line.product_id.default_code == 'AUTH_NET_FEE' for line in self.invoice_line_ids):
-            return
-
-        fee_product = self.env['product.product'].search([('default_code', '=', 'AUTH_NET_FEE')], limit=1)
-        if not fee_product:
-            raise UserError(_("Product with Internal Reference 'AUTH_NET_FEE' not found."))
+        # Avoid duplicate fee lines
+        fee_product = self.env.ref('authorize_net_fee_handler_auto_checkout.product_authorize_net_fee')
+        for line in self.invoice_line_ids:
+            if line.product_id == fee_product:
+                return  # already added
 
         fee_amount = self.amount_total * 0.03
 
-        self.write({
-            'invoice_line_ids': [(0, 0, {
-                'product_id': fee_product.id,
-                'quantity': 1,
-                'price_unit': fee_amount,
-                'name': _('Authorize.Net 3% Surcharge'),
-            })]
+        self.invoice_line_ids.create({
+            'move_id': self.id,
+            'product_id': fee_product.id,
+            'name': 'Authorize.Net Surcharge (3%)',
+            'quantity': 1,
+            'price_unit': fee_amount,
+            'account_id': fee_product.property_account_income_id.id or fee_product.categ_id.property_account_income_categ_id.id,
         })
