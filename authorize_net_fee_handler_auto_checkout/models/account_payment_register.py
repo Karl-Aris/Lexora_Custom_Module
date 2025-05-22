70c3bc9c1bdb276b70a81638f0a4c4a921c25088
Karl-Aris <areglado291995@gmail.com>
2025-05-22 11:38:40 -0400

    Update account_payment_register.py

--- authorize_net_fee_handler_auto_checkout/models/account_payment_register.py
+++ authorize_net_fee_handler_auto_checkout/models/account_payment_register.py
@@ -4,11 +4,9 @@ class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
 
     def _create_payments(self):
-        payments = super()._create_payments()
+    payments = super()._create_payments()
+    for payment in payments:
+        for invoice in payment.invoice_ids:
+            invoice._add_authorize_net_fee_line_if_needed(payment)
+    return payments
 
-        for payment in payments:
-            if payment.payment_method_line_id and payment.payment_method_line_id.payment_provider_id.code == 'authorize':
-                for invoice in payment.reconciled_invoices:
-                    invoice.apply_authorize_net_fee(payment.transaction_id or payment)
-
-        return payments
