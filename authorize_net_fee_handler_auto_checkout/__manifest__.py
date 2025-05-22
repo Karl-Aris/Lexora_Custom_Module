a1c7cf8633fc916d701f0f8c14fc3c9a713c3b66
Karl-Aris <areglado291995@gmail.com>
2025-05-22 11:46:31 -0400

    Update __manifest__.py

--- authorize_net_fee_handler_auto_checkout/__manifest__.py
+++ authorize_net_fee_handler_auto_checkout/__manifest__.py
@@ -5,6 +5,9 @@
     'author': 'Carl Areglado',
     'category': 'Accounting',
     'description': 'Automatically adds a 3% surcharge on invoice if Authorize.Net is used for payment.',
+    'data': [
+    'data/product_data.xml',
+    ],
     'installable': True,
     'application': False,
     'auto_install': False,
