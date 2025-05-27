2ae3a2cbb0139d7faa00bcb34b55a164c0e12285
Karl-Aris <areglado291995@gmail.com>
2025-05-27 13:07:11 -0400

    Update __manifest__.py

--- website_sale_availability_filter/__manifest__.py
+++ website_sale_availability_filter/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Website Sale Availability Filter",
     "summary": "Add frontend filter for product availability on /shop",
     "version": "1.0",
-    "author": "ChatGPT",
+    "author": "Carl Areglado",
     "category": "Website",
     "depends": ["website_sale", "stock"],
     "data": [],
