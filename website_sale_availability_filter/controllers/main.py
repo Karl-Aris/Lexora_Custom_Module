6bfb0512c565f32c4672e363ff5a32d89f04d9c8
Karl-Aris <areglado291995@gmail.com>
2025-05-27 14:32:34 -0400

    Update main.py

--- website_sale_availability_filter/controllers/main.py
+++ website_sale_availability_filter/controllers/main.py
@@ -1,31 +1,14 @@
+# File: website_hide_out_of_stock/controllers/main.py
+
 from odoo import http
-from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
-class WebsiteSale(http.Controller):
+class WebsiteSaleStockFiltered(WebsiteSale):
 
-    @http.route(['/shop'], type='http', auth="public", website=True)
-    def shop(self, **kwargs):
-        domain = []
+    def _shop_get_product_domain(self, search, category, attrib_values):
+        domain = super()._shop_get_product_domain(search, category, attrib_values)
 
-        # Product Availability filtering logic
-        availability = kwargs.get('availability')
-        if availability:
-            location = request.env.ref('stock.stock_location_stock')  # Change if needed
-            product_ids_with_stock = request.env['stock.quant'].sudo().read_group(
-                [('location_id', '=', location.id)],
-                ['product_id', 'quantity:sum'],
-                ['product_id']
-            )
-            in_stock_product_ids = [res['product_id'][0] for res in product_ids_with_stock if res['quantity'] > 0]
+        # Only show templates that have at least one variant with virtual_available > 0
+        domain.append(('product_variant_ids.virtual_available', '>', 0))
 
-            if availability == 'available':
-                domain += [('id', 'in', in_stock_product_ids)]
-            elif availability == 'not_available':
-                domain += [('id', 'not in', in_stock_product_ids)]
-
-        # continue with your normal shop product search logic
-        products = request.env['product.template'].sudo().search(domain)
-
-        return request.render("website_sale.products", {
-            'products': products,
-        })
+        return domain
