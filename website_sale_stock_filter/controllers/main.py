949db101c840fa84ced5201f66a0d4b84fe265ff
Karl-Aris <areglado291995@gmail.com>
2025-05-27 13:49:45 -0400

    Update main.py

--- website_sale_stock_filter/controllers/main.py
+++ website_sale_stock_filter/controllers/main.py
@@ -3,6 +3,5 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSaleStockFilter(WebsiteSale):
     def _shop_get_product_domain(self, search, category, attrib_values):
         domain = super()._shop_get_product_domain(search, category, attrib_values)
-        # Uncomment the next line after confirming it's stable
-        # domain += [('product_variant_ids.qty_available', '>', 0)]
+        domain += [('product_variant_ids.qty_available', '>', 0)]
         return domain
