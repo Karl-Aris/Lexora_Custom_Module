3c89365252ded71273b87a5f3ac4bc04e2a24945
Karl-Aris <areglado291995@gmail.com>
2025-05-27 09:38:55 -0400

    Update main.py

--- website_sale_stock_filter/controllers/main.py
+++ website_sale_stock_filter/controllers/main.py
@@ -4,10 +4,10 @@ from odoo.http import request
 class WebsiteSaleStockFiltered(WebsiteSale):
 
     def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
-        # Get the original domain
         domain = super()._get_search_domain(search, category, attrib_values, search_in_description)
 
-        # Add condition: only show products where at least one variant has stock
-        domain += [('product_variant_ids.qty_available', '>', 0)]
+        # Only include templates where at least one variant has stock
+        in_stock_variant_ids = request.env['product.product'].search([('qty_available', '>', 0)]).ids
+        domain += [('product_variant_ids', 'in', in_stock_variant_ids)]
 
         return domain
