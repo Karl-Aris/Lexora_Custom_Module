368f4de69bd63ffeb4ef853f3fe38367e2377f0f
Karl-Aris <areglado291995@gmail.com>
2025-05-27 14:13:46 -0400

    Update main.py

--- website_sale_stock_filter/controllers/main.py
+++ website_sale_stock_filter/controllers/main.py
@@ -5,17 +5,6 @@ class WebsiteSaleStockFilter(WebsiteSale):
     def _get_search_domain(self, search, category, attrib_values):
         domain = super()._get_search_domain(search, category, attrib_values)
 
-        # Add logic to show only products with positive available quantity
-        in_stock_product_templates = http.request.env['product.template'].search([
-            ('type', '=', 'product'),
-            ('website_published', '=', True),
-        ])
-
-        # Filter templates with stock
-        in_stock_ids = []
-        for template in in_stock_product_templates:
-            if any(variant.qty_available > 0 for variant in template.product_variant_ids):
-                in_stock_ids.append(template.id)
-
-        domain += [('id', 'in', in_stock_ids)]
+        # Only show products with quantity on hand greater than 0
+        domain += [('qty_available', '>', 0)]
         return domain
