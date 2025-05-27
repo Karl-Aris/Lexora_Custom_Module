240939e8a91ae7c60cbc18448afec8e90153f17e
Karl-Aris <areglado291995@gmail.com>
2025-05-27 15:47:22 -0400

    Update main.py

--- website_product_stock_filter/controllers/main.py
+++ website_product_stock_filter/controllers/main.py
@@ -6,8 +6,8 @@ class WebsiteSaleStockFilter(WebsiteSale):
     def _shop_get_product_domain(self, search, category, attrib_values):
         domain = super()._shop_get_product_domain(search, category, attrib_values)
 
-        availability = http.request.params.get('availability', '')
-        filters = availability.split(',') if availability else []
+        availability = http.request.params.getlist('availability[]')
+        filters = availability if availability else []
 
         if filters and 'available' in filters and 'not_available' not in filters:
             domain.append(('product_variant_ids.virtual_available', '>', 0))
