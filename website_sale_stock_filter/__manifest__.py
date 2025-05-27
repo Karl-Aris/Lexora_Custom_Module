83ae97ce7d5c956150b3bbc6fde31780aeccd2c6
Karl-Aris <areglado291995@gmail.com>
2025-05-27 13:47:48 -0400

    Add files via upload

---
+++ website_sale_stock_filter/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Website Sale Stock Filter",
+    "version": "1.0",
+    "category": "Website",
+    "summary": "Filter products to show only in-stock items on shop page",
+    "author": "Your Name or Company",
+    "depends": ["website_sale"],
+    "data": [],
+    "installable": True,
+    "application": False
+}
