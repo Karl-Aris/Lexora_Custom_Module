1bb64e3316fdd815ee9dfe2bb2dcb10921baa16a
Karl-Aris <areglado291995@gmail.com>
2025-05-27 10:52:53 -0400

    Update __manifest__.py

--- website_sale_stock_filter/__manifest__.py
+++ website_sale_stock_filter/__manifest__.py
@@ -2,8 +2,11 @@
     "name": "Website Shop Stock Filter",
     "version": "1.0",
     "summary": "Only show in-stock products on website shop",
+    "category": "Website",
+    "author": "Carl Areglado",
     "depends": ["website_sale"],
     "data": [],
     "installable": True,
-    "application": False
+    "application": False,
+    "auto_install": False
 }
