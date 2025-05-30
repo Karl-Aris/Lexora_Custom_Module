5da0289d51f6ecc3bf8d5c244fbc02548b00442c
Karl-Aris <areglado291995@gmail.com>
2025-05-30 15:40:57 -0400

    Update main.py

--- website_sale_stock_filter/controllers/main.py
+++ website_sale_stock_filter/controllers/main.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
+import logging
 from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+_logger = logging.getLogger(__name__)
 
 class WebsiteSaleStockFilter(WebsiteSale):
 
     @http.route(['/shop'], type='http', auth="public", website=True, sitemap=True)
     def shop(self, page=0, category=None, search='', **post):
-        response = super().shop(page=page, category=category, search=search, **post)
-
         availability = post.get('availability')
+        _logger.info("Availability filter selected: %s", availability)
+
+        response = super().shop(page=page, category=category, search=search, **post)
         products = response.qcontext.get('products')
 
-        if products:
-            # Auto publish/unpublish based on stock
-            for product in products.sudo():  # gives full access
-                if product.qty_available > 0 and not product.website_published:
-                    product.sudo().write({'website_published': True})
-                elif product.qty_available <= 0 and product.website_published:
-                    product.sudo().write({'website_published': False})
+        if products and availability:
+            products = products.sudo()
+            _logger.info("Initial product count before filtering: %d", len(products))
 
+            if availability == 'available':
+                products = products.filtered(lambda p: p.qty_available > 0)
+                _logger.info("Filtered to %d products with qty > 0", len(products))
+            elif availability == 'not_available':
+                products = products.filtered(lambda p: p.qty_available <= 0)
+                _logger.info("Filtered to %d products with qty <= 0", len(products))
+
+            # Optional: Log product IDs that passed filter
+            _logger.debug("Filtered product IDs: %s", products.ids)
 
             response.qcontext['products'] = products
 
