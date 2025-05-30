b5515945848ce1893c4b0e0d2a5c3baa11706a50
Karl-Aris <areglado291995@gmail.com>
2025-05-30 15:27:30 -0400

    Update main.py

--- website_sale_stock_filter/controllers/main.py
+++ website_sale_stock_filter/controllers/main.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
@@ -13,9 +13,9 @@ class WebsiteSaleStockFilter(WebsiteSale):
         products = response.qcontext.get('products')
         if products and availability:
             if availability == 'available':
-                products = products.filtered(lambda p: p.qty_available > 0)
+                products = products.filtered(lambda p: p.inventory_quantity_auto_apply > 0)
             elif availability == 'not_available':
-                products = products.filtered(lambda p: p.qty_available <= 0)
+                products = products.filtered(lambda p: p.inventory_quantity_auto_apply <= 0)
             response.qcontext['products'] = products
 
         return response
