c384aabf258c7968926438db41d7421f87e35cbf
Karl-Aris <areglado291995@gmail.com>
2025-05-30 13:08:29 -0400

    Update main.py

--- website_sale_stock_filter/controllers/main.py
+++ website_sale_stock_filter/controllers/main.py
@@ -7,13 +7,13 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSaleStockFilter(WebsiteSale):
 
     def _shop_get_product_search_domain(self, search, category, attrib_values):
-        # Start with the default domain
-        domain = super()._shop_get_product_search_domain(search, category, attrib_values)
+    domain = super()._shop_get_product_search_domain(search, category, attrib_values)
+    availability = request.params.get('availability')
 
-        availability = request.params.get('availability')
         if availability == 'available':
-            domain += [('inventory_quantity_auto_apply', '>', 0)]
+            # Filter templates that have at least one variant with qty_available > 0
+            domain += [('product_variant_ids.qty_available', '>', 0)]
         elif availability == 'not_available':
-            domain += [('inventory_quantity_auto_apply', '<=', 0)]
-
+            domain += [('product_variant_ids.qty_available', '<=', 0)]
+    
         return domain
