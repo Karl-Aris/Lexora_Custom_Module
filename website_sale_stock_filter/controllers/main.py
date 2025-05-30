ffebee70980f9faeb06daf4ab58ad514da9a663c
Karl-Aris <areglado291995@gmail.com>
2025-05-30 15:46:22 -0400

    Update main.py

--- website_sale_stock_filter/controllers/main.py
+++ website_sale_stock_filter/controllers/main.py
@@ -11,24 +11,22 @@ class WebsiteSaleStockFilter(WebsiteSale):
     @http.route(['/shop'], type='http', auth="public", website=True, sitemap=True)
     def shop(self, page=0, category=None, search='', **post):
         availability = post.get('availability')
-        _logger.info("Availability filter selected: %s", availability)
+        _logger.info("[LOG] Availability selected: %s", availability)
 
         response = super().shop(page=page, category=category, search=search, **post)
         products = response.qcontext.get('products')
 
-        if products and availability:
+        if products:
             products = products.sudo()
-            _logger.info("Initial product count before filtering: %d", len(products))
+            _logger.info("[LOG] Initial products count: %d", len(products))
+            _logger.info("[LOG] Product IDs before filtering: %s", products.mapped('id'))
 
             if availability == 'available':
                 products = products.filtered(lambda p: p.qty_available > 0)
-                _logger.info("Filtered to %d products with qty > 0", len(products))
+                _logger.info("[LOG] Filtered (available) products: %s", products.mapped('id'))
             elif availability == 'not_available':
                 products = products.filtered(lambda p: p.qty_available <= 0)
-                _logger.info("Filtered to %d products with qty <= 0", len(products))
-
-            # Optional: Log product IDs that passed filter
-            _logger.debug("Filtered product IDs: %s", products.ids)
+                _logger.info("[LOG] Filtered (not available) products: %s", products.mapped('id'))
 
             response.qcontext['products'] = products
 
