f575e41b3545d129f074abb492bf69ad6e625182
Karl-Aris <areglado291995@gmail.com>
2025-06-02 13:28:24 -0400

    Create manifest.py

---
+++ website_sale_payment_method/manifest.py
@@ -0,0 +1,12 @@
+{
+    "name": "Website Sale Payment Method",
+    "version": "1.0",
+    "summary": "Adds payment method selection to website sale order confirmation",
+    "author": "Your Company",
+    "category": "Website",
+    "depends": ["website_sale"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
