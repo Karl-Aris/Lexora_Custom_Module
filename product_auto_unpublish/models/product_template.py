562993562d71cab8c516efadebc440cec741fd2e
Karl-Aris <areglado291995@gmail.com>
2025-06-05 15:55:20 -0400

    Update product_template.py

--- product_auto_unpublish/models/product_template.py
+++ product_auto_unpublish/models/product_template.py
@@ -7,17 +7,12 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     def check_and_toggle_published(self):
-        products = self.with_context(active_test=False).search([])
+    for product in self.with_context(active_test=False).search([]):
+        qty = product.qty_available
+        published = product.website_published
 
-        for product in products:
-            qty = sum(product.product_variant_ids.mapped('qty_available'))
-            published = product.website_published
+        if qty <= 0 and published:
+            product.website_published = False
+        elif qty > 0 and not published:
+            product.website_published = True 
 
-            _logger.info(f"[Stock Check] Product: {product.name} | Qty: {qty} | Published: {published}")
-
-            if qty <= 0 and published:
-                product.website_published = False
-                _logger.info(f"→ Unpublished: {product.name} (Out of stock)")
-            elif qty > 0 and not published:
-                product.website_published = True
-                _logger.info(f"→ Published: {product.name} (Back in stock)")
