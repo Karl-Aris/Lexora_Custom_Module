af5a50b555286cd25de36f20ac0c263d3d233bb9
Karl-Aris <areglado291995@gmail.com>
2025-06-05 14:43:38 -0400

    Add files via upload

---
+++ product_auto_unpublish/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Auto Unpublish Out-of-Stock Products",
+    "version": "1.0",
+    "depends": ["website_sale", "stock"],
+    "author": "ChatGPT",
+    "category": "Website",
+    "description": "Automatically unpublishes products from the website when they go out of stock.",
+    "installable": True,
+    "application": False,
+}
---
+++ product_auto_unpublish/models/product_template.py
@@ -0,0 +1,23 @@
+
+from odoo import models, fields, api
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def create(self, vals):
+        record = super().create(vals)
+        record._check_stock_and_publish()
+        return record
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._check_stock_and_publish()
+        return res
+
+    def _check_stock_and_publish(self):
+        for product in self:
+            if product.virtual_available <= 0 and product.website_published:
+                product.website_published = False
+            elif product.virtual_available > 0 and not product.website_published:
+                product.website_published = True
