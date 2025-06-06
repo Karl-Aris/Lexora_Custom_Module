a51a9f39e007ed835a350c6589f5c802750add3c
Karl-Aris <areglado291995@gmail.com>
2025-06-06 10:25:49 -0400

    Update product_template.py

--- product_auto_unpublish/models/product_template.py
+++ product_auto_unpublish/models/product_template.py
@@ -10,14 +10,13 @@ class ProductTemplate(models.Model):
     )
     
     def check_and_toggle_published(self):
-        for product in self.with_context(active_test=False).search([]):
-            if product.is_saleable:
-                # Force publish if marked as saleable
-                if not product.website_published:
-                    product.website_published = True
-            else:
-                # If not saleable, control via stock logic
-                if product.qty_available <= 0 and product.website_published:
-                    product.website_published = False
-                elif product.qty_available > 0 and not product.website_published:
-                    product.website_published = True
+    for product in self.with_context(active_test=False).search([]):
+        qty = product.qty_available
+        published = product.website_published
+
+        # Respect is_saleable flag
+        if not product.is_saleable and qty <= 0 and published:
+            product.website_published = False
+        elif (qty > 0 or product.is_saleable) and not published:
+            product.website_published = True
+
