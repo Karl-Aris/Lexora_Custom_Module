47f03231f4fbcb6a2c13fe3c095f1eccfec974ef
Karl-Aris <areglado291995@gmail.com>
2025-06-10 11:32:40 -0400

    Create sale_order.py

---
+++ Local_Partner_Update/models/sale_order.py
@@ -0,0 +1,25 @@
+from odoo import models, api
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.model
+    def create(self, vals):
+        order = super(SaleOrder, self).create(vals)
+
+        allowed_merchants = {
+            'Bell+Modern Shopify',
+            'Lore & Lane Amazon',
+            'Bell+Modern Amazon',
+            'Lexora Shopify',
+            'Dealers',
+            "Lowe's"
+        }
+
+        if order.partner_id:
+            if order.partner_id.name in allowed_merchants:
+                order.partner_id.x_studio_local = True
+            else:
+                order.partner_id.x_studio_local = False
+
+        return order
