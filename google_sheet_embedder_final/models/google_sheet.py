584e3bc7ddad454e495602c2dfadea0fb476d482
Karl-Aris <areglado291995@gmail.com>
2025-06-16 13:07:46 -0400

    Delete google_sheet_embedder_final directory

--- google_sheet_embedder_final/models/google_sheet.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class GoogleSheetEmbed(models.Model):
-    _name = 'google.sheet.embed'
-    _description = 'Google Sheet Embed'
-
-    name = fields.Char(required=True)
-    iframe_html = fields.Html(string="Iframe HTML", sanitize=False)
