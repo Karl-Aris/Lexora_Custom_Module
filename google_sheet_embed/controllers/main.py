c2598efba19e610f7eb9f12664077b4e2613cb8f
Karl-Aris <areglado291995@gmail.com>
2025-06-16 15:49:36 -0400

    Update main.py

--- google_sheet_embed/controllers/main.py
+++ google_sheet_embed/controllers/main.py
@@ -1,21 +1,8 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
-class XReport(models.Model):
-    _name = 'x_report'
-    _description = 'Custom Report'
+class GoogleSheetEmbed(models.Model):
+    _name = 'google.sheet.embed'
+    _description = 'Google Sheet Embed'
 
-    x_name = fields.Char()
-    x_google_url = fields.Char(string="Google Sheet URL")
-    x_iframe = fields.Html(string="Embedded Iframe", compute='_compute_iframe', sanitize=False)
-
-    @api.depends('x_google_url')
-    def _compute_iframe(self):
-        for rec in self:
-            if rec.x_google_url:
-                rec.x_iframe = f"""
-                    <iframe src="{rec.x_google_url}"
-                            width="100%" height="600" frameborder="0" allowfullscreen>
-                    </iframe>
-                """
-            else:
-                rec.x_iframe = False
+    name = fields.Char(string="Title")
+    iframe_html = fields.Html(string="Iframe HTML", sanitize=False)
