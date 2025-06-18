6037291e7223760712bd3ab077406d8a2bb967ec
Karl-Aris <areglado291995@gmail.com>
2025-06-18 09:42:38 -0400

    Update __manifest__.py

--- google_sheet_dashboard/__manifest__.py
+++ google_sheet_dashboard/__manifest__.py
@@ -3,11 +3,6 @@
     "version": "1.0",
     "depends": ["web"],
     "data": ["views/dashboard_view.xml"],
-    "assets": {
-        "web.assets_backend": [
-            "google_sheet_dashboard/static/src/js/google_sheet_dashboard.js"
-        ]
-    },
     "category": "Tools",
-    "application": True
+    "application": True,
 }
