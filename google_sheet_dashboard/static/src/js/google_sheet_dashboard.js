e7f70557651bfeb942fdc80c8757c0b24d9c3a9e
Karl-Aris <areglado291995@gmail.com>
2025-06-18 10:30:51 -0400

    Update google_sheet_dashboard.js

--- google_sheet_dashboard/static/src/js/google_sheet_dashboard.js
+++ google_sheet_dashboard/static/src/js/google_sheet_dashboard.js
@@ -3,17 +3,8 @@
 import { registry } from "@web/core/registry";
 import AbstractAction from "@web/webclient/actions/abstract_action";
 
-export class GoogleSheetDashboard extends AbstractAction {
-    setup() {
-        // Any additional logic if needed
-    }
-
-    async willStart() {
-        await super.willStart();
-    }
-}
+export class GoogleSheetDashboard extends AbstractAction {}
 
 GoogleSheetDashboard.template = "google_sheet_dashboard.google_sheet_iframe";
 
-// Register the action
 registry.category("actions").add("google_sheet_dashboard.action", GoogleSheetDashboard);
