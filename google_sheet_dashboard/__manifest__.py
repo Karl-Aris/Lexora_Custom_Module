4e87863819151577148f758dca7e089d197c0550
Karl-Aris <areglado291995@gmail.com>
2025-06-18 10:16:37 -0400

    Delete google_sheet_dashboard directory

--- google_sheet_dashboard/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    "name": "Google Sheet Dashboard",
-    "version": "1.0",
-    "depends": ["web"],
-    "data": ["views/dashboard_view.xml"],
-    "category": "Tools",
-    "application": True,
-}
