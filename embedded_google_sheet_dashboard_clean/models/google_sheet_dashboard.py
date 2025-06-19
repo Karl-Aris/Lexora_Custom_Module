7ab038ae818cb216d36c005d1add3876ec6f503d
Karl-Aris <areglado291995@gmail.com>
2025-06-19 13:04:18 -0400

    Update google_sheet_dashboard.py

--- embedded_google_sheet_dashboard_clean/models/google_sheet_dashboard.py
+++ embedded_google_sheet_dashboard_clean/models/google_sheet_dashboard.py
@@ -1,16 +1,20 @@
 from odoo import models, fields, api
 
-class Report(models.Model):
-    _inherit = 'x_report'  # Inherit your studio model
+class ReportDashboard(models.Model):
+    _name = 'report.dashboard'
+    _description = 'Google Sheet Dashboard'
 
-    x_google_sheet_url = fields.Char(string="Google Sheet URL")
-    x_studio_embedded_google_sheet = fields.Html(string="Embedded Google Sheet", compute="_compute_embed_sheet", store=True)
+    name = fields.Char(string="Dashboard Name", required=True)
+    google_sheet_url = fields.Char(string="Google Sheet URL")
+    embedded_google_sheet = fields.Html(string="Embedded Google Sheet", compute="_compute_embed", store=True)
 
-    @api.depends('x_google_sheet_url')
-    def _compute_embed_sheet(self):
-        for record in self:
-            if record.x_google_sheet_url:
-                sheet_url = record.x_google_sheet_url.replace('/edit', '/pubhtml?widget=true&headers=false')
-                record.x_studio_embedded_google_sheet = f'<iframe src="{sheet_url}" width="100%" height="800" frameborder="0"></iframe>'
+    @api.depends('google_sheet_url')
+    def _compute_embed(self):
+        for rec in self:
+            if rec.google_sheet_url:
+                url = rec.google_sheet_url.replace('/edit', '/pubhtml?widget=true&headers=false')
+                rec.embedded_google_sheet = (
+                    f'<iframe src="{url}" width="100%" height="800" frameborder="0"></iframe>'
+                )
             else:
-                record.x_studio_embedded_google_sheet = ''
+                rec.embedded_google_sheet = ''
