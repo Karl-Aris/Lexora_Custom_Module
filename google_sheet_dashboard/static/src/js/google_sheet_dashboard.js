c058621342b54cdfed01e551c908a443b7a53b08
Karl-Aris <areglado291995@gmail.com>
2025-06-19 12:14:37 -0400

    Update google_sheet_dashboard.js

--- google_sheet_dashboard/static/src/js/google_sheet_dashboard.js
+++ google_sheet_dashboard/static/src/js/google_sheet_dashboard.js
@@ -1,20 +1,20 @@
-odoo.define('google_sheet_dashboard.google_sheet_dashboard', [
-    'web.AbstractAction',
-    'web.core'
-], function (require) {
-    'use strict';
+#odoo.define('google_sheet_dashboard.google_sheet_dashboard', [
+#    'web.AbstractAction',
+#    'web.core'
+#], function (require) {
+#    'use strict';
 
-    const AbstractAction = require('web.AbstractAction');
-    const core = require('web.core');
+#    const AbstractAction = require('web.AbstractAction');
+#    const core = require('web.core');
 
-    const GoogleSheetDashboard = AbstractAction.extend({
-        template: 'google_sheet_iframe',
+#    const GoogleSheetDashboard = AbstractAction.extend({
+#        template: 'google_sheet_iframe',
 
-        start() {
-            return this._super(...arguments);
-        },
-    });
+#        start() {
+#            return this._super(...arguments);
+#        },
+#    });
 
-    core.action_registry.add('google_sheet_dashboard', GoogleSheetDashboard);
-    return GoogleSheetDashboard;
-});
+ #   core.action_registry.add('google_sheet_dashboard', GoogleSheetDashboard);
+ #   return GoogleSheetDashboard;
+#});
