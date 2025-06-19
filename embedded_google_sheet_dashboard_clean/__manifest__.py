e9ad90b50457dd37ec0ff6251af2413a1a13ebf8
Karl-Aris <areglado291995@gmail.com>
2025-06-19 12:39:35 -0400

    Add files via upload

---
+++ embedded_google_sheet_dashboard_clean/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Embedded Google Sheet Dashboard Clean",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        "views/google_sheet_dashboard_view.xml"
+    ],
+    "installable": true,
+    "application": true,
+    "category": "Tools",
+    "summary": "Embed live Google Sheet Dashboards into Odoo backend",
+    "license": "LGPL-3"
+}
