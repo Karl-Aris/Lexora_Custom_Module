0a4466fa3f9acff19471d1755bac1898d4819761
Karl-Aris <areglado291995@gmail.com>
2025-06-19 12:09:32 -0400

    Update google_sheet_dashboard.js

--- google_sheet_dashboard/static/src/js/google_sheet_dashboard.js
+++ google_sheet_dashboard/static/src/js/google_sheet_dashboard.js
@@ -1,11 +1,14 @@
-odoo.define('google_sheet_dashboard.google_sheet_dashboard', function (require) {
+odoo.define('google_sheet_dashboard.google_sheet_dashboard', [
+    'web.AbstractAction',
+    'web.core'
+], function (require) {
     'use strict';
 
     const AbstractAction = require('web.AbstractAction');
     const core = require('web.core');
 
     const GoogleSheetDashboard = AbstractAction.extend({
-        template: 'google_sheet_iframe', // Must match t-name in your XML
+        template: 'google_sheet_iframe',
 
         start() {
             return this._super(...arguments);
