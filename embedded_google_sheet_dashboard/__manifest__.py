ddbd87b668bd31040da85ea0bbae09863c64064a
Karl-Aris <areglado291995@gmail.com>
2025-06-19 12:22:40 -0400

    Add files via upload

---
+++ embedded_google_sheet_dashboard/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Embedded Google Sheet Dashboard",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        "views/google_sheet_dashboard_view.xml"
+    ],
+    "installable": true,
+    "application": true,
+    "category": "Tools",
+    "summary": "Embed live Google Sheet Dashboards into Odoo backend"
+}
