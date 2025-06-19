d99e3af3e666621080ca977290bb2a3810954ece
Karl-Aris <areglado291995@gmail.com>
2025-06-19 14:56:58 -0400

    Delete embedded_google_sheet_dashboard_clean directory

--- embedded_google_sheet_dashboard_clean/controllers/main.py
+++
@@ -1,8 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-class GoogleSheetController(http.Controller):
-    @http.route(['/my_google_sheet/<int:dashboard_id>'], auth='user', type='http', website=True)
-    def show_sheet(self, dashboard_id):
-        dashboard = request.env['report.dashboard'].browse(dashboard_id)
-        return request.render('embedded_google_sheet_dashboard_clean.google_sheet_template', {'dashboard': dashboard})
