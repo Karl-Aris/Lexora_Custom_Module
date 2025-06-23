19e2fa9d1c706da5d327c9f28e731017137adae3
Karl-Aris <areglado291995@gmail.com>
2025-06-23 14:03:39 -0400

    Add files via upload

---
+++ inventory_adjustment_restrictor/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Inventory Adjustment Restrictor",
+    "version": "16.0.1.0.0",
+    "category": "Inventory",
+    "summary": "Restricts inventory adjustments and internal transfers to authorized users.",
+    "author": "ChatGPT",
+    "depends": ["stock"],
+    "data": [
+        "security/groups.xml"
+    ],
+    "installable": True,
+    "application": False
+}
