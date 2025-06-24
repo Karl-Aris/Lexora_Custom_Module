7259a6bc94717d07d5f7b8016041d3cf5197b94d
Karl-Aris <areglado291995@gmail.com>
2025-06-24 15:09:53 -0400

    Add files via upload

---
+++ hide_inventory_adjustment_button/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Hide Inventory Adjustment in Barcode",
+    "version": "1.0",
+    "depends": ["stock_barcode"],
+    "author": "ChatGPT Custom",
+    "category": "Warehouse",
+    "description": "Hides the Inventory Adjustment button from the Barcode dashboard.",
+    "data": [
+        "views/barcode_dashboard.xml"
+    ],
+    "installable": True,
+    "application": False
+}
