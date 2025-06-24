16bbe23af643d3b0eebaed8a0be199ca63eb6b6c
Karl-Aris <areglado291995@gmail.com>
2025-06-24 15:42:23 -0400

    Add files via upload

---
+++ hide_inventory_adjustments_button/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Hide Inventory Adjustment Button",
+    "version": "1.0",
+    "category": "Stock",
+    "summary": "Hides the Inventory Adjustment button for unauthorized users",
+    "depends": ["stock", "stock_barcode", "web"],
+    "data": [
+        "views/assets.xml"
+    ],
+    "assets": {},
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
