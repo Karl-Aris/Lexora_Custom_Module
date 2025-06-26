51ad5eef9dca423d5ae7f647c35925498c10e1f6
Karl-Aris <areglado291995@gmail.com>
2025-06-26 09:43:37 -0400

    Add files via upload

---
+++ custom_barcode_controls/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Custom Barcode Controls",
+    "version": "1.0",
+    "category": "Inventory",
+    "summary": "Hides Inventory Adjustment in Barcode App",
+    "depends": ["stock_barcode"],
+    "data": [
+        "views/hide_inventory_adjustment_in_barcode.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
