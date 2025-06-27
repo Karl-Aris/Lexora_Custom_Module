620fc7edfd88f169c95f8dcfae336d5c62175cb0
Karl-Aris <areglado291995@gmail.com>
2025-06-27 09:28:50 -0400

    Add files via upload

---
+++ vendor_discrepancy/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Vendor Bill Discrepancy",
+    "version": "1.0",
+    "category": "Accounting",
+    "summary": "Adds discrepancy field between charged amount and vendor bill total",
+    "author": "Code Copilot",
+    "depends": ["account"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
