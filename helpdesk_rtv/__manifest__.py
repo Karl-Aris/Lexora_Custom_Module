2c7b054f3c971181949cac79bd27f37ea39d6fff
Karl-Aris <areglado291995@gmail.com>
2025-07-01 15:50:28 -0400

    Add files via upload

---
+++ helpdesk_rtv/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Helpdesk RTV",
+    "version": "1.0",
+    "depends": ["helpdesk", "stock", "purchase"],
+    "author": "Code Copilot",
+    "category": "Custom",
+    "description": "Add Return to Vendor (RTV) logic to Helpdesk Tickets",
+    "data": [
+        "views/helpdesk_ticket_view.xml"
+    ],
+    "installable": True,
+    "application": False
+}
