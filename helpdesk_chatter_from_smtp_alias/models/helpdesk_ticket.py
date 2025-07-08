06140e57ac6bc0e8c0a784254ba7dc3044969fb9
Karl-Aris <areglado291995@gmail.com>
2025-07-08 14:09:22 -0400

    Update helpdesk_ticket.py

--- helpdesk_chatter_from_smtp_alias/models/helpdesk_ticket.py
+++ helpdesk_chatter_from_smtp_alias/models/helpdesk_ticket.py
@@ -7,9 +7,10 @@ class HelpdeskTicket(models.Model):
     def message_post(self, **kwargs):
         alias = self.team_id.alias_id
         if alias:
-            # Try to extract email from alias name or display_name
-            match = re.search(r'<([^>]+)>', alias.name or alias.display_name or '')
-            alias_email = match.group(1) if match else alias.name
+            # Try to extract from alias_display_name
+            raw = alias.alias_display_name or alias.alias_name
+            match = re.search(r'<([^>]+)>', raw)
+            alias_email = match.group(1) if match else raw
 
             if alias_email:
                 kwargs['email_from'] = alias_email
