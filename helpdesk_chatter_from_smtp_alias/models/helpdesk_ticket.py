89879c027054e1a4451e110fb29350d6cd6b0b70
Karl-Aris <areglado291995@gmail.com>
2025-07-08 14:08:27 -0400

    Update helpdesk_ticket.py

--- helpdesk_chatter_from_smtp_alias/models/helpdesk_ticket.py
+++ helpdesk_chatter_from_smtp_alias/models/helpdesk_ticket.py
@@ -1,11 +1,16 @@
 from odoo import models
+import re
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     def message_post(self, **kwargs):
         alias = self.team_id.alias_id
-        domain = self.env['ir.config_parameter'].sudo().get_param('mail.catchall.domain')
-        if alias and domain:
-            kwargs['email_from'] = f"{alias.alias_name}@{domain}"
+        if alias:
+            # Try to extract email from alias name or display_name
+            match = re.search(r'<([^>]+)>', alias.name or alias.display_name or '')
+            alias_email = match.group(1) if match else alias.name
+
+            if alias_email:
+                kwargs['email_from'] = alias_email
         return super().message_post(**kwargs)
