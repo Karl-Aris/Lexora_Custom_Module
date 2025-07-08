40bd0e9c86d443d276437afecf41a51a8b0ce051
Karl-Aris <areglado291995@gmail.com>
2025-07-08 11:21:21 -0400

    Delete helpdesk_mail_router directory

--- helpdesk_mail_router/models/helpdesk_team.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class HelpdeskTeam(models.Model):
-    _inherit = 'helpdesk.team'
-
-    mail_server_id = fields.Many2one('ir.mail_server', string="Outgoing Mail Server")
