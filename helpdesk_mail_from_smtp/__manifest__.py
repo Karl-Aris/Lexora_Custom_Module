e82e8c79673ca352a7727e4235065d85606bc24c
Karl-Aris <areglado291995@gmail.com>
2025-07-08 13:19:58 -0400

    Update __manifest__.py

--- helpdesk_mail_from_smtp/__manifest__.py
+++ helpdesk_mail_from_smtp/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Helpdesk Mail From = SMTP User",
     "version": "1.0",
     "depends": ["mail", "helpdesk"],
-    "author": "Code Copilot",
+    "author": "Karl Areglado",
     "category": "Helpdesk",
     "summary": "Force mail.email_from = mail_server_id.smtp_user for Helpdesk replies.",
     "installable": True,
