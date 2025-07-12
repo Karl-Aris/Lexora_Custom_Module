0d5eba5d773ceae074d259d4092eb10ef732fbfc
Karl-Aris <areglado291995@gmail.com>
2025-07-12 03:43:56 -0400

    Update __manifest__.py

--- sale_order_reply_to_fix/__manifest__.py
+++ sale_order_reply_to_fix/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Sale Order Reply-To Fix",
     "version": "1.0",
     "depends": ["sale", "crm"],
-    "author": "Code Copilot",
+    "author": "Karl Areglado",
     "category": "Sales",
     "description": "Use Sales Team Reply-To Email in outgoing order emails.",
     "data": [],
