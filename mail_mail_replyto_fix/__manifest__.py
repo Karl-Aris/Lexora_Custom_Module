14ce6651d63130a4ed04c0275c2a0d4a023cbd86
Karl-Aris <areglado291995@gmail.com>
2025-07-12 05:25:12 -0400

    Add files via upload

---
+++ mail_mail_replyto_fix/__manifest__.py
@@ -0,0 +1,9 @@
+{'name': 'Mail Mail Reply-To Fix',
+'version': '1.0',
+'category': 'Mail',
+'summary': 'Sets mail.mail.reply_to to email_from if not provided',
+'depends': ['mail'],
+'data': [],
+'installable': True,
+'auto_install': False,
+'application': False}
