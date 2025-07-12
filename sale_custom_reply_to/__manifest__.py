03a7781a245664b3f931b030a25451ea5b4ca516
Karl-Aris <areglado291995@gmail.com>
2025-07-12 06:26:26 -0400

    Add files via upload

---
+++ sale_custom_reply_to/__manifest__.py
@@ -0,0 +1,11 @@
+{ 
+    'name': 'Sale Custom Reply-To',
+    'version': '1.0',
+    'summary': 'Overrides reply_to for sale order quotation emails',
+    'author': 'ChatGPT',
+    'depends': ['sale_management', 'mail'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
