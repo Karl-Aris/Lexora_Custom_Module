2cb7ed20c22b36c187129a7ca6097f6eedc074f2
Karl-Aris <areglado291995@gmail.com>
2025-07-12 05:25:46 -0400

    Delete mail_mail_replyto_fix directory

--- mail_mail_replyto_fix/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{'name': 'Mail Mail Reply-To Fix',
-'version': '1.0',
-'category': 'Mail',
-'summary': 'Sets mail.mail.reply_to to email_from if not provided',
-'depends': ['mail'],
-'data': [],
-'installable': True,
-'auto_install': False,
-'application': False}
