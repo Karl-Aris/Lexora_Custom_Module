2db6f7d1c5c94d3fd4e8d051b4d2be2c65d9b5f8
Karl-Aris <areglado291995@gmail.com>
2025-07-12 04:40:19 -0400

    Update sale_order.py

--- sale_custom_replyto/models/sale_order.py
+++ sale_custom_replyto/models/sale_order.py
@@ -1,10 +1,14 @@
-
 from odoo import models
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+class MailTemplate(models.Model):
+    _inherit = "mail.template"
 
-    def _notify_prepare_email_values(self, message):
-        values = super()._notify_prepare_email_values(message)
-        values['reply_to'] = 'orders@lexorahome.com'
-        return values
+    def send_mail(self, res_id, force_send=False, raise_exception=False, email_values=None, *args, **kwargs):
+        if email_values is None:
+            email_values = {}
+
+        # Check if the template is used for sale.order and override reply_to
+        if self.model == 'sale.order':
+            email_values['reply_to'] = 'orders@lexorahome.com'
+
+        return super().send_mail(res_id, force_send, raise_exception, email_values=email_values, *args, **kwargs)
