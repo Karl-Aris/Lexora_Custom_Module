5476700cc82bcd7cfc5d7e00577c410c1682046c
Karl-Aris <areglado291995@gmail.com>
2025-07-12 04:33:39 -0400

    Delete sale_order_reply_to_fix directory

--- sale_order_reply_to_fix/models/crm_team.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class CrmTeam(models.Model):
-    _inherit = 'crm.team'
-
-    reply_to = fields.Char(string="Reply-To Email")
-    email = fields.Char(string="Sales Team Email")  # fallback, optional
