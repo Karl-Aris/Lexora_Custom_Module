84a11ebc6c88a03d261f492383eaefcdb1d70bae
Karl-Aris <areglado291995@gmail.com>
2025-07-14 15:05:03 -0400

    Update main.py

--- website_sale_lexora_shipping/controllers/main.py
+++ website_sale_lexora_shipping/controllers/main.py
@@ -1,44 +1,26 @@
-# controllers/main.py
 from odoo import http
 from odoo.http import request
 from werkzeug.utils import redirect
+from werkzeug.exceptions import Forbidden
 
-class LexoraShipping(http.Controller):
 
-    @http.route(['/shop/shipping-details/confirm'], type='http', auth='public', website=True, csrf=True)
-    def shipping_details_confirm(self, **post):
-        purchase_order = post.get('purchase_order')
-        order_customer = post.get('order_customer')
-        order_phone = post.get('order_phone')
-        order_address = post.get('order_address')
+class WebsiteSaleLexora(http.Controller):
 
-        # Get current order
-        sale_order = request.website.sale_get_order()
+    @http.route(['/shop/shipping/validate'], type='http', methods=['POST'], auth="user", website=True, sitemap=False)
+    def order_finalize_validate(self, **kw):
+        order = request.website.sale_get_order()
 
-        # Check if PO number already exists in another sale.order
-        existing = request.env['sale.order'].sudo().search([
-            ('purchase_order', '=', purchase_order),
-            ('id', '!=', sale_order.id)
-        ], limit=1)
+        if order and kw:
+            order.sudo().write({
+                'purchase_order': kw.get("purchase_order"),       # <-- this is the key field
+                'partner_name': kw.get("order_customer"),
+                'partner_phone': kw.get("order_phone"),
+                'partner_street': kw.get("order_address"),
+                'x_payment_method': kw.get("x_payment_method"),   # if this is a valid custom field
+            })
 
-        if existing:
-            error = {'purchase_order': 'PO number already used. Please enter a unique value.'}
-            values = {
-                'error': error,
-                'purchase_order': purchase_order,
-                'order_customer': order_customer,
-                'order_phone': order_phone,
-                'order_address': order_address,
-            }
-            return request.render('website_sale_lexora_shipping.shipping-details', values)
+            # Optional: reset session or move to thank you page
+            request.website.sale_reset()
+            return request.render("website_sale_lexora.order_complete_thank_you")
 
-        # Save the shipping info to the current sale order
-        sale_order.sudo().write({
-            'purchase_order': purchase_order,
-            'partner_name': order_customer,
-            'partner_phone': order_phone,
-            'partner_street': order_address,
-        })
-
-        # Redirect to payment or next step
-        return redirect('/shop/payment')
+        return Forbidden()
