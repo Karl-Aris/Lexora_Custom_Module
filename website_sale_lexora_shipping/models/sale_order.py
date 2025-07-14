3ce704276748f3421bddde38e12ce5ed75fe6174
Karl-Aris <areglado291995@gmail.com>
2025-07-14 14:46:40 -0400

    Update sale_order.py

--- website_sale_lexora_shipping/models/sale_order.py
+++ website_sale_lexora_shipping/models/sale_order.py
@@ -1,7 +1,10 @@
-# in models/sale_order.py
 from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     purchase_order = fields.Char(string="PO #")
+
+    _sql_constraints = [
+        ('unique_purchase_order', 'unique(purchase_order)', 'PO # must be unique.')
+    ]
