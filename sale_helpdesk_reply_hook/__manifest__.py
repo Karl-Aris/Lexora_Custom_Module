35b5a78502d6ca5b1d24390b27d854e9c34b65d3
Karl-Aris <areglado291995@gmail.com>
2025-07-14 13:22:06 -0400

    Delete sale_helpdesk_reply_hook directory

--- sale_helpdesk_reply_hook/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Sale Order Helpdesk Reply Hook',
-    'version': '1.0',
-    'category': 'Sales',
-    'depends': ['sale', 'helpdesk'],
-    'data': [],
-    'installable': True,
-    'auto_install': False,
-}
--- sale_helpdesk_reply_hook/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    def message_post(self, **kwargs):
-        if kwargs.get('message_type') == 'email':
-            kwargs['reply_to'] = 'dump.lexora@lexorahome.com'
-        return super().message_post(**kwargs)
