3a2b09d0e04fafebcb164b75ee9fbfde1842952d
Karl-Aris <areglado291995@gmail.com>
2025-07-14 14:39:52 -0400

    Create sale_order.py

---
+++ website_sale_lexora_shipping/models/sale_order.py
@@ -0,0 +1,7 @@
+# in models/sale_order.py
+from odoo import models, fields
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    purchase_order = fields.Char(string="PO #", required=True)
