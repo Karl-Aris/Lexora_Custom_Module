34b00db72086a423f56eb0718a07cbc85f40a5f0
Karl-Aris <areglado291995@gmail.com>
2025-07-14 15:12:52 -0400

    Update sale_order.py

--- website_sale_lexora_shipping/models/sale_order.py
+++ website_sale_lexora_shipping/models/sale_order.py
@@ -1,9 +1,7 @@
+# sale.order extension model
 from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    purchase_order = fields.Char(string="PO #", copy=False)
-    order_customer = fields.Char(string="Order Customer")
-    order_address = fields.Char(string="Order Address")
-    order_phone = fields.Char(string="Order Phone")
+    purchase_order = fields.Char(string="PO #", copy=False)  # Do NOT add required=True here
