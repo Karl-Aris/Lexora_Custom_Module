61bad7785f830976bb344076e993cbb1987d62b7
Karl-Aris <areglado291995@gmail.com>
2025-07-14 15:59:48 -0400

    Delete website_sale_lexora_shipping directory

--- website_sale_lexora_shipping/models/sale_order.py
+++
@@ -1,7 +0,0 @@
-# sale.order extension model
-from odoo import models, fields
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    purchase_order = fields.Char(string="PO #", copy=False)  # Do NOT add required=True here
