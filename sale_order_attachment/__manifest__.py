01d7ffa1b112278d617b479c461f099591c161ce
Karl-Aris <areglado291995@gmail.com>
2025-07-15 12:38:56 -0400

    Create __manifest__.py

---
+++ sale_order_attachment/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sale Order Attachment Download with Selection",
+    "version": "1.1",
+    "depends": ["sale"],
+    "author": "Karl Areglado",
+    "category": "Sales",
+    "description": "Add attachment selection list and download on sale order form.",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order_view.xml"
+    ],
+    "installable": True,
+    "application": False
+}
