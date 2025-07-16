27ac6504d2c767136e818549153e2032518c8811
Karl-Aris <areglado291995@gmail.com>
2025-07-16 12:38:50 -0400

    Update custom_attachment_download.js

--- multi_attachment_print/static/src/js/custom_attachment_download.js
+++ multi_attachment_print/static/src/js/custom_attachment_download.js
@@ -5,14 +5,14 @@ import { ClientAction } from "@web/core/client_action/client_action";
 
 class MultiAttachmentDownload extends ClientAction {
     setup() {
-        const attachment_ids = this.props.action.context.attachment_ids || [];
+        const ids = this.props.action.context.attachment_ids || [];
 
-        for (const id of attachment_ids) {
+        for (const id of ids) {
             const url = `/web/content/${id}?download=true`;
             window.open(url, '_blank');
         }
 
-        // Close the action after a short delay
+        // Close action after a short delay
         setTimeout(() => {
             this.props.closeAction();
         }, 500);
