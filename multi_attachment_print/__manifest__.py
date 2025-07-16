21aa0a30457aa9f2a2cbbf38ec3ee6badb14e0a7
Karl-Aris <areglado291995@gmail.com>
2025-07-16 12:23:19 -0400

    Update __manifest__.py

--- multi_attachment_print/__manifest__.py
+++ multi_attachment_print/__manifest__.py
@@ -1,14 +1,17 @@
 {
-    "name": "Multi Attachment Print",
-    "version": "1.0",
-    "summary": "Print multiple ir.attachment PDFs",
-    "category": "Tools",
-    "depends": ["base"],
-    "assets": {
-        "web.assets_backend": [
-            "multi_attachment_print/static/src/js/custom_attachment_download.js"
-        ]
+    'name': 'Multi Attachment Print',
+    'version': '1.0',
+    'summary': 'Print multiple ir.attachment PDFs',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        # Add your XML/CSV data files here if needed
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'multi_attachment_print/static/src/js/custom_attachment_download.js',
+        ],
     },
-    "installable": True,
-    "application": False,
+    'installable': True,
+    'application': False,
 }
