68199e02a008f50c1f41ce133eda7f6c6768bd1a
Karl-Aris <areglado291995@gmail.com>
2025-07-16 12:21:42 -0400

    Add files via upload

---
+++ multi_attachment_print/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Multi Attachment Print",
+    "version": "1.0",
+    "summary": "Print multiple ir.attachment PDFs",
+    "category": "Tools",
+    "depends": ["base"],
+    "assets": {
+        "web.assets_backend": [
+            "multi_attachment_print/static/src/js/custom_attachment_download.js"
+        ]
+    },
+    "installable": True,
+    "application": False,
+}
