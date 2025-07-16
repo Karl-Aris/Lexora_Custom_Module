1da5f43813c83c02bf05759c081dca3e955e4dca
Karl-Aris <areglado291995@gmail.com>
2025-07-16 13:36:14 -0400

    Update __manifest__.py

--- multi_attachment_print/__manifest__.py
+++ multi_attachment_print/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Tools',
     'license': 'LGPL-3',
     'depends': ['base', 'web'],
-    'data': ['assets.xml'],
+    'data': ["views/assets.xml"],
     'assets': {
         'web.assets_backend': [
             'multi_attachment_print/static/src/js/multi_attachment_download.js',
