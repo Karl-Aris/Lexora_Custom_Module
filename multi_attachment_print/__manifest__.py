de149b3a83adc102d98373e3411523595683e0ce
Karl-Aris <areglado291995@gmail.com>
2025-07-16 12:19:09 -0400

    Delete multi_attachment_print directory

--- multi_attachment_print/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Multi Attachment Print",
-    "version": "1.0",
-    "summary": "Print multiple ir.attachment PDFs",
-    "category": "Tools",
-    "depends": ["base"],
-    "installable": True,
-    "application": False,
-}
