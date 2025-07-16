806cd8e9954442cb188435ea9ce73f4bcb2a9a0c
Karl-Aris <areglado291995@gmail.com>
2025-07-16 11:24:21 -0400

    Add files via upload

---
+++ ir_attachment_print_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "IR Attachment Print Report",
+    "version": "1.0",
+    "depends": ["base"],
+    "category": "Tools",
+    "summary": "Adds a print option for ir.attachment",
+    "data": [
+        "views/report_attachment_template.xml",
+        "views/report.xml"
+    ],
+    "installable": True,
+    "application": False
+}
