02a1548da62bcd9201a314b889c20f8794e721a9
Karl-Aris <areglado291995@gmail.com>
2025-07-16 10:58:11 -0400

    Add files via upload

---
+++ attachment_report_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Attachment Report Module",
+    "version": "1.0",
+    "depends": ["base"],
+    "category": "Tools",
+    "summary": "Print action for ir.attachment",
+    "data": [
+        "views/report_attachment_template.xml",
+        "views/report.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
