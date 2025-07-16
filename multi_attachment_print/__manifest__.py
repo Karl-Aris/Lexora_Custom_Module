344c09d166df5718bbcb84971d455493ece14e3f
Karl-Aris <areglado291995@gmail.com>
2025-07-16 12:39:03 -0400

    Update __manifest__.py

--- multi_attachment_print/__manifest__.py
+++ multi_attachment_print/__manifest__.py
@@ -1,11 +1,10 @@
 {
     'name': 'Multi Attachment Print',
     'version': '1.0',
-    'summary': 'Print multiple ir.attachment PDFs',
+    'summary': 'Download multiple ir.attachment PDFs in one go',
     'category': 'Tools',
     'license': 'LGPL-3',
-    'depends': ['web'],
-    'data': [],
+    'depends': ['base', 'web'],
     'assets': {
         'web.assets_backend': [
             'multi_attachment_print/static/src/js/custom_attachment_download.js',
