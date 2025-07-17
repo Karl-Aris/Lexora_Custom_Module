669ee134ac97c4e11e4a6eb0234aa03e0691b4aa
Karl-Aris <areglado291995@gmail.com>
2025-07-17 12:42:30 -0400

    Update mail_mail.py

--- helpdesk_email_bcc/models/mail_mail.py
+++ helpdesk_email_bcc/models/mail_mail.py
@@ -1,6 +1,14 @@
+# file: models/mail_mail.py
+
 from odoo import models, fields
 
 class MailMail(models.Model):
     _inherit = 'mail.mail'
 
     email_bcc = fields.Char(string='BCC Emails')
+
+    def send(self, auto_commit=False, raise_exception=False):
+        for mail in self:
+            if mail.email_bcc:
+                mail.bcc = mail.email_bcc
+        return super().send(auto_commit=auto_commit, raise_exception=raise_exception)
