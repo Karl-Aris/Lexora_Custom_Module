d1908981a997b4fb9909e863ca0e98943e2e7565
Karl-Aris <areglado291995@gmail.com>
2025-07-17 13:20:16 -0400

    Update __manifest__.py

--- helpdesk_bcc_chatter/__manifest__.py
+++ helpdesk_bcc_chatter/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Helpdesk BCC Chatter",
     "summary": "Enable BCC in chatter messages for helpdesk",
     "version": "1.0",
-    "depends": ["helpdesk", "mail"],
+    "depends": ["helpdesk", "mail", "mail_bot"],
     "data": ["views/helpdesk_ticket_views.xml"],
     "author": "Karl Areglado",
     "category": "Tools",
