47d223527509455f6968ea91798174f0f6a43fe1
Karl-Aris <areglado291995@gmail.com>
2025-07-17 13:09:15 -0400

    Add files via upload

---
+++ helpdesk_bcc_chatter/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Helpdesk BCC Chatter",
+    "summary": "Enable BCC in chatter messages for helpdesk",
+    "version": "1.0",
+    "depends": ["helpdesk", "mail"],
+    "author": "Your Company",
+    "category": "Tools",
+    "installable": True,
+    "application": False
+}
