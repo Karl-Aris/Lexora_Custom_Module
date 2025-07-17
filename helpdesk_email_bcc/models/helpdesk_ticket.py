2b770b28e4aae3d6a3ee5bb6c78b265c38117e89
Karl-Aris <areglado291995@gmail.com>
2025-07-17 12:36:22 -0400

    Update helpdesk_ticket.py

--- helpdesk_email_bcc/models/helpdesk_ticket.py
+++ helpdesk_email_bcc/models/helpdesk_ticket.py
@@ -5,26 +5,19 @@ class HelpdeskTicket(models.Model):
 
     bcc_partner_ids = fields.Many2many('res.partner', string='BCC Recipients')
 
-    def message_post(self, **kwargs):
-        if self.bcc_partner_ids:
-            bcc_emails = ','.join(self.bcc_partner_ids.mapped('email'))
-            kwargs.setdefault('email_bcc', bcc_emails)
-        return super().message_post(**kwargs)
-
     def action_send_bcc_email(self):
         Mail = self.env['mail.mail']
         for ticket in self:
-            bcc_emails = ticket.bcc_partner_ids.mapped('email')
+            bcc_emails = ticket.bcc_partner_ids.filtered(lambda p: p.email).mapped('email')
             if not bcc_emails:
                 continue
 
-            mail = Mail.create({
+            Mail.create({
                 'subject': f"[Ticket #{ticket.id}] {ticket.name}",
                 'body_html': ticket.description or "<p>No content.</p>",
                 'email_from': self.env.user.email or self.env.company.email,
-                'email_to': '',  # Force BCC-only
+                'email_to': '',  # hidden TO
                 'email_bcc': ','.join(bcc_emails),
                 'model': 'helpdesk.ticket',
                 'res_id': ticket.id,
-            })
-            mail.send()
+            }).send()
