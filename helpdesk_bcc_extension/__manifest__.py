aa2c2ea20ce8a6588dd72830cc170e731cbfd309
Karl-Aris <areglado291995@gmail.com>
2025-07-17 15:07:47 -0400

    Update __manifest__.py

--- helpdesk_bcc_extension/__manifest__.py
+++ helpdesk_bcc_extension/__manifest__.py
@@ -5,7 +5,6 @@
     "author": "Karl Areglado",
     "category": "Helpdesk",
     "data": ['views/helpdesk_ticket_views.xml',
-            'views/mail_message_bcc_views.xml'
             ],
     "description": "Custom module to handle BCC in helpdesk tickets.",
     "installable": True,
