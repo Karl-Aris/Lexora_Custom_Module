dd38eafc783dc35498d2224afcaacddbcebf3e6e
Karl-Aris <areglado291995@gmail.com>
2025-07-17 14:41:06 -0400

    Update helpdesk_ticket.py

--- helpdesk_bcc_extension/models/helpdesk_ticket.py
+++ helpdesk_bcc_extension/models/helpdesk_ticket.py
@@ -12,15 +12,22 @@ class HelpdeskTicket(models.Model):
     )
 
     def message_post(self, **kwargs):
-        if 'email_layout_xmlid' in kwargs or 'template_id' in kwargs:
-            # Extract email addresses from bcc_partner_ids
-            bcc_emails = [partner.email for partner in self.bcc_partner_ids if partner.email]
+        # Get BCC emails
+        bcc_emails = [partner.email for partner in self.bcc_partner_ids if partner.email]
 
-            # Inject BCC in email context if not already passed
-            ctx = kwargs.get('mail_post_autofollow', True)
-            if bcc_emails:
-                # Force context and override email values
-                ctx = dict(self.env.context, bcc=bcc_emails)
-                self = self.with_context(ctx)
+        # Inject BCC into email values if message is meant to be emailed
+        if kwargs.get('message_type') == 'email':
+            email_values = kwargs.get('email_values', {})
+            existing_bcc = email_values.get('bcc', '')
+
+            # Combine existing and new BCCs (if any)
+            combined_bcc = existing_bcc.split(',') if existing_bcc else []
+            combined_bcc += bcc_emails
+            # Remove duplicates
+            combined_bcc = list(set([email.strip() for email in combined_bcc if email.strip()]))
+
+            # Update email_values with new BCC list
+            email_values['bcc'] = ','.join(combined_bcc)
+            kwargs['email_values'] = email_values
 
         return super(HelpdeskTicket, self).message_post(**kwargs)
