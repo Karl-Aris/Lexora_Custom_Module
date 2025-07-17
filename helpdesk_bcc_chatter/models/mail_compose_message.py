ac99803e1a04776900a2e65c62ca3939f5eb8482
Karl-Aris <areglado291995@gmail.com>
2025-07-17 13:23:08 -0400

    Update mail_compose_message.py

--- helpdesk_bcc_chatter/models/mail_compose_message.py
+++ helpdesk_bcc_chatter/models/mail_compose_message.py
@@ -4,7 +4,8 @@ class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     partner_cc_ids = fields.Many2many(
-        'res.partner',
-        string="CC Partners",
-        help="Optional CC contacts for the message"
-    )
+    'res.partner',
+    string="CC Partners",
+    help="Optional CC recipients for messages"
+)
+
