99e49656f33ea0e665c60d1f8b8bbf2b642ea118
Karl-Aris <areglado291995@gmail.com>
2025-07-17 15:15:56 -0400

    Update helpdesk_ticket.py

--- helpdesk_bcc_extension/models/helpdesk_ticket.py
+++ helpdesk_bcc_extension/models/helpdesk_ticket.py
@@ -4,21 +4,22 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     def message_post(self, **kwargs):
+        # Remove unsupported key
         bcc_partners = kwargs.pop('bcc_partner_ids', False)
 
-        # Post the normal message first
+        # Call original message_post
         message = super().message_post(**kwargs)
 
+        # Do your custom BCC logic
         if bcc_partners:
             bcc_emails = self.env['res.partner'].browse(bcc_partners).mapped('email')
             if bcc_emails:
-                mail_values = {
+                self.env['mail.mail'].create({
                     'subject': kwargs.get('subject', f'Re: {self.display_name}'),
                     'body_html': kwargs.get('body', ''),
                     'email_from': self.env.user.email_formatted,
                     'email_bcc': ','.join(bcc_emails),
                     'auto_delete': True,
-                }
-                self.env['mail.mail'].create(mail_values).send()
+                }).send()
 
         return message
