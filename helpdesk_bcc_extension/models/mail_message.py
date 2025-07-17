bce3fed55e762e37fde235704795637733341eec
Karl-Aris <areglado291995@gmail.com>
2025-07-17 15:04:56 -0400

    Update mail_message.py

--- helpdesk_bcc_extension/models/mail_message.py
+++ helpdesk_bcc_extension/models/mail_message.py
@@ -1,12 +1,19 @@
-from odoo import models, fields
+from odoo import models
 
 class MailMessage(models.Model):
     _inherit = 'mail.message'
 
-    bcc_partner_ids = fields.Many2many(
-        'res.partner',
-        'mail_message_bcc_partner_rel',
-        'message_id',
-        'partner_id',
-        string='BCC Recipients'
-    )
+    def _notify(self, force_send=False, send_after_commit=True, user_signature=True):
+        notifications = super()._notify(
+            force_send=force_send,
+            send_after_commit=send_after_commit,
+            user_signature=user_signature,
+        )
+
+        for msg in self:
+            if msg.model == 'helpdesk.ticket' and msg.res_id:
+                ticket = self.env['helpdesk.ticket'].browse(msg.res_id)
+                bcc_partners = ticket.bcc_partner_ids
+                bcc_notif = msg._notify_record_by_email(bcc_partners, force_send=force_send, send_after_commit=send_after_commit)
+                notifications |= bcc_notif
+        return notifications
