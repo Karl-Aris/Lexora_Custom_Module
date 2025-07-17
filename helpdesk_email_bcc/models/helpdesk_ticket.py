75f70373edc780c400f8afc34512e79aefd4c80d
Karl-Aris <areglado291995@gmail.com>
2025-07-17 12:45:36 -0400

    Update helpdesk_ticket.py

--- helpdesk_email_bcc/models/helpdesk_ticket.py
+++ helpdesk_email_bcc/models/helpdesk_ticket.py
@@ -1,3 +1,5 @@
+# helpdesk_email_bcc/models/helpdesk_ticket.py
+
 from odoo import models, fields
 
 class HelpdeskTicket(models.Model):
@@ -9,13 +11,14 @@ class HelpdeskTicket(models.Model):
         res = super().message_post(**kwargs)
         for ticket in self:
             if ticket.bcc_partner_ids:
-                self.env['mail.mail'].create({
-                    'subject': res.subject or ticket.name,
-                    'body_html': res.body or '',
-                    'model': ticket._name,
-                    'res_id': ticket.id,
-                    'email_from': self.env.user.email,
-                    'email_to': '',  # real recipients hidden
-                    'email_bcc': ','.join(ticket.bcc_partner_ids.mapped('email')),
-                }).send()
+                bcc_emails = ','.join(ticket.bcc_partner_ids.mapped('email'))
+                if bcc_emails:
+                    self.env['mail.mail'].create({
+                        'subject': f"Ticket #{ticket.id}: {ticket.name}",
+                        'body_html': ticket.description or "<p>No content</p>",
+                        'email_to': '',  # no TO
+                        'bcc': bcc_emails,
+                        'model': 'helpdesk.ticket',
+                        'res_id': ticket.id,
+                    }).send()
         return res
