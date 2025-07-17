c24f7aec8442a30a7f921c36e8a80bc28a87613c
Karl-Aris <areglado291995@gmail.com>
2025-07-17 15:19:30 -0400

    Update helpdesk_ticket.py

--- helpdesk_bcc_extension/models/helpdesk_ticket.py
+++ helpdesk_bcc_extension/models/helpdesk_ticket.py
@@ -1,25 +1,39 @@
-from odoo import models
+from odoo import models, fields
+
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
+    bcc_partner_ids = fields.Many2many(
+        comodel_name='res.partner',
+        relation='helpdesk_ticket_bcc_rel',
+        column1='ticket_id',
+        column2='partner_id',
+        string='BCC Recipients'
+    )
+
     def message_post(self, **kwargs):
-        # Remove unsupported key
+        # Remove unsupported key before passing to super
         bcc_partners = kwargs.pop('bcc_partner_ids', False)
 
-        # Call original message_post
+        # Call parent method
         message = super().message_post(**kwargs)
 
-        # Do your custom BCC logic
+        # Handle BCC sending logic separately
         if bcc_partners:
-            bcc_emails = self.env['res.partner'].browse(bcc_partners).mapped('email')
+            partners = self.env['res.partner'].browse(bcc_partners)
+            bcc_emails = partners.mapped('email')
             if bcc_emails:
-                self.env['mail.mail'].create({
-                    'subject': kwargs.get('subject', f'Re: {self.display_name}'),
-                    'body_html': kwargs.get('body', ''),
-                    'email_from': self.env.user.email_formatted,
-                    'email_bcc': ','.join(bcc_emails),
-                    'auto_delete': True,
-                }).send()
+                for record in self:
+                    self.env['mail.mail'].create({
+                        'subject': kwargs.get('subject', f'Re: {record.display_name}'),
+                        'body_html': kwargs.get('body', ''),
+                        'email_from': self.env.user.email_formatted,
+                        'email_to': False,  # Explicitly no "To" email
+                        'email_bcc': ','.join(bcc_emails),
+                        'model': record._name,
+                        'res_id': record.id,
+                        'auto_delete': True,
+                    }).send()
 
         return message
