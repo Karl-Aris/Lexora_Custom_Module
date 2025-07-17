203fe96f1dccef54a82cedcc11cd39d95b81e4f6
Karl-Aris <areglado291995@gmail.com>
2025-07-17 13:04:23 -0400

    Delete helpdesk_bcc directory

--- helpdesk_bcc/helpdesk_bcc/models/helpdesk_ticket.py
+++
@@ -1,7 +0,0 @@
-
-from odoo import models, fields
-
-class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
-
-    bcc_partner_ids = fields.Many2many('res.partner', string='BCC Recipients')
