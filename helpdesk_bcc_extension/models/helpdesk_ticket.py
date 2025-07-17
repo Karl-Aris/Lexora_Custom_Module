217d360071ee21b8964159c64a6cfed04f9ee59b
Karl-Aris <areglado291995@gmail.com>
2025-07-17 14:19:30 -0400

    Update helpdesk_ticket.py

--- helpdesk_bcc_extension/models/helpdesk_ticket.py
+++ helpdesk_bcc_extension/models/helpdesk_ticket.py
@@ -3,17 +3,17 @@ from odoo import models, fields
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    bcc_partner_ids = fields.Many2one('res.partner')
+    bcc_partner_id = fields.Many2one('res.partner', string="BCC Recipient")
 
     def custom_send_email_with_bcc(self, body, subject, to_partner_id):
         if not to_partner_id:
             return
-        bcc_emails = [partner.email for partner in self.bcc_partner_ids if partner.email]
+        bcc_email = self.bcc_partner_id.email if self.bcc_partner_id and self.bcc_partner_id.email else False
         self.message_post(
             body=body,
             subject=subject,
             partner_ids=[to_partner_id.id],
-            email_bcc=bcc_emails,
+            email_bcc=[bcc_email] if bcc_email else [],
             message_type='email',
             subtype_xmlid='mail.mt_comment',
         )
