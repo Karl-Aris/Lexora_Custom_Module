71da13ec515d3df53ea4739c8e5aab5ead6cb80c
Karl-Aris <areglado291995@gmail.com>
2025-07-17 15:12:42 -0400

    Update mail_message.py

--- helpdesk_bcc_extension/models/mail_message.py
+++ helpdesk_bcc_extension/models/mail_message.py
@@ -1,12 +1,15 @@
-# helpdesk_bcc_extension/models/mail_message.py
 from odoo import models, fields
 
 class MailMessage(models.Model):
     _inherit = 'mail.message'
 
-    recipient_cc_ids = fields.Many2many('res.partner', string='CC Recipients')
-
-
+    recipient_cc_ids = fields.Many2many(
+        'res.partner',
+        'mail_message_res_partner_cc_rel',  # custom relation table
+        'mail_message_id',                  # column referencing mail.message
+        'res_partner_id',                   # column referencing res.partner
+        string='CC Recipients'
+    )
     def _notify(self, force_send=False, send_after_commit=True, user_signature=True):
         notifications = super()._notify(
             force_send=force_send,
