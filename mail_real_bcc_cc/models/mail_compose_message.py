23422cca18212841ffee9b5a3ad73533ea631ce7
Karl-Aris <areglado291995@gmail.com>
2025-07-19 00:02:52 +0800

    Update mail_compose_message.py

--- mail_real_bcc_cc/models/mail_compose_message.py
+++ mail_real_bcc_cc/models/mail_compose_message.py
@@ -1,37 +1,45 @@
-from odoo import models, fields, api
+from odoo import models, fields
+
 
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
-        cc_partner_ids = fields.Many2many(
+    cc_partner_ids = fields.Many2many(
         comodel_name='res.partner',
         relation='mail_compose_cc_partner_rel',
         column1='compose_id',
         column2='partner_id',
         string='CC'
-        )
-        bcc_partner_ids = fields.Many2many(
-            comodel_name='res.partner',
-            relation='mail_compose_bcc_partner_rel',  # ✅ different relation name!
-            column1='compose_id',
-            column2='partner_id',
-            string='BCC'
-        )
+    )
+    bcc_partner_ids = fields.Many2many(
+        comodel_name='res.partner',
+        relation='mail_compose_bcc_partner_rel',
+        column1='compose_id',
+        column2='partner_id',
+        string='BCC'
+    )
 
     def action_send_mail(self):
-        res = super().action_send_mail()
-        if not self.env.context.get('mail_mail_created_ids'):
-            return res
+        email_to = ','.join(self.partner_ids.mapped('email'))
+        email_cc = ','.join(self.cc_partner_ids.mapped('email'))
+        email_bcc = ','.join(self.bcc_partner_ids.mapped('email'))
 
-        mails = self.env['mail.mail'].browse(self.env.context.get('mail_mail_created_ids'))
-        for mail in mails:
-            # Set CC header
-            if self.cc_partner_ids:
-                mail.email_cc = ','.join(self.cc_partner_ids.mapped('email'))
-            # Set BCC header and remove those from To:
-            if self.bcc_partner_ids:
-                bcc_emails = self.bcc_partner_ids.mapped('email')
-                mail.email_bcc = ','.join(bcc_emails)
-                if mail.email_to:
-                    mail.email_to = ','.join(e for e in mail.email_to.split(',') if e.strip() not in bcc_emails)
-        return res
+        mail_values = {
+            'subject': self.subject or '(No Subject)',
+            'body_html': self.body or '',
+            'email_to': email_to,
+            'email_cc': email_cc,
+            'email_bcc': email_bcc,
+            'auto_delete': True,
+            'email_from': self.env.user.email or 'noreply@example.com',
+            'reply_to': self.env.user.email or 'noreply@example.com',
+        }
+
+        res_model = self.model
+        res_id = self.env.context.get('default_res_id')
+        if res_model and res_id:
+            mail_values['res_model'] = res_model
+            mail_values['res_id'] = res_id
+
+        self.env['mail.mail'].sudo().create(mail_values).send()
+        return {'type': 'ir.actions.act_window_close'}
