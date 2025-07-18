a9382e45fcdf40bbd6f18dbc1e9dbdbce98cbbf2
Karl-Aris <areglado291995@gmail.com>
2025-07-18 22:56:12 +0800

    Create __manifest__.py

---
+++ mail_real_bcc_cc/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Real CC/BCC in Mail Composer",
+    "version": "15.0.1.0.0",
+    "summary": "Restore proper CC and BCC email headers, bypassing OCA social",
+    "author": "Karl Areglado",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": ["views/mail_compose_message.xml"],
+    "installable": True,
+    "auto_install": False
+}
