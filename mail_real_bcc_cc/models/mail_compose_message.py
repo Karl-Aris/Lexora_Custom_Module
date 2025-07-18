220c6e5e8d204a46ed477204bc5c42d2da02c150
Karl-Aris <areglado291995@gmail.com>
2025-07-18 23:51:45 +0800

    Update mail_compose_message.py

--- mail_real_bcc_cc/models/mail_compose_message.py
+++ mail_real_bcc_cc/models/mail_compose_message.py
@@ -19,23 +19,24 @@ class MailComposeMessage(models.TransientModel):
     )
 
     def action_send_mail(self):
-    email_to = ','.join(self.partner_ids.mapped('email'))
-    email_cc = ','.join(self.cc_partner_ids.mapped('email'))
-    email_bcc = ','.join(self.bcc_partner_ids.mapped('email'))
+        email_to = ','.join(self.partner_ids.mapped('email'))
+        email_cc = ','.join(self.cc_partner_ids.mapped('email'))
+        email_bcc = ','.join(self.bcc_partner_ids.mapped('email'))
 
-    mail_values = {
-        'subject': self.subject or '(No Subject)',
-        'body_html': self.body or '',
-        'email_to': email_to,
-        'email_cc': email_cc,
-        'email_bcc': email_bcc,
-        'auto_delete': True,
-        'email_from': self.env.user.email or 'noreply@example.com',
-    }
+        mail_values = {
+            'subject': self.subject or '(No Subject)',
+            'body_html': self.body or '',
+            'email_to': email_to,
+            'email_cc': email_cc,
+            'email_bcc': email_bcc,
+            'auto_delete': True,
+            'email_from': self.env.user.email or 'noreply@example.com',
+            'reply_to': self.env.user.email or 'noreply@example.com',
+        }
 
-    if self.model and self.res_id:
-        mail_values['res_model'] = self.model
-        mail_values['res_id'] = self.res_id
+        if self.model and self.res_id:
+            mail_values['res_model'] = self.model
+            mail_values['res_id'] = self.res_id
 
-    self.env['mail.mail'].create(mail_values).send()
-    return {'type': 'ir.actions.act_window_close'}
+        self.env['mail.mail'].sudo().create(mail_values).send()
+        return {'type': 'ir.actions.act_window_close'}
