1284287b61ec299284c468e261e1b28b25622da4
Karl-Aris <areglado291995@gmail.com>
2025-07-21 12:06:13 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc_fixes/models/mail_mail.py
+++ mail_composer_cc_bcc_fixes/models/mail_mail.py
@@ -3,10 +3,8 @@ from odoo import models
 class MailMail(models.Model):
     _inherit = 'mail.mail'
 
-    def send(self, auto_commit=False, raise_exception=False):
-        for mail in self:
-            if mail.email_bcc:
-                # inject BCC directly into email_values
-                email_values = {'email_bcc': mail.email_bcc}
-                mail.write(email_values)
-        return super().send(auto_commit=auto_commit, raise_exception=raise_exception)
+    def _send_prepare_values(self, mail_values=None):
+        values = super()._send_prepare_values(mail_values)
+        if self.email_bcc:
+            values['bcc'] = self.email_bcc.split(',') if isinstance(self.email_bcc, str) else self.email_bcc
+        return values
