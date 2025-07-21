5ae71dea124d1caa5bfb87a7fe386791ab2b79f1
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-22 01:40:34 +0800

    Update __manifest__.py

--- product_configuration/__manifest__.py
+++ product_configuration/__manifest__.py
@@ -5,7 +5,9 @@
     'author': 'Aljon Garde',
     'category': 'Website',
     'description': 'Automatically hides or shows products on the website based on stock availability.',
-    'data': ['views/product_configuration.xml'],
+    'data': [
+        'views/product_configuration.xml'
+    ],
     'installable': True,
     'auto_install': False,
     'application': False,
