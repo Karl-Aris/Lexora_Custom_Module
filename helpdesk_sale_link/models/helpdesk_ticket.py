167afe5ef1f2c183aaa88c7cb8ec5674443536a7
Karl-Aris <areglado291995@gmail.com>
2025-07-21 10:08:31 -0400

    Update helpdesk_ticket.py

--- helpdesk_sale_link/models/helpdesk_ticket.py
+++ helpdesk_sale_link/models/helpdesk_ticket.py
@@ -4,19 +4,11 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     x_matched_sale_order_ids = fields.Many2many('sale.order', string="Matched Sale Orders")
-    x_matched_sale_order_count = fields.Integer(string="Matched SO Count", compute="_compute_matched_sale_order_count")
+    x_x_matched_sale_order_ids_helpdesk_ticket_count = fields.Integer(
+        string="Matched Sale Orders Count",
+        compute="_compute_matched_sale_order_count"
+    )
 
     def _compute_matched_sale_order_count(self):
         for record in self:
-            record.x_matched_sale_order_count = len(record.x_matched_sale_order_ids)
-
-    def action_view_matched_sale_orders(self):
-        self.ensure_one()
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Matched Sale Orders',
-            'res_model': 'sale.order',
-            'view_mode': 'tree,form',
-            'domain': [('id', 'in', self.x_matched_sale_order_ids.ids)],
-            'context': {'create': False}
-        }
+            record.x_x_matched_sale_order_ids_helpdesk_ticket_count = len(record.x_matched_sale_order_ids)
