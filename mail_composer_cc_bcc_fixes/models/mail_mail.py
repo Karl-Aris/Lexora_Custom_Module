f923286d3aeef43976a117e5dffdec89b714d3af
Karl-Aris <areglado291995@gmail.com>
2025-07-21 12:31:27 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc_fixes/models/mail_mail.py
+++ mail_composer_cc_bcc_fixes/models/mail_mail.py
@@ -6,14 +6,15 @@ class MailMail(models.Model):
 
     def send(self, auto_commit=False, raise_exception=False):
         for mail in self:
-            email = mail._prepare_email()
+            email = mail._build_email()
             if mail.email_bcc:
                 bcc_list = [e.strip() for e in mail.email_bcc.split(',') if e.strip()]
-                email['Bcc'] = ', '.join(bcc_list)  # 👈 add it to MIME headers
+                email['Bcc'] = ', '.join(bcc_list)  # Force Bcc into headers
+
             try:
                 smtp_session = mail._get_smtp_session()
                 smtp_session.send_message(email)
             except Exception as e:
                 if raise_exception:
-                    raise MailDeliveryException(_("Failed to send email."), e)
+                    raise MailDeliveryException("Failed to send email.", e)
         return True
