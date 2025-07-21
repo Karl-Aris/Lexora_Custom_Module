7e69089e2731b2b53947812e90bd698fe1e9228f
Karl-Aris <areglado291995@gmail.com>
2025-07-21 12:33:17 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc_fixes/models/mail_mail.py
+++ mail_composer_cc_bcc_fixes/models/mail_mail.py
@@ -1,20 +1,13 @@
 from odoo import models
-from odoo.addons.mail.models.mail_mail import MailDeliveryException
 
 class MailMail(models.Model):
     _inherit = 'mail.mail'
 
     def send(self, auto_commit=False, raise_exception=False):
         for mail in self:
-            email = mail._build_email()
             if mail.email_bcc:
-                bcc_list = [e.strip() for e in mail.email_bcc.split(',') if e.strip()]
-                email['Bcc'] = ', '.join(bcc_list)  # Force Bcc into headers
-
-            try:
-                smtp_session = mail._get_smtp_session()
-                smtp_session.send_message(email)
-            except Exception as e:
-                if raise_exception:
-                    raise MailDeliveryException("Failed to send email.", e)
-        return True
+                # This ensures BCC is passed to the mail gateway
+                mail.email_bcc = ', '.join(
+                    email.strip() for email in mail.email_bcc.split(',') if email.strip()
+                )
+        return super().send(auto_commit=auto_commit, raise_exception=raise_exception)
