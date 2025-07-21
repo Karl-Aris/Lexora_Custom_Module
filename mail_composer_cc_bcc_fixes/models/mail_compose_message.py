fa383bbb95db4503339393c69ec8fd21fa2aae40
Karl-Aris <areglado291995@gmail.com>
2025-07-21 13:13:38 -0400

    Update mail_compose_message.py

--- mail_composer_cc_bcc_fixes/models/mail_compose_message.py
+++ mail_composer_cc_bcc_fixes/models/mail_compose_message.py
@@ -1,29 +1,13 @@
-from odoo import api, models
+from odoo import models, fields
 
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
-    @api.model
-    def _action_send_mail(self, auto_commit=False):
-        mails, messages = super()._action_send_mail(auto_commit=auto_commit)
+    custom_bcc = fields.Char(string='BCC (Custom)')
 
-        ctx = self.env.context
-        bcc_partner_ids = ctx.get('default_partner_bcc_ids', [])
-        bcc_emails = ctx.get('default_bcc_emails', False)
-
-        emails = set()
-        if isinstance(bcc_partner_ids, (list, tuple)) and bcc_partner_ids:
-            partner_ids = bcc_partner_ids[2] if len(bcc_partner_ids) >= 3 else bcc_partner_ids
-            for partner in self.env['res.partner'].browse(partner_ids):
-                if partner.email:
-                    emails.add(partner.email)
-        if bcc_emails:
-            emails.update([e.strip() for e in bcc_emails.split(',') if e.strip()])
-
-        if emails:
-            for mail in mails:
-                mail.write({'email_bcc': ','.join(emails)})
-
-        for mail in mails:
-            mail.send(auto_commit=auto_commit)
-        return mails, messages
+    def get_mail_values(self, res_ids):
+        mail_values = super().get_mail_values(res_ids)
+        for res_id in res_ids:
+            if self.custom_bcc:
+                mail_values[res_id]['email_bcc'] = self.custom_bcc
+        return mail_values
