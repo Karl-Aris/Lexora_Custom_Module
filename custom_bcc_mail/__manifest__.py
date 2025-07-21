9ed2102ac14ed4992f432f14ca07899559d66e9a
Karl-Aris <areglado291995@gmail.com>
2025-07-21 13:30:47 -0400

    Update __manifest__.py

--- custom_bcc_mail/__manifest__.py
+++ custom_bcc_mail/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    "name": "Custom BCC Mail v2",
+    "name": "Custom BCC Mail v3",
     "version": "1.0",
     "depends": ["mail"],
     "author": "Karl Areglado",
     "category": "Tools",
-    "description": "Add BCC field to mail.compose.message",
+    "description": "Add BCC field to mail.compose.message wizard",
     "data": ["views/mail_compose_message_view.xml"],
     "installable": True,
     "auto_install": False
