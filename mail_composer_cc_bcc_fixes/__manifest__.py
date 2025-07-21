7da923811c11c48657995fa4eb091c870d4e674e
Karl-Aris <areglado291995@gmail.com>
2025-07-21 13:03:19 -0400

    Update __manifest__.py

--- mail_composer_cc_bcc_fixes/__manifest__.py
+++ mail_composer_cc_bcc_fixes/__manifest__.py
@@ -4,6 +4,8 @@
     "summary": "Ensures BCC emails are sent when using mail_composer_cc_bcc",
     "category": "Mail",
     "depends": ["mail_composer_cc_bcc", "mail"],
-    "data": [],
+    "data": [
+        "views/mail_mail_views.xml",
+    ],
     "installable": True
 }
