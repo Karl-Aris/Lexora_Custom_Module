0b18e5622b5fbbbd74e3ee0d4d7574f303ca5c39
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-21 22:30:53 +0800

    Add files via upload

---
+++ product_configuration/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Hide Out-of-Stock Products on Website',
+    'version': '1.0',
+    'depends': ['product', 'website_sale'],
+    'author': 'Karl Areglado',
+    'category': 'Website',
+    'description': 'Automatically hides or shows products on the website based on stock availability.',
+    'data': ['data/product_configuration.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
