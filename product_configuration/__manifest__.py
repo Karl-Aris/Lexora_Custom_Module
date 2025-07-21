cbd6649dd5b038a18967f56859bfde0432299c4f
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-22 01:56:12 +0800

    Add files via upload

---
+++ product_configuration/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Product Config',
+    'version': '1.0',
+    'depends': ['product', 'website_sale'],
+    'author': 'Aljon Garde',
+    'category': 'Website',
+    'description': 'Automatically hides or shows products on the website based on stock availability.',
+    'data': [
+        'views/product_configuration.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
