4f1e038e09e5bbdc801248caf72316581700262a
Karl-Aris <areglado291995@gmail.com>
2025-07-21 13:36:37 -0400

    Create mail_mail.py

---
+++ custom_bcc_mail/models/mail_mail.py
@@ -0,0 +1,10 @@
+from odoo import models
+
+class MailMail(models.Model):
+    _inherit = 'mail.mail'
+
+    def _send_prepare_values(self, mail_values=None):
+        values = super()._send_prepare_values(mail_values)
+        if self.email_bcc:
+            values['bcc'] = [e.strip() for e in self.email_bcc.split(',') if e.strip()]
+        return values
