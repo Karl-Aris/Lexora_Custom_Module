050884a3a322bf024d7ba9cb04f518b747f43bfa
Karl-Aris <areglado291995@gmail.com>
2025-07-21 12:10:57 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc_fixes/models/mail_mail.py
+++ mail_composer_cc_bcc_fixes/models/mail_mail.py
@@ -6,5 +6,5 @@ class MailMail(models.Model):
     def _send_prepare_values(self, mail_values=None):
         values = super()._send_prepare_values(mail_values)
         if self.email_bcc:
-            values['bcc'] = self.email_bcc.split(',') if isinstance(self.email_bcc, str) else self.email_bcc
+            values['bcc'] = [email.strip() for email in self.email_bcc.split(',') if email.strip()]
         return values
