d113ecb52f13de0d7b723304c72086029162af22
Karl-Aris <areglado291995@gmail.com>
2025-07-21 09:52:18 -0400

    Update __manifest__.py

--- helpdesk_sale_link/__manifest__.py
+++ helpdesk_sale_link/__manifest__.py
@@ -7,8 +7,9 @@
     "description": "Link Sale Orders to Helpdesk Tickets with smart button",
     "data": [
         "views/helpdesk_ticket_views.xml",
-        "views/helpdesk_ticket_action.xml"
+        "views/helpdesk_ticket_action.xml",
+        "views/helpdesk_sale_link_actions.xml",
     ],
     "installable": True,
-    "application": False
+    "application": False,
 }
