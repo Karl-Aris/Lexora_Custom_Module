34e71581ef6688eb1528a32dfb1c251e080b8207
Karl-Aris <areglado291995@gmail.com>
2025-07-21 13:25:22 -0400

    Update mail_compose_message.py

--- custom_bcc_mail/models/mail_compose_message.py
+++ custom_bcc_mail/models/mail_compose_message.py
@@ -3,11 +3,4 @@ from odoo import models, fields
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
-    custom_bcc = fields.Char(string='BCC')
-
-    def get_mail_values(self, res_ids):
-        mail_values = super().get_mail_values(res_ids)
-        for res_id in res_ids:
-            if self.custom_bcc:
-                mail_values[res_id]['bcc'] = [e.strip() for e in self.custom_bcc.split(',') if e.strip()]
-        return mail_values
+    email_bcc = fields.Char(string='BCC')
