f07f9dcf80aeb8f3e6ecc0038102ab42451137e6
Karl-Aris <areglado291995@gmail.com>
2025-07-21 13:36:16 -0400

    Update mail_compose_message.py

--- custom_bcc_mail/models/mail_compose_message.py
+++ custom_bcc_mail/models/mail_compose_message.py
@@ -4,3 +4,11 @@ class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     email_bcc = fields.Char(string='BCC')
+
+    def send_mail(self, auto_commit=False):
+        res = super().send_mail(auto_commit=auto_commit)
+        if self.email_bcc:
+            mail_mail = self.env['mail.mail'].search([('mail_message_id', 'in', self.mail_message_id.ids)])
+            for mail in mail_mail:
+                mail.email_bcc = self.email_bcc
+        return res
