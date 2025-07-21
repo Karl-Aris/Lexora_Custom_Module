3afcea324ea2dbfbe5fd048184c2491de8a30885
Karl-Aris <areglado291995@gmail.com>
2025-07-21 12:47:50 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc_fixes/models/mail_mail.py
+++ mail_composer_cc_bcc_fixes/models/mail_mail.py
@@ -1,13 +1,27 @@
+# File: mail_mail_bcc_override/models/mail_mail.py
+
 from odoo import models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class MailMail(models.Model):
     _inherit = 'mail.mail'
 
-    def send(self, auto_commit=False, raise_exception=False):
-        for mail in self:
-            if mail.email_bcc:
-                # This ensures BCC is passed to the mail gateway
-                mail.email_bcc = ', '.join(
-                    email.strip() for email in mail.email_bcc.split(',') if email.strip()
-                )
-        return super().send(auto_commit=auto_commit, raise_exception=raise_exception)
+    def _build_email(self, mail_values):
+        """
+        Override core _build_email method to force-inject 'bcc'
+        so it reaches SMTP email object regardless of OCA interference.
+        """
+        message = super()._build_email(mail_values)
+
+        if self.email_bcc:
+            bcc_list = [e.strip() for e in self.email_bcc.split(',') if e.strip()]
+            existing_bcc = message.get_all('Bcc', [])
+            # Avoid duplicates
+            for bcc in bcc_list:
+                if bcc not in existing_bcc:
+                    message['Bcc'] = message.get('Bcc', '') + (',' if message.get('Bcc') else '') + bcc
+            _logger.info("Injected BCC into outgoing email: %s", message['Bcc'])
+
+        return message
