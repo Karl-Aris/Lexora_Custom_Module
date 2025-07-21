4555a956df53011511e735afb42fb2a633e16951
Karl-Aris <areglado291995@gmail.com>
2025-07-21 12:11:38 -0400

    Delete product_configuration/controllers/mail_composer_cc_bcc_fixes directory

--- product_configuration/controllers/mail_composer_cc_bcc_fixes/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Fix BCC in Composer",
-    "version": "17.0.1.0.0",
-    "summary": "Ensures BCC emails are sent when using mail_composer_cc_bcc",
-    "category": "Mail",
-    "depends": ["mail_composer_cc_bcc", "mail"],
-    "data": [],
-    "installable": True
-}
