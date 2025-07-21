c137150f419fabf140ee01e69500328bfb148d0e
Karl-Aris <areglado291995@gmail.com>
2025-07-21 21:49:57 +0800

    Add files via upload

---
+++ helpdesk_sale_link/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Helpdesk Sale Order Link",
+    "version": "1.0",
+    "depends": ["helpdesk", "sale_management"],
+    "author": "ChatGPT",
+    "category": "Custom",
+    "description": "Link Sale Orders to Helpdesk Tickets with smart button",
+    "data": [
+        "views/helpdesk_ticket_views.xml",
+        "views/helpdesk_ticket_action.xml"
+    ],
+    "installable": True,
+    "application": False
+}
