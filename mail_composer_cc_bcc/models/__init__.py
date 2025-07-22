687c154619cc8afb98db94855dd957823f138067
Karl-Aris <areglado291995@gmail.com>
2025-07-22 10:25:37 -0400

    Update __init__.py

--- mail_composer_cc_bcc/models/__init__.py
+++ mail_composer_cc_bcc/models/__init__.py
@@ -1,6 +1,7 @@
 # These modules are sorted by calling sequence, i.e. mail_thread calls
 # mail_message, etc.
 from . import res_company
+from . import ir_mail_server
 from . import mail_template
 from . import mail_thread
 from . import mail_message
