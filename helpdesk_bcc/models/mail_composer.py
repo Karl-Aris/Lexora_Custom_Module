5af7cc2716dd9b5251b5eedf61d8d0fd55cd5e3f
Karl-Aris <areglado291995@gmail.com>
2025-07-22 08:38:39 -0400

    Update mail_composer.py

--- helpdesk_bcc/models/mail_composer.py
+++ helpdesk_bcc/models/mail_composer.py
@@ -1,22 +1,13 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class MailComposer(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     bcc = fields.Char('BCC')
 
-    @api.model
-    def create(self, vals):
-        if vals.get('bcc'):
-            self = super().create(vals)
-            self = self.with_context(bcc=vals['bcc'])
-            return self
-        return super().create(vals)
-
-    def send_mail(self, auto_commit=False):
-        res = super().send_mail(auto_commit=auto_commit)
-        bcc = self.bcc
-        if bcc:
-            mail_mail = self.env['mail.mail'].search([('mail_message_id', 'in', self.mail_ids.ids)])
-            mail_mail.write({'email_bcc': bcc})
-        return res
+    def get_mail_values(self, res_ids):
+        mail_values = super().get_mail_values(res_ids)
+        for res_id in res_ids:
+            if res_id in mail_values and self.bcc:
+                mail_values[res_id]['email_bcc'] = self.bcc
+        return mail_values
