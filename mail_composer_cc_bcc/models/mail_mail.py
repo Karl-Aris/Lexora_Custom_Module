da6f6fd2262901dd11fb97094d0082ed7a29b0b1
Karl-Aris <areglado291995@gmail.com>
2025-07-22 10:00:12 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc/models/mail_mail.py
+++ mail_composer_cc_bcc/models/mail_mail.py
@@ -1,28 +1,86 @@
-# Copyright 2023 Camptocamp
+# Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import fields, models, tools
+from odoo.addons.base.models.ir_mail_server import extract_rfc2822_addresses
 
 
 def format_emails(partners):
-    """Convert a recordset of res.partner into email strings."""
-    return ', '.join(p.email for p in partners if p.email) if partners else ''
+    emails = [
+        tools.formataddr((p.name or "", tools.email_normalize(p.email)))
+        for p in partners
+        if p.email
+    ]
+    return ", ".join(emails)
+
+
+def format_emails_raw(partners):
+    emails = [p.email for p in partners if p.email]
+    return ", ".join(emails)
 
 
 class MailMail(models.Model):
     _inherit = "mail.mail"
 
-    email_bcc = fields.Char("Bcc", help="Blind carbon copy recipients")
-    email_cc = fields.Char("Cc", help="Carbon copy recipients")
+    email_bcc = fields.Char("Bcc", help="Blind Cc message recipients")
 
-    @api.model
-    def create(self, values):
-        if values.get("recipient_cc_ids"):
-            partners_cc = self.env["res.partner"].browse(values["recipient_cc_ids"][0][2])
-            values["email_cc"] = format_emails(partners_cc)
+    def _prepare_outgoing_list(self, recipients_follower_status=None):
+        # First, return if we're not coming from the Mail Composer
+        res = super()._prepare_outgoing_list(
+            recipients_follower_status=recipients_follower_status
+        )
+        is_out_of_scope = len(self.ids) > 1
+        is_from_composer = self.env.context.get("is_from_composer", False)
 
-        if values.get("recipient_bcc_ids"):
-            partners_bcc = self.env["res.partner"].browse(values["recipient_bcc_ids"][0][2])
-            values["email_bcc"] = format_emails(partners_bcc)
+        if is_out_of_scope or not is_from_composer:
+            return res
 
-        return super().create(values)
+        # Prepare values for To, Cc headers
+        partners_cc_bcc = self.recipient_cc_ids + self.recipient_bcc_ids
+        partner_to_ids = [r.id for r in self.recipient_ids if r not in partners_cc_bcc]
+        partner_to = self.env["res.partner"].browse(partner_to_ids)
+        email_to = format_emails(partner_to)
+        email_to_raw = format_emails_raw(partner_to)
+        email_cc = format_emails(self.recipient_cc_ids)
+        email_bcc = [r.email for r in self.recipient_bcc_ids if r.email]
+
+        # Collect recipients (RCPT TO) and update all emails
+        recipients = set()
+        for m in res:
+            rcpt_to = None
+            if m["email_to"]:
+                rcpt_to = extract_rfc2822_addresses(m["email_to"][0])[0]
+
+                # Insert X-Odoo-Bcc and visible warning in the body
+                if rcpt_to in email_bcc:
+                    m["headers"].update({"X-Odoo-Bcc": m["email_to"][0]})
+
+                    # Insert visible warning in the email body
+                    warning_html = (
+                        '<div style="color:red; font-weight:bold; margin-bottom:10px;">'
+                        '🔒 You received this email as a BCC (Blind Carbon Copy). Please do not reply.'
+                        '</div>'
+                    )
+                    if "body" in m and m["body"]:
+                        m["body"] = warning_html + m["body"]
+
+            elif m.get("email_cc"):
+                rcpt_to = extract_rfc2822_addresses(m["email_cc"][0])[0]
+
+            if rcpt_to:
+                recipients.add(rcpt_to)
+
+            m.update(
+                {
+                    "email_to": email_to,
+                    "email_to_raw": email_to_raw,
+                    "email_cc": email_cc,
+                }
+            )
+
+        self.env.context = {**self.env.context, "recipients": list(recipients)}
+
+        if len(res) > len(recipients):
+            res.pop()
+
+        return res
