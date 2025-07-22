13065c8058c1216a3687ee436c3e6647922b2ee6
Karl-Aris <areglado291995@gmail.com>
2025-07-22 09:11:19 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc/models/mail_mail.py
+++ mail_composer_cc_bcc/models/mail_mail.py
@@ -1,68 +1,48 @@
-# Copyright 2023 Camptocamp SA
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models, tools
-from odoo.addons.base.models.ir_mail_server import extract_rfc2822_addresses
-
-
-def format_emails(partners):
-    emails = [
-        tools.formataddr((p.name or "", tools.email_normalize(p.email)))
-        for p in partners
-        if p.email
-    ]
-    return ", ".join(emails)
-
-
-def format_emails_raw(partners):
-    emails = [p.email for p in partners if p.email]
-    return ", ".join(emails)
+from odoo.addons.mail.models.mail_mail import format_emails, format_emails_raw, extract_rfc2822_addresses
+from odoo import models
 
 
 class MailMail(models.Model):
     _inherit = "mail.mail"
 
-    email_bcc = fields.Char("Bcc", help="Blind Cc message recipients")
-
     def _prepare_outgoing_list(self, recipients_follower_status=None):
-    res = super()._prepare_outgoing_list(recipients_follower_status=recipients_follower_status)
+        res = super()._prepare_outgoing_list(recipients_follower_status=recipients_follower_status)
 
-    is_out_of_scope = len(self.ids) > 1
-    is_from_composer = self.env.context.get("is_from_composer", False)
-    if is_out_of_scope or not is_from_composer:
-        return res
+        is_out_of_scope = len(self.ids) > 1
+        is_from_composer = self.env.context.get("is_from_composer", False)
+        if is_out_of_scope or not is_from_composer:
+            return res
 
-    partners_cc_bcc = self.recipient_cc_ids + self.recipient_bcc_ids
-    partner_to_ids = [r.id for r in self.recipient_ids if r not in partners_cc_bcc]
-    partner_to = self.env["res.partner"].browse(partner_to_ids)
+        partners_cc_bcc = self.recipient_cc_ids + self.recipient_bcc_ids
+        partner_to_ids = [r.id for r in self.recipient_ids if r not in partners_cc_bcc]
+        partner_to = self.env["res.partner"].browse(partner_to_ids)
 
-    email_to = format_emails(partner_to)
-    email_to_raw = format_emails_raw(partner_to)
-    email_cc = format_emails(self.recipient_cc_ids)
-    email_bcc_list = [p.email for p in self.recipient_bcc_ids if p.email]
+        email_to = format_emails(partner_to)
+        email_to_raw = format_emails_raw(partner_to)
+        email_cc = format_emails(self.recipient_cc_ids)
+        email_bcc_list = [p.email for p in self.recipient_bcc_ids if p.email]
 
-    new_res = []
-    for m in res:
-        if m.get("email_to"):
-            rcpt_to = extract_rfc2822_addresses(m["email_to"][0])[0]
-        elif m.get("email_cc"):
-            rcpt_to = extract_rfc2822_addresses(m["email_cc"][0])[0]
-        else:
-            rcpt_to = None
+        new_res = []
+        for m in res:
+            if m.get("email_to"):
+                rcpt_to = extract_rfc2822_addresses(m["email_to"][0])[0]
+            elif m.get("email_cc"):
+                rcpt_to = extract_rfc2822_addresses(m["email_cc"][0])[0]
+            else:
+                rcpt_to = None
 
-        if not rcpt_to:
-            continue
+            if not rcpt_to:
+                continue
 
-        m.update({
-            "email_to": email_to,
-            "email_to_raw": email_to_raw,
-            "email_cc": email_cc,
-        })
+            m.update({
+                "email_to": email_to,
+                "email_to_raw": email_to_raw,
+                "email_cc": email_cc,
+            })
 
-        # Show Bcc only to the Bcc recipient
-        if rcpt_to in email_bcc_list:
-            m["headers"].update({"Bcc": rcpt_to})
-        new_res.append(m)
+            if rcpt_to in email_bcc_list:
+                m["headers"].update({"Bcc": rcpt_to})
 
-    return new_res
+            new_res.append(m)
 
+        return new_res
