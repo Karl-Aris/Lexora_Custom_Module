c810a915aa274806795f3815c122a2cdae95a53c
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-22 23:25:04 +0800

    Update __manifest__.py

--- product_configuration/__manifest__.py
+++ product_configuration/__manifest__.py
@@ -1,12 +1,12 @@
 {
-    'name': 'Product Config',
+    "name": "Product SKU Search Page",
     'version': '1.0',
-    'depends': ['product', 'website_sale'],
+    "depends": ["website", "product"],
     'author': 'Aljon Garde',
     'category': 'Website',
     'description': 'Automatically hides or shows products on the website based on stock availability.',
     'data': [
-        'views/product_configuration.xml'
+        'views/product_configuration.xml',
     ],
     'installable': True,
     'auto_install': False,
