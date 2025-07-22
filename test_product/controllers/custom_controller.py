b5b8eea74b9cf0c200c01571ad22376b056d82a9
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-23 01:36:46 +0800

    Update custom_controller.py

--- test_product/controllers/custom_controller.py
+++ test_product/controllers/custom_controller.py
@@ -3,10 +3,10 @@ from odoo.http import request
 
 class WebsiteCustomController(http.Controller):
 
-    @http.route(['/custom_test'], type='http', auth='public', website=True)
+    @http.route(['/custom-test2'], type='http', auth='public', website=True)
     def search_by_sku(self, **kwargs):
         return request.render('custom_configuration.custom_page_template')
     
-    @http.route(['/custom_test/button'], type='http', auth='public', website=True)
+    @http.route(['/custom-test2/button'], type='http', auth='public', website=True)
     def search_by_sku(self, **kwargs):
         return request.redirect('/store')
