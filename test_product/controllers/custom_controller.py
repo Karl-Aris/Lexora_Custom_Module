0d9d5cbae113cdde88dbe3a52ec778f52e1926a3
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-23 01:41:42 +0800

    Update custom_controller.py

--- test_product/controllers/custom_controller.py
+++ test_product/controllers/custom_controller.py
@@ -5,8 +5,8 @@ class WebsiteCustomController(http.Controller):
 
     @http.route(['/custom_test2'], type='http', auth='public', website=True)
     def custom_test2(self, **kwargs):
-        return request.render('custom_configuration.custom_page_template')
+        return request.render('test_product.custom_page_template')
     
     @http.route(['/custom_test2/button'], type='http', auth='public', website=True)
     def custom_test2(self, **kwargs):
-        return request.redirect('/store')
+        return request.redirect('/custom_test2')
