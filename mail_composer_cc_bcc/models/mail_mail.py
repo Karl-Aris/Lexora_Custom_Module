2471a0180b245a3d13af049cdb15f4df4495929e
Karl-Aris <areglado291995@gmail.com>
2025-07-22 09:55:22 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc/models/mail_mail.py
+++ mail_composer_cc_bcc/models/mail_mail.py
@@ -1,30 +1,28 @@
-# mail_mail.py
-from odoo import models
-from odoo.addons.mail.models.mail_mail import MailMail
+# Copyright 2023 Camptocamp
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-def format_emails(emails):
-    return ', '.join(emails) if emails else ''
+from odoo import models, fields, api
+
+
+def format_emails(partners):
+    """Convert a recordset of res.partner into email strings."""
+    return ', '.join(p.email for p in partners if p.email) if partners else ''
+
 
 class MailMail(models.Model):
-    _inherit = 'mail.mail'
+    _inherit = "mail.mail"
 
-    def send(self, auto_commit=False, raise_exception=False):
-        # Send BCC emails individually so they are visible to each recipient
-        mails_to_send = self.filtered(lambda m: m.state == 'outgoing')
+    email_bcc = fields.Char("Bcc", help="Blind carbon copy recipients")
+    email_cc = fields.Char("Cc", help="Carbon copy recipients")
 
-        for mail in mails_to_send:
-            bcc_list = mail.email_bcc.split(',') if mail.email_bcc else []
-            if bcc_list:
-                for bcc_email in bcc_list:
-                    # Create a copy for each BCC recipient
-                    mail_copy = mail.copy({
-                        'email_to': bcc_email.strip(),
-                        'email_bcc': False,  # Prevent recursion
-                        'body_html': f'<p><strong>BCC:</strong> {bcc_email.strip()}</p>' + (mail.body_html or ''),
-                    })
-                    super(MailMail, mail_copy).send(auto_commit=auto_commit, raise_exception=raise_exception)
-                mail.write({'state': 'sent'})
-            else:
-                super(MailMail, mail).send(auto_commit=auto_commit, raise_exception=raise_exception)
+    @api.model
+    def create(self, values):
+        if values.get("recipient_cc_ids"):
+            partners_cc = self.env["res.partner"].browse(values["recipient_cc_ids"][0][2])
+            values["email_cc"] = format_emails(partners_cc)
 
-        return True
+        if values.get("recipient_bcc_ids"):
+            partners_bcc = self.env["res.partner"].browse(values["recipient_bcc_ids"][0][2])
+            values["email_bcc"] = format_emails(partners_bcc)
+
+        return super().create(values)
