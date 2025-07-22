e93513c2c06b204e4287f1e293ff48ebf22f8245
Karl-Aris <areglado291995@gmail.com>
2025-07-22 08:27:34 -0400

    Update __manifest__.py

--- helpdesk_bcc/__manifest__.py
+++ helpdesk_bcc/__manifest__.py
@@ -2,13 +2,18 @@
     "name": "Helpdesk BCC Support",
     "version": "17.0.1.0.0",
     "summary": "Adds BCC support to Helpdesk emails and chatter",
-    "author": "ChatGPT",
+    "author": "Karl Areglado",
     "category": "Helpdesk",
     "depends": ["mail", "helpdesk"],
-    "data": [
-        "views/mail_composer.xml"
-    ],
+    "data": [],
+    "assets": {},
     "installable": True,
     "application": False,
-    "license": "LGPL-3"
+    "license": "LGPL-3",
+    "post_init_hook": "post_init_hook",
+    "pre_init_hook": "pre_init_hook",
+    "auto_install": False,
+    "data": [
+        "views/mail_composer.xml"  # <-- leave this here, but make sure models are initialized first
+    ]
 }
