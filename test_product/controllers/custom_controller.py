34bcab418b2f4e88c63be83687603a19a5dbf57e
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-23 03:02:03 +0800

    Update custom_controller.py

--- test_product/controllers/custom_controller.py
+++ test_product/controllers/custom_controller.py
@@ -10,3 +10,14 @@ class WebsiteCustomController(http.Controller):
     @http.route(['/custom_test2/button'], type='http', auth='public', website=True)
     def button_redirect(self, **kwargs):
         return request.redirect('/custom_test2')
+    
+    @http.route(['/custom_test2/search_sku'], type='http', auth='public', website=True)
+    def search_by_sku(self, sku=None, **kwargs):
+        product = None
+        if sku:
+            product = request.env['product.product'].sudo().search([('default_code', '=', sku)], limit=1)
+
+        return request.render('test_product.custom_page_template', {
+            'product': product,
+            'sku': sku,
+        })
