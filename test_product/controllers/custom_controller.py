a6059d78a2296d5656cc0f397eb13b01c6b94d32
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-23 02:00:06 +0800

    Update custom_controller.py

--- test_product/controllers/custom_controller.py
+++ test_product/controllers/custom_controller.py
@@ -4,9 +4,9 @@ from odoo.http import request
 class WebsiteCustomController(http.Controller):
 
     @http.route(['/custom_test2'], type='http', auth='public', website=True)
-    def custom_test2(self, **kwargs):
+    def render_custom_test2(self, **kwargs):
         return request.render('test_product.custom_page_template')
-    
+
     @http.route(['/custom_test2/button'], type='http', auth='public', website=True)
-    def custom_test2(self, **kwargs):
+    def button_redirect(self, **kwargs):
         return request.redirect('/custom_test2')
