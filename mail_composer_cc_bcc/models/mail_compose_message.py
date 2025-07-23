fc865c485ece6d9ab6acaf3e181df782c14e5d12
Karl-Aris <areglado291995@gmail.com>
2025-07-23 15:56:11 -0400

    Update mail_compose_message.py

--- mail_composer_cc_bcc/models/mail_compose_message.py
+++ mail_composer_cc_bcc/models/mail_compose_message.py
@@ -5,4 +5,15 @@ class MailComposer(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     def get_mail_values(self, res_ids):
-        return super().get_mail_values(res_ids)
+        mail_values = super().get_mail_values(res_ids)
+        for res_id, values in mail_values.items():
+            # Add BCC notice only to the BCC-specific message body
+            context_partner_id = self.env.context.get('force_bcc_partner_id')
+            if context_partner_id and values.get('body'):
+                note = (
+                    "<p style='color:gray;font-size:small;'>"
+                    "🔒 You received this email as a BCC (Blind Carbon Copy). "
+                    "Please do not reply to all.</p>"
+                )
+                values['body'] = note + values['body']
+        return mail_values
