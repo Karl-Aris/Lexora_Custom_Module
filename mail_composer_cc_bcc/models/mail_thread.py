5935e3dc6e2c8a139e9947d942685d26cd33ec21
Karl-Aris <areglado291995@gmail.com>
2025-07-23 13:04:17 -0400

    Update mail_thread.py

--- mail_composer_cc_bcc/models/mail_thread.py
+++ mail_composer_cc_bcc/models/mail_thread.py
@@ -1,22 +1,35 @@
+# Copyright 2023 Camptocamp
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import models
 
-
-def format_emails(partners):
-    return ", ".join(
-        f'{partner.name} <{partner.email}>' if partner.name else partner.email
-        for partner in partners if partner.email
-    )
+from .mail_mail import format_emails
 
 
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
+    # ------------------------------------------------------------
+    # MAIL.MESSAGE HELPERS
+    # ------------------------------------------------------------
+
     def _get_message_create_valid_field_names(self):
+        """
+        add cc and bcc field to create record in mail.mail
+        """
         field_names = super()._get_message_create_valid_field_names()
         field_names.update({"recipient_cc_ids", "recipient_bcc_ids"})
         return field_names
 
+    # ------------------------------------------------------
+    # NOTIFICATION API
+    # ------------------------------------------------------
+
     def _notify_by_email_get_base_mail_values(self, message, additional_values=None):
+        """
+        This is to add cc, bcc addresses to mail.mail objects so that email
+        can be sent to those addresses.
+        """
         res = super()._notify_by_email_get_base_mail_values(
             message, additional_values=additional_values
         )
@@ -25,81 +38,21 @@ class MailThread(models.AbstractModel):
         if skip_adding_cc_bcc:
             return res
 
-        partners_cc = context.get("partner_cc_ids")
+        partners_cc = context.get("partner_cc_ids", None)
         if partners_cc:
-            res["recipient_cc_ids"] = [(6, 0, [p.id for p in partners_cc])]
             res["email_cc"] = format_emails(partners_cc)
 
-        # Don't include email_bcc in the header; keep Bcc secret
-        partners_bcc = context.get("partner_bcc_ids")
+        partners_bcc = context.get("partner_bcc_ids", None)
         if partners_bcc:
-            res["recipient_bcc_ids"] = [(6, 0, [p.id for p in partners_bcc])]
+            res["email_bcc"] = format_emails(partners_bcc)
 
         return res
 
-    def _notify_send(self, message, msg_vals, notif_recipients, **kwargs):
-        """
-        Split notifications: one for To+Cc, and separate ones for each Bcc.
-        """
-        MailMail = self.env['mail.mail']
-        notif_to_cc = []
-        notif_bcc_map = {}
-
-        for notif in notif_recipients:
-            partner = notif['partner']
-            if not partner.email:
-                continue
-
-            # Determine recipient role
-            if 'partner_bcc_ids' in self.env.context and partner in self.env.context['partner_bcc_ids']:
-                notif_bcc_map[partner.id] = notif
-            else:
-                notif_to_cc.append(notif)
-
-        # Send mail to To + Cc in one email
-        if notif_to_cc:
-            self.with_context(bcc_mode=False)._notify_send_single_mail(
-                message, msg_vals, notif_to_cc, **kwargs
-            )
-
-        # Send individual emails to Bcc (one by one)
-        for notif in notif_bcc_map.values():
-            partner = notif['partner']
-            self.with_context(
-                bcc_mode=True,
-                bcc_recipient_id=partner.id,
-                email_to=format_emails([partner]),
-                email_cc="",  # no cc
-            )._notify_send_single_mail(
-                message, msg_vals, [notif], **kwargs
-            )
-
-    def _notify_send_single_mail(self, message, msg_vals, notif_recipients, **kwargs):
-        """
-        Compose a single mail.mail object and send it.
-        Context must contain proper headers.
-        """
-        MailMail = self.env['mail.mail']
-        mail_values = self._notify_by_email_get_base_mail_values(message)
-        email_to = self.env.context.get("email_to")
-        email_cc = self.env.context.get("email_cc")
-        bcc_mode = self.env.context.get("bcc_mode", False)
-
-        if not bcc_mode:
-            to_emails = [p['partner'].email for p in notif_recipients if p['partner'].email]
-            mail_values['email_to'] = ", ".join(to_emails)
-            mail_values['email_cc'] = email_cc or ""
-        else:
-            mail_values['email_to'] = email_to or ""
-            # Do NOT include Bcc in header fields
-
-        mail_values['recipient_ids'] = [(6, 0, [p['partner'].id for p in notif_recipients])]
-
-        # Create and send mail
-        mail = MailMail.create(mail_values)
-        mail.send()
-
     def _notify_get_recipients(self, message, msg_vals, **kwargs):
+        """
+        This is to add cc, bcc recipients so that they can be grouped with
+        other recipients.
+        """
         ResPartner = self.env["res.partner"]
         MailFollowers = self.env["mail.followers"]
         rdata = super()._notify_get_recipients(message, msg_vals, **kwargs)
@@ -108,41 +61,45 @@ class MailThread(models.AbstractModel):
         skip_adding_cc_bcc = context.get("skip_adding_cc_bcc", False)
         if not is_from_composer or skip_adding_cc_bcc:
             return rdata
-
         for pdata in rdata:
             pdata["type"] = "customer"
-
         partners_cc_bcc = context.get("partner_cc_ids", ResPartner)
         partners_cc_bcc += context.get("partner_bcc_ids", ResPartner)
         msg_sudo = message.sudo()
-        message_type = msg_vals.get("message_type") if msg_vals else msg_sudo.message_type
+        message_type = (
+            msg_vals.get("message_type") if msg_vals else msg_sudo.message_type
+        )
         subtype_id = msg_vals.get("subtype_id") if msg_vals else msg_sudo.subtype_id.id
-
         recipients_cc_bcc = MailFollowers._get_recipient_data(
             None, message_type, subtype_id, partners_cc_bcc.ids
         )
-        partners_already_included = {r.get("id") for r in rdata}
-
+        partners_already_marked_as_recipient = [r.get("id", False) for r in rdata]
         for _, value in recipients_cc_bcc.items():
             for _, data in value.items():
-                partner_id = data.get("id")
-                if not partner_id or partner_id in partners_already_included:
+                if (
+                    not data.get("id")
+                    or data.get("id") in partners_already_marked_as_recipient
+                ):
                     continue
-
-                notif = data.get("notif") or "email"
+                if not data.get(
+                    "notif"
+                ):  # notif is False, has no user, is therefore customer
+                    notif = "email"
+                msg_type = "customer"
                 pdata = {
-                    "id": partner_id,
+                    "id": data.get("id"),
                     "active": data.get("active"),
                     "share": data.get("share"),
-                    "notif": notif,
-                    "type": "customer",
+                    "notif": data.get("notif") and data.get("notif") or notif,
+                    "type": msg_type,
                     "is_follower": data.get("is_follower"),
                 }
                 rdata.append(pdata)
-
         return rdata
 
-    def _notify_get_recipients_classify(self, message, recipients_data, model_description, msg_vals=None):
+    def _notify_get_recipients_classify(
+        self, message, recipients_data, model_description, msg_vals=None
+    ):
         res = super()._notify_get_recipients_classify(
             message, recipients_data, model_description, msg_vals=msg_vals
         )
@@ -150,7 +107,6 @@ class MailThread(models.AbstractModel):
         skip_adding_cc_bcc = self.env.context.get("skip_adding_cc_bcc", False)
         if not is_from_composer or skip_adding_cc_bcc:
             return res
-
         ids = []
         customer_data = None
         for rcpt_data in res:
@@ -158,14 +114,12 @@ class MailThread(models.AbstractModel):
                 customer_data = rcpt_data
             else:
                 ids += rcpt_data["recipients"]
-
         if not customer_data:
             customer_data = res[0]
             customer_data["notification_group_name"] = "customer"
             customer_data["recipients"] = ids
         else:
             customer_data["recipients"] += ids
-
         return [customer_data]
 
     def _notify_thread(self, message, msg_vals=False, **kwargs):
