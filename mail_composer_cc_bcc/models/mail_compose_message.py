c4075b894ef95389768a384acf46f5618451c836
Karl-Aris <areglado291995@gmail.com>
2025-07-23 09:27:44 -0400

    Update mail_compose_message.py

--- mail_composer_cc_bcc/models/mail_compose_message.py
+++ mail_composer_cc_bcc/models/mail_compose_message.py
@@ -1,18 +1,21 @@
-from odoo import models
+from odoo import models, fields
 
 
 class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
 
+    bcc_recipient_ids = fields.Many2many(
+        comodel_name="res.partner",
+        string="Bcc",
+        help="Partners who will receive a blind carbon copy of the email."
+    )
+
     def _action_send_mail(self, auto_commit=False):
-        # Call the original behavior
         res = super()._action_send_mail(auto_commit=auto_commit)
 
-        # Assign bcc_recipient_ids to the resulting mail.mail records
         mail_mail = self.env["mail.mail"]
         for wizard in self:
             if wizard.bcc_recipient_ids:
-                # Get all mail.mail records linked to the message created by this wizard
                 mails = mail_mail.search([
                     ("mail_message_id", "=", wizard.mail_message_id.id)
                 ])
