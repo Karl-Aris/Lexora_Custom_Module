b0d1dbd6613a12534e090d1a8648ed50a692603a
Karl-Aris <areglado291995@gmail.com>
2025-07-23 15:29:02 -0400

    Update mail_compose_message.py

--- mail_composer_cc_bcc/models/mail_compose_message.py
+++ mail_composer_cc_bcc/models/mail_compose_message.py
@@ -1,8 +1,17 @@
-from odoo import models
+from odoo import models, api, tools
 
 
 class MailComposer(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     def get_mail_values(self, res_ids):
-        return super().get_mail_values(res_ids)
+        mail_values = super().get_mail_values(res_ids)
+
+        has_bcc = bool(self.bcc_partner_ids)
+
+        for res_id, values in mail_values.items():
+            if has_bcc:
+                # Force a unique message_id to avoid reuse and threading issues
+                values["message_id"] = tools.generate_tracking_message_id()
+
+        return mail_values
