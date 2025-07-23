853ba438e93d37fb5c77af1d82d70a65ca0cc17e
Karl-Aris <areglado291995@gmail.com>
2025-07-23 11:11:31 -0400

    Update mail_mail.py

--- mail_composer_cc_bcc/models/mail_mail.py
+++ mail_composer_cc_bcc/models/mail_mail.py
@@ -16,53 +16,65 @@ class MailMail(models.Model):
 
     def _prepare_outgoing_list(self, recipients_follower_status=None):
         res = super()._prepare_outgoing_list(recipients_follower_status=recipients_follower_status)
-
+    
         if len(self.ids) != 1 or not self.env.context.get("is_from_composer"):
             return res
-
+    
         mail = self[0]
         recipient_to = mail.recipient_ids - mail.recipient_cc_ids - mail.recipient_bcc_ids
         recipient_cc = mail.recipient_cc_ids
         recipient_bcc = mail.recipient_bcc_ids
-
+    
         email_to = format_emails(recipient_to)
         email_cc = format_emails(recipient_cc)
-
         bcc_emails = [tools.email_normalize(p.email) for p in recipient_bcc if p.email]
+    
         final_msgs = []
-
-        # 1. Add the main message (To + Cc)
+        seen_recipients = set()
+    
         for msg in res:
+            if not isinstance(msg, dict):
+                _logger.warning("Skipped non-dict msg in mail._prepare_outgoing_list: %s", msg)
+                continue
+    
+            to_emails = extract_rfc2822_addresses(msg.get("email_to", ""))[0]
+            if not to_emails:
+                continue
+    
+            recipient_email = tools.email_normalize(to_emails[0])
+            if recipient_email in bcc_emails or recipient_email in seen_recipients:
+                continue
+    
             msg.update({
                 "email_to": email_to,
                 "email_cc": email_cc,
-                "email_bcc": "",  # hide bcc
+                "email_bcc": False,
             })
             final_msgs.append(msg)
-            break  # only one standard message
-
-        # 2. Add BCC messages separately
+            seen_recipients.update(extract_rfc2822_addresses(email_to)[0])
+            seen_recipients.update(extract_rfc2822_addresses(email_cc)[0])
+            break  # Only one main message
+    
+        # Now add BCC messages
         for bcc_email in bcc_emails:
-            new_msg = {
-                "subject": mail.subject,
-                "body": (
-                    "<p style='color:gray; font-style:italic;'>🔒 You received this email as a BCC (Blind Carbon Copy). "
-                    "Please do not reply-all.</p>"
-                    + mail.body
-                ),
-                "body_html": mail.body,
-                "email_from": mail.email_from,
-                "email_to": bcc_email,     # set direct recipient
-                "email_cc": email_cc,      # visible
-                "email_bcc": "",           # hidden
-                "reply_to": mail.reply_to,
-                "model": mail.model,
-                "res_id": mail.res_id,
-                "mail_server_id": mail.mail_server_id.id,
-                "auto_delete": mail.auto_delete,
-                "headers": mail.headers,
-                "message_id": tools.generate_tracking_message_id('mail.mail'),
-            }
-            final_msgs.append(new_msg)
-
+            if bcc_email in seen_recipients:
+                continue
+    
+            for base_msg in res:
+                if not isinstance(base_msg, dict):
+                    continue
+    
+                bcc_msg = base_msg.copy()
+                bcc_msg.update({
+                    "email_to": bcc_email,
+                    "email_cc": "",
+                    "email_bcc": "",
+                    "body": (
+                        "<p style='color:gray; font-style:italic;'>🔒 You received this email as a BCC (Blind Carbon Copy). "
+                        "Please do not reply.</p>" + base_msg.get("body", "")
+                    ),
+                })
+                final_msgs.append(bcc_msg)
+                break
+    
         return final_msgs
