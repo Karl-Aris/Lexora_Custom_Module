0a1796f17d90005233e47749183467feda020ae0
Karl-Aris <areglado291995@gmail.com>
2025-07-23 13:01:55 -0400

    Update ir_mail_server.py

--- mail_composer_cc_bcc/models/ir_mail_server.py
+++ mail_composer_cc_bcc/models/ir_mail_server.py
@@ -1,28 +1,35 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 import logging
+
 from odoo import models
 
 _logger = logging.getLogger(__name__)
+
 
 class IrMailServer(models.Model):
     _inherit = "ir.mail_server"
 
     def _prepare_email_message(self, message, smtp_session):
         """
-        Override to allow BCC messages to be sent to only one recipient.
+        Define smtp_to based on context instead of To+Cc+Bcc
         """
         x_odoo_bcc_value = next(
-            (value for key, value in message._headers if key == "X-Odoo-Bcc"),
-            None
+            (value for key, value in message._headers if key == "X-Odoo-Bcc"), None
         )
-
-        # Set actual Bcc header only temporarily for RFC compliance
+        # Add Bcc field inside message to pass validation
         if x_odoo_bcc_value:
             message["Bcc"] = x_odoo_bcc_value
 
-        smtp_from, smtp_to_list, message = super()._prepare_email_message(message, smtp_session)
+        smtp_from, smtp_to_list, message = super()._prepare_email_message(
+            message, smtp_session
+        )
 
-        if self.env.context.get("is_from_composer") and x_odoo_bcc_value:
-            _logger.debug("Redirecting BCC-only mail to: %s", x_odoo_bcc_value)
-            smtp_to_list = [x_odoo_bcc_value]
+        is_from_composer = self.env.context.get("is_from_composer", False)
+        if is_from_composer and self.env.context.get("recipients", False):
+            smtp_to = self.env.context["recipients"].pop(0)
+            _logger.debug("smtp_to: %s", smtp_to)
+            smtp_to_list = [smtp_to]
 
         return smtp_from, smtp_to_list, message
