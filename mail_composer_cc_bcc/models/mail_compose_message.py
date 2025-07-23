9ba5649be0b4dc351c0045fb6ef0e4f99cd3a972
Karl-Aris <areglado291995@gmail.com>
2025-07-23 14:37:45 -0400

    Update mail_compose_message.py

--- mail_composer_cc_bcc/models/mail_compose_message.py
+++ mail_composer_cc_bcc/models/mail_compose_message.py
@@ -5,5 +5,4 @@ class MailComposer(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     def get_mail_values(self, res_ids):
-        # Do not inject the BCC notice here — it's handled later in mail.mail
         return super().get_mail_values(res_ids)
