222b748ef4be0d2974f8a56320d9217a6c4e7c87
Karl-Aris <areglado291995@gmail.com>
2025-07-23 12:46:34 -0400

    Update mail_thread.py

--- mail_composer_cc_bcc/models/mail_thread.py
+++ mail_composer_cc_bcc/models/mail_thread.py
@@ -46,14 +46,15 @@ class MailThread(models.AbstractModel):
         if skip_adding_cc_bcc:
             return res
 
-       partners_cc = context.get("partner_cc_ids")
+        partners_cc = context.get("partner_cc_ids")
         if partners_cc:
             res["recipient_cc_ids"] = [(6, 0, [p.id for p in partners_cc])]
-        
+
         partners_bcc = context.get("partner_bcc_ids")
         if partners_bcc:
             res["recipient_bcc_ids"] = [(6, 0, [p.id for p in partners_bcc])]
 
+        return res  # <- make sure this line is present
 
     def _notify_get_recipients(self, message, msg_vals, **kwargs):
         """
