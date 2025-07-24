16302f3db016d1dc87c3290a414f6a6bb924281d
Karl-Aris <areglado291995@gmail.com>
2025-07-24 08:31:44 -0400

    Update __manifest__.py

--- mail_composer_cc_bcc/__manifest__.py
+++ mail_composer_cc_bcc/__manifest__.py
@@ -1,14 +1,11 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Email CC and BCC",
+    "name": "CC and BCC",
     "summary": "This module enables sending mail to CC and BCC partners in mail composer form.",  # noqa: E501
     "version": "17.0.1.0.1",
-    "development_status": "Alpha",
     "category": "Social",
-    "website": "https://github.com/OCA/social",
-    "author": "Camptocamp, Odoo Community Association (OCA)",
-    "maintainers": ["trisdoan"],
+    "author": "Karl Areglado",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
