fb8a53613b31cf659071ec4416d467f9159fa50e
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-24 22:25:58 +0800

    Update controller_config.py

--- product_configuration/controllers/controller_config.py
+++ product_configuration/controllers/controller_config.py
@@ -3,11 +3,6 @@ from odoo.http import request
 
 class ProductConfigurationController(http.Controller):
 
-    @http.route(['/store'], type='http', auth='public', website=True)
-    def render_product(self, **kwargs):
-        return request.render('product_configuration.template_product_configuration')
-
-    
     @http.route(['/store/cabinet'], type='http', auth='public', website=True)
     def search_by_sku(self, sku=None, **kwargs):
         product = None
@@ -16,18 +11,27 @@ class ProductConfigurationController(http.Controller):
         if sku:
             product = request.env['product.product'].sudo().search([('default_code', '=', sku)], limit=1)
 
-            if product and product.product_tmpl_id.collection_id:
-                collection_id = product.product_tmpl_id.collection_id.id
-                # Find all variants from other products in same collection
-                related_products = request.env['product.product'].sudo().search([
-                    ('product_tmpl_id.collection_id', '=', collection_id)
-                ])
+            if product:
+                template = product.product_tmpl_id
+                tag_names = template.product_tag_ids.mapped('name')
 
-                # Exclude current SKU if you want
-                related_sizes = related_products.filtered(lambda p: p.id != product.id)
+                # Extract collection tag (first non-numeric tag)
+                collection_tag = next((tag for tag in tag_names if not tag.isdigit()), None)
+
+                if collection_tag:
+                    # Search other templates with the same collection tag
+                    collection_tag_rec = request.env['product.tag'].sudo().search([('name', '=', collection_tag)], limit=1)
+
+                    matching_templates = request.env['product.template'].sudo().search([
+                        ('product_tag_ids', 'in', collection_tag_rec.id)
+                    ])
+
+                    related_sizes = request.env['product.product'].sudo().search([
+                        ('product_tmpl_id', 'in', matching_templates.ids)
+                    ])
 
         return request.render('product_configuration.template_product_configuration', {
             'product': product,
             'related_sizes': related_sizes,
-            'sku': sku,
+            'collection_name': collection_tag if product else '',
         })
