c1168edbf9b92a38874673cd7ed902a2696bbf64
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-24 22:31:50 +0800

    Update controller_config.py

--- product_configuration/controllers/controller_config.py
+++ product_configuration/controllers/controller_config.py
@@ -7,6 +7,7 @@ class ProductConfigurationController(http.Controller):
     def search_by_sku(self, sku=None, **kwargs):
         product = None
         related_sizes = []
+        collection_tag = ''
 
         if sku:
             product = request.env['product.product'].sudo().search([('default_code', '=', sku)], limit=1)
@@ -15,17 +16,26 @@ class ProductConfigurationController(http.Controller):
                 template = product.product_tmpl_id
                 tag_names = template.product_tag_ids.mapped('name')
 
-                # Extract collection tag (first non-numeric tag)
-                collection_tag = next((tag for tag in tag_names if not tag.isdigit()), None)
+                # Extract collection tag: the first non-numeric tag NOT in exclusion list
+                filter_out = ['Single', 'Vanity Only', 'Bathroom Vanities']
+                collection_tag = next((tag for tag in tag_names if not tag.isdigit() and tag not in filter_out), None)
 
                 if collection_tag:
-                    # Search other templates with the same collection tag
                     collection_tag_rec = request.env['product.tag'].sudo().search([('name', '=', collection_tag)], limit=1)
 
+                    # Required filtering tags
+                    filter_tag_names = ['Single', 'Vanity Only', 'Bathroom Vanities']
+                    filter_tags = request.env['product.tag'].sudo().search([('name', 'in', filter_tag_names)])
+
+                    # Search product templates that:
+                    # - Have the same collection tag
+                    # - AND have at least one of the required tags
                     matching_templates = request.env['product.template'].sudo().search([
-                        ('product_tag_ids', 'in', collection_tag_rec.id)
+                        ('product_tag_ids', 'in', collection_tag_rec.id),
+                        ('product_tag_ids', 'in', filter_tags.ids),
                     ])
 
+                    # Get all variants of those templates
                     related_sizes = request.env['product.product'].sudo().search([
                         ('product_tmpl_id', 'in', matching_templates.ids)
                     ])
@@ -33,5 +43,5 @@ class ProductConfigurationController(http.Controller):
         return request.render('product_configuration.template_product_configuration', {
             'product': product,
             'related_sizes': related_sizes,
-            'collection_name': collection_tag if product else '',
+            'collection_name': collection_tag,
         })
