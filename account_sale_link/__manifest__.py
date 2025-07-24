828debefb99169d9a8b9aa81c9341b939237c9b8
Karl-Aris <areglado291995@gmail.com>
2025-07-24 16:16:20 -0400

    Add files via upload

---
+++ account_sale_link/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Vendor Bill to Sale Order Link",
+    "version": "1.0",
+    "category": "Accounting",
+    "summary": "Link Vendor Bills to related Sale Orders using PO Number",
+    "author": "Carl Aris Areglado",
+    "depends": ["account", "sale"],
+    "data": [
+        "views/account_move_view.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
