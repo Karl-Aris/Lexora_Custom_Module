1937430b4155dbe55fad2b7c0a0eb67920659e84
Karl-Aris <areglado291995@gmail.com>
2025-07-24 08:10:54 -0400

    Update mail_compose_message.py

--- mail_composer_cc_bcc/models/mail_compose_message.py
+++ mail_composer_cc_bcc/models/mail_compose_message.py
@@ -16,23 +16,21 @@ class MailComposeMessage(models.TransientModel):
 
     @api.model
     def default_get(self, fields_list):
-        """Ensure context flag is set so mail_mail logic triggers."""
         res = super().default_get(fields_list)
         self = self.with_context(is_from_composer=True)
         return res
 
     def get_mail_values(self, res_ids):
-        """Inject CC and BCC partners into the mail values passed to mail.mail"""
         values = super().get_mail_values(res_ids)
 
         for res_id in res_ids:
             val = values[res_id]
 
-            # Add CC and BCC recipient partners
+            # Attach CC and BCC partner IDs
             val['recipient_cc_ids'] = [(6, 0, self.recipient_cc_ids.ids)]
             val['recipient_bcc_ids'] = [(6, 0, self.recipient_bcc_ids.ids)]
 
-            # Also ensure context flag is set (optional but safe)
+            # Ensure context flag is passed
             val['context'] = dict(self._context, is_from_composer=True)
 
         return values
