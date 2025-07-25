0761513481ba661dfc62eaeb18406c4fed455077
Karl-Aris <areglado291995@gmail.com>
2025-07-25 14:26:10 -0400

    Update sale_order.py

--- sale_order_vendor_bills_fixed/models/sale_order.py
+++ sale_order_vendor_bills_fixed/models/sale_order.py
@@ -1,12 +1,19 @@
 from odoo import models, fields
 
 class SaleOrder(models.Model):
-    _inherit = "sale.order"
+    _inherit = 'sale.order'
 
-    vendor_bill_ids = fields.One2many('account.move', 'sale_order_id', string='Vendor Bills',
-                                      domain=[('move_type', '=', 'in_invoice')])
+    vendor_bill_ids = fields.One2many(
+        'account.move', 'sale_origin_ref_id',
+        string='Vendor Bills',
+        compute='_compute_vendor_bill_ids',
+        store=False,
+    )
 
-class AccountMove(models.Model):
-    _inherit = "account.move"
-
-    sale_order_id = fields.Many2one('sale.order', string='Sales Order')
+    def _compute_vendor_bill_ids(self):
+        for order in self:
+            vendor_bills = self.env['account.move'].search([
+                ('move_type', '=', 'in_invoice'),
+                ('invoice_origin', '=', order.name),
+            ])
+            order.vendor_bill_ids = vendor_bills
