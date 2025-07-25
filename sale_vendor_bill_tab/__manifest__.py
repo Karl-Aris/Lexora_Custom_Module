66528e0be2dde173358ab1c34d453e1137d2d8f3
Karl-Aris <areglado291995@gmail.com>
2025-07-25 09:20:08 -0400

    Add files via upload

---
+++ sale_vendor_bill_tab/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Sale Order Vendor Bill Tab",
+    "version": "1.0",
+    "depends": ["sale", "account"],
+    "author": "ChatGPT",
+    "category": "Sales",
+    "description": "Adds a Vendor Bills tab to the Sale Order form linking account.move",
+    "data": [
+        "views/sale_order_view.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ sale_vendor_bill_tab/models/sale_order.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    vendor_bill_ids = fields.One2many(
+        'account.move', 'x_sale_order_id', string='Vendor Bills',
+        domain=[('move_type', '=', 'in_invoice')]
+    )
