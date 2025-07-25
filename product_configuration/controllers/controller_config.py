a5d76a850b577b121afd9e323810242bacb49c93
aljongarde1197 <aljon.garde1197@gmail.com>
2025-07-25 23:56:18 +0800

    Update controller_config.py

--- product_configuration/controllers/controller_config.py
+++ product_configuration/controllers/controller_config.py
@@ -1,23 +1,24 @@
-# controllers/main.py
-
 from odoo import http
 from odoo.http import request
 from collections import defaultdict
 
 class ProductKitsController(http.Controller):
 
-    @http.route(['/kits/<string:collection>'], type='http', auth='public', website=True)
-    def kits_by_collection(self, collection, **kwargs):
-        # Fetch product.kits records matching collection
+    @http.route('/store', type='http', auth='public', website=True)
+    def store_by_collection(self, **kwargs):
+        collection = kwargs.get('collection')
+        if not collection:
+            return request.not_found()
+
+        # Fetch kits by collection
         kits = request.env['product.kits'].sudo().search([('collection', '=', collection)])
 
-        # Group by size
+        # Group kits by size
         grouped_kits = defaultdict(list)
         for kit in kits:
             grouped_kits[kit.size].append(kit)
 
-        # Send to template
-        return request.render('your_module.kits_template', {
+        return request.render('product_configuration.template_product_configuration', {
             'collection': collection,
             'grouped_kits': grouped_kits,
         })
