f4a23ca3e7a2794f7ef5e5ba9c647db5d0af3ccb
Karl-Aris <areglado291995@gmail.com>
2025-07-25 14:28:59 -0400

    Update sale_order.py

--- sale_order_vendor_bills_fixed/models/sale_order.py
+++ sale_order_vendor_bills_fixed/models/sale_order.py
@@ -1,19 +1,19 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     vendor_bill_ids = fields.One2many(
-        'account.move', 'sale_origin_ref_id',
-        string='Vendor Bills',
+        comodel_name='account.move',
         compute='_compute_vendor_bill_ids',
+        string='Vendor Bills',
         store=False,
     )
 
+    @api.depends('name')
     def _compute_vendor_bill_ids(self):
         for order in self:
-            vendor_bills = self.env['account.move'].search([
+            order.vendor_bill_ids = self.env['account.move'].search([
                 ('move_type', '=', 'in_invoice'),
                 ('invoice_origin', '=', order.name),
             ])
-            order.vendor_bill_ids = vendor_bills
