340a9881b5220f3ff36ab2526522128b3874aa7c
Karl-Aris <areglado291995@gmail.com>
2025-07-25 10:03:43 -0400

    Update sale_order.py

--- sale_vendor_bill_tab/models/sale_order.py
+++ sale_vendor_bill_tab/models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -9,3 +9,18 @@ class SaleOrder(models.Model):
         string='Vendor Bills',
         domain=[('move_type', '=', 'in_invoice')]
     )
+
+    def action_create_vendor_bill(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Create Vendor Bill'),
+            'res_model': 'account.move',
+            'view_mode': 'form',
+            'target': 'current',
+            'context': {
+                'default_move_type': 'in_invoice',
+                'default_x_sale_order_id': self.id,
+                'default_company_id': self.company_id.id,
+            }
+        }
