5da2c22b7baa2f094037a2cb223f3d5a59ddd0ca
Karl-Aris <areglado291995@gmail.com>
2025-07-25 14:19:22 -0400

    Add files via upload

---
+++ sale_order_vendor_bills/models/account_move.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    sale_order_id = fields.Many2one(
+        'sale.order',
+        string='Related Sale Order'
+    )
