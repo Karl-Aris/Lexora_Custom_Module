f72dda66b0209005f24840d5a2004e3cbf1f029e
Karl-Aris <areglado291995@gmail.com>
2025-07-25 16:39:02 -0400

    Update sale_order.py

--- sale_vendor_bill_link/models/sale_order.py
+++ sale_vendor_bill_link/models/sale_order.py
@@ -9,3 +9,16 @@ class SaleOrder(models.Model):
         string='Linked Vendor Bills',
         domain=[('move_type', '=', 'in_invoice')]
     )
+
+    def action_view_vendor_bills(self):
+        return {
+            'name': 'Vendor Bills',
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+            'view_mode': 'tree,form',
+            'domain': [('id', 'in', self.vendor_bill_ids.ids)],
+            'context': {
+                'default_sale_order_id': self.id,
+                'default_move_type': 'in_invoice',
+            }
+        }
