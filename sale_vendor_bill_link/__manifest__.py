8221fe76d6565a4adf43d8cb012a765ebe340667
Karl-Aris <areglado291995@gmail.com>
2025-07-25 13:25:29 -0400

    Update __manifest__.py

--- sale_vendor_bill_link/__manifest__.py
+++ sale_vendor_bill_link/__manifest__.py
@@ -5,9 +5,9 @@
     'author': 'ChatGPT',
     'depends': ['sale_management', 'account'],
     'data': [
-        'views/sale_order_view.xml',
+        'views/sale_order_view.xml'
     ],
     'installable': True,
     'application': True,
-    'auto_install': False,
+    'auto_install': False
 }
