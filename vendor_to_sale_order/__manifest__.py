7c9b8835705563a6dc4e626fd4db46f8ebc9324e
Karl-Aris <areglado291995@gmail.com>
2025-07-28 13:23:43 -0400

    Update __manifest__.py

--- vendor_to_sale_order/__manifest__.py
+++ vendor_to_sale_order/__manifest__.py
@@ -6,7 +6,6 @@
         'security/ir.model.access.csv',
         'views/sale_order_view.xml',
         'views/account_move_view.xml',
-        'views/vendor_bill_wizard_view.xml',
     ],
     'installable': True,
     'application': False,
