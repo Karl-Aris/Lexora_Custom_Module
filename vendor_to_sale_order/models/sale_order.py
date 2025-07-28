864e5d9f5f8ba458a3c2308fea5fb3a0f00891e6
Karl-Aris <areglado291995@gmail.com>
2025-07-28 13:24:22 -0400

    Update sale_order.py

--- vendor_to_sale_order/models/sale_order.py
+++ vendor_to_sale_order/models/sale_order.py
@@ -1,18 +1,23 @@
-from odoo import models
+from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def action_create_vendor_bill(self):
         self.ensure_one()
+        bill = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'invoice_origin': self.name,
+            'invoice_date': fields.Date.context_today(self),
+            'ref': self.client_order_ref,
+            'sale_order_id': self.id,
+            'x_po_vb_id': self.purchase_order,  # Auto-fill PO#
+        })
         return {
-            'name': 'Create Vendor Bill',
+            'name': 'Vendor Bill',
             'type': 'ir.actions.act_window',
-            'res_model': 'vendor.bill.wizard',
+            'res_model': 'account.move',
+            'res_id': bill.id,
             'view_mode': 'form',
-            'target': 'new',
-            'context': {
-                'default_sale_order_id': self.id,
-                'default_x_po_vb_id': self.purchase_order,
-            }
+            'target': 'new',  # <-- This makes it a mini popup
         }
