be2f2e15dcc274506d4c4fcc0675d148bf056468
Karl-Aris <areglado291995@gmail.com>
2025-07-28 10:55:41 -0400

    Update sale_order.py

--- sale_order_vendor_bill_link/models/sale_order.py
+++ sale_order_vendor_bill_link/models/sale_order.py
@@ -19,15 +19,16 @@ class SaleOrder(models.Model):
     def _compute_vendor_bill_count(self):
         for order in self:
             order.vendor_bill_count = len(order.vendor_bill_ids)
-
-    def action_open_vendor_bills(self):
+    
+    def action_view_vendor_bills(self):
         self.ensure_one()
-        return {
-            'name': 'Vendor Bills',
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move',
-            'view_mode': 'tree,form',
-            'domain': [('id', 'in', self.vendor_bill_ids.ids)],
-            'context': {'default_move_type': 'in_invoice'},
-        }
-
+        vendor_bills = self.vendor_bill_ids
+        action = self.env.ref('account.action_move_in_invoice_type').read()[0]
+    
+        if len(vendor_bills) == 1:
+            action['views'] = [(self.env.ref('account.view_move_form').id, 'form')]
+            action['res_id'] = vendor_bills.id
+        else:
+            action['domain'] = [('id', 'in', vendor_bills.ids)]
+            action['views'] = [(False, 'list'), (self.env.ref('account.view_move_form').id, 'form')]
+        return action
