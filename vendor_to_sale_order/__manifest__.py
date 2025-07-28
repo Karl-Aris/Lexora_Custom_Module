85a82550c84dd1ad357ff2fd81c3cbf58db638cb
Karl-Aris <areglado291995@gmail.com>
2025-07-28 13:14:24 -0400

    Create __manifest__.py

---
+++ vendor_to_sale_order/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sale Order Vendor Bill Link',
+    'version': '1.0',
+    'depends': ['sale', 'account'],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/account_move_view.xml',
+        'views/vendor_bill_wizard_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
