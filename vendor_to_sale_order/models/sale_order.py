2132109e9f94ecad17c1863531dc4cdcc852153d
Karl-Aris <areglado291995@gmail.com>
2025-07-28 16:26:33 -0400

    Update sale_order.py

--- vendor_to_sale_order/models/sale_order.py
+++ vendor_to_sale_order/models/sale_order.py
@@ -3,14 +3,21 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    vendor_bill_id = fields.Many2one(
+        'account.move',
+        string="Linked Vendor Bill",
+        domain="[('move_type', '=', 'in_invoice')]"
+    )
+
     vendor_bill_count = fields.Integer(string="Vendor Bill Count", compute="_compute_vendor_bill_count")
 
     def _compute_vendor_bill_count(self):
         for order in self:
-            order.vendor_bill_count = self.env['account.move'].search_count([
+            count = self.env['account.move'].search_count([
                 ('sale_order_id', '=', order.id),
                 ('move_type', '=', 'in_invoice')
             ])
+            order.vendor_bill_count = count
 
     def action_view_vendor_bills(self):
         self.ensure_one()
@@ -21,23 +28,4 @@ class SaleOrder(models.Model):
             'view_mode': 'tree,form',
             'domain': [('sale_order_id', '=', self.id), ('move_type', '=', 'in_invoice')],
             'context': {'default_sale_order_id': self.id},
-        }
-
-    def action_create_vendor_bill(self):
-        self.ensure_one()
-        bill = self.env['account.move'].create({
-            'move_type': 'in_invoice',
-            'invoice_origin': self.name,
-            'invoice_date': fields.Date.context_today(self),
-            'ref': self.client_order_ref,
-            'sale_order_id': self.id,
-            'x_po_vb_id': self.purchase_order,
-        })
-        return {
-            'name': 'Vendor Bill',
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move',
-            'res_id': bill.id,
-            'view_mode': 'form',
-            'target': 'new',
         }
