5171709a53c96c2d01613f74a90f9e2b9b89704d
Karl-Aris <areglado291995@gmail.com>
2025-07-28 14:34:28 -0400

    Update sale_order.py

--- vendor_to_sale_order/models/sale_order.py
+++ vendor_to_sale_order/models/sale_order.py
@@ -6,15 +6,14 @@ class SaleOrder(models.Model):
     vendor_bill_count = fields.Integer(
         string="Vendor Bill Count",
         compute="_compute_vendor_bill_count",
-        store=False
     )
 
     show_create_vendor_bill = fields.Boolean(
-        compute="_compute_show_create_vendor_bill"
+        compute="_compute_vendor_bill_visibility", store=False
     )
 
     show_vendor_bill_button = fields.Boolean(
-        compute="_compute_show_vendor_bill_button"
+        compute="_compute_vendor_bill_visibility", store=False
     )
 
     def _compute_vendor_bill_count(self):
@@ -24,31 +23,31 @@ class SaleOrder(models.Model):
                 ('move_type', '=', 'in_invoice'),
             ])
 
-    @api.depends('state')
-    def _compute_show_create_vendor_bill(self):
+    def _compute_vendor_bill_visibility(self):
         for order in self:
             order.show_create_vendor_bill = order.state == 'sale'
-
-    @api.depends('vendor_bill_count')
-    def _compute_show_vendor_bill_button(self):
-        for order in self:
             order.show_vendor_bill_button = order.vendor_bill_count > 0
 
     def action_create_vendor_bill(self):
         self.ensure_one()
+
         bill = self.env['account.move'].create({
             'move_type': 'in_invoice',
             'invoice_origin': self.name,
             'invoice_date': fields.Date.context_today(self),
             'ref': self.client_order_ref,
             'sale_order_id': self.id,
-            'x_po_vb_id': self.purchase_order,
+            'x_po_vb_id': self.purchase_order,  # Only if this field exists
         })
+
+        form_view = self.env.ref('account.view_move_form')  # Correct vendor bill form
+
         return {
             'name': 'Vendor Bill',
             'type': 'ir.actions.act_window',
             'res_model': 'account.move',
             'res_id': bill.id,
             'view_mode': 'form',
+            'views': [(form_view.id, 'form')],
             'target': 'new',
         }
