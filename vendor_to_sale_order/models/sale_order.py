416a5293e08186142b94882e4e571ed938cc91e0
Karl-Aris <areglado291995@gmail.com>
2025-07-28 15:50:19 -0400

    Update sale_order.py

--- vendor_to_sale_order/models/sale_order.py
+++ vendor_to_sale_order/models/sale_order.py
@@ -5,22 +5,21 @@ class SaleOrder(models.Model):
 
     vendor_bill_count = fields.Integer(string="Vendor Bill Count", compute="_compute_vendor_bill_count")
 
-    def _compute_vendor_bill_count(self):
-        for order in self:
-            count = self.env['account.move'].search_count([
-                ('sale_order_id', '=', order.id),
-                ('move_type', '=', 'in_invoice')
-            ])
-            order.vendor_bill_count = count
-
-
-    def action_view_vendor_bills(self):
+    def action_create_vendor_bill(self):
         self.ensure_one()
+        bill = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'invoice_origin': self.name,
+            'invoice_date': fields.Date.context_today(self),
+            'ref': self.client_order_ref,
+            'sale_order_id': self.id,
+            'x_po_vb_id': self.purchase_order,  # Ensure this field exists
+        })
         return {
+            'name': 'Vendor Bill',
             'type': 'ir.actions.act_window',
-            'name': 'Vendor Bills',
             'res_model': 'account.move',
-            'view_mode': 'tree,form',
-            'domain': [('sale_order_id', '=', self.id), ('move_type', '=', 'in_invoice')],
-            'context': {'default_sale_order_id': self.id},
+            'res_id': bill.id,
+            'view_mode': 'form',
+            'target': 'new',
         }
