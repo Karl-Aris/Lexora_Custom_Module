f31362667c97da578f2b96146aa9c6c828658569
Karl-Aris <areglado291995@gmail.com>
2025-07-28 15:46:33 -0400

    Update sale_order.py

--- vendor_to_sale_order/models/sale_order.py
+++ vendor_to_sale_order/models/sale_order.py
@@ -1,24 +1,26 @@
-
-from odoo import models, fields
+from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    def action_create_vendor_bill(self):
+    vendor_bill_count = fields.Integer(string="Vendor Bill Count", compute="_compute_vendor_bill_count")
+
+    @api.depends('id')
+    def _compute_vendor_bill_count(self):
+        for order in self:
+            count = self.env['account.move'].search_count([
+                ('sale_order_id', '=', order.id),
+                ('move_type', '=', 'in_invoice')
+            ])
+            order.vendor_bill_count = count
+
+    def action_view_vendor_bills(self):
         self.ensure_one()
-        bill = self.env['account.move'].create({
-            'move_type': 'in_invoice',
-            'invoice_origin': self.name,
-            'invoice_date': fields.Date.context_today(self),
-            'ref': self.client_order_ref,
-            'sale_order_id': self.id,
-            'x_po_vb_id': self.purchase_order,  # Auto-fill PO#
-        })
         return {
-            'name': 'Vendor Bill',
             'type': 'ir.actions.act_window',
+            'name': 'Vendor Bills',
             'res_model': 'account.move',
-            'res_id': bill.id,
-            'view_mode': 'form',
-            'target': 'new',  # <-- This makes it a mini popup
+            'view_mode': 'tree,form',
+            'domain': [('sale_order_id', '=', self.id), ('move_type', '=', 'in_invoice')],
+            'context': {'default_sale_order_id': self.id},
         }
