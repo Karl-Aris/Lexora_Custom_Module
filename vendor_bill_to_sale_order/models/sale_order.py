05af7ff5715ef0673a187a900dfc332164d37593
Karl-Aris <areglado291995@gmail.com>
2025-07-28 12:21:56 -0400

    Update sale_order.py

--- vendor_bill_to_sale_order/models/sale_order.py
+++ vendor_bill_to_sale_order/models/sale_order.py
@@ -3,30 +3,23 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    x_vendor_bill_count = fields.Integer(string='Vendor Bill Count', compute='_compute_vendor_bill_count')
+    vendor_bill_ids = fields.One2many('account.move', 'sale_order_id', string="Vendor Bills", domain=[('move_type', '=', 'in_invoice')])
+    vendor_bill_count = fields.Integer(string='Vendor Bill Count', compute='_compute_vendor_bill_count')
 
+    @api.depends('vendor_bill_ids')
     def _compute_vendor_bill_count(self):
         for order in self:
-            order.x_vendor_bill_count = self.env['account.move'].search_count([
-                ('invoice_origin', '=', order.name),
-                ('move_type', '=', 'in_invoice'),
-            ])
+            order.vendor_bill_count = len(order.vendor_bill_ids)
 
-    def action_create_vendor_bill(self):
-        self.ensure_one()
-
-        ctx = {
-            'default_move_type': 'in_invoice',  # Indicates Vendor Bill
-            'default_invoice_origin': self.name,
-            'default_partner_id': self.partner_id.id,
-            'default_invoice_date': fields.Date.context_today(self),
-        }
-
+    def open_create_vendor_bill_wizard(self):
         return {
-            'name': 'Create Vendor Bill',
             'type': 'ir.actions.act_window',
-            'res_model': 'account.move',
+            'name': 'Create Vendor Bill',
+            'res_model': 'create.vendor.bill.wizard',
             'view_mode': 'form',
-            'target': 'new',  # Opens in a modal
-            'context': ctx,
+            'target': 'new',
+            'context': {
+                'default_sale_order_id': self.id,
+                'default_purchase_reference': self.client_order_ref,
+            }
         }
