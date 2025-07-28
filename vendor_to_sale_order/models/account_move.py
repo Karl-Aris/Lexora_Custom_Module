64876587c3df436069404bbf49004e33bd66ce10
Karl-Aris <areglado291995@gmail.com>
2025-07-28 13:42:12 -0400

    Update account_move.py

--- vendor_to_sale_order/models/account_move.py
+++ vendor_to_sale_order/models/account_move.py
@@ -4,4 +4,3 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     sale_order_id = fields.Many2one('sale.order', string="Related Sale Order")
-    x_po_vb_id = fields.Char(string="PO# from Sale Order")
