ec3a4f04dc8d33785572079f33fd71611cc66615
Karl-Aris <areglado291995@gmail.com>
2025-07-28 16:54:10 -0400

    Add files via upload

---
+++ vendor_to_sale_order1/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Vendor to Sale Order",
+    "version": "1.0",
+    "depends": ["sale", "account"],
+    "author": "Custom",
+    "category": "Sales",
+    "description": "Add a button to create vendor bills from sale orders.",
+    "data": [
+        "views/sale_order_vendor_bill_button.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
