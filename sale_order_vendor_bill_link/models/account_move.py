dc078c4d02079ea084dc4f0771a49498e8748a89
Karl-Aris <areglado291995@gmail.com>
2025-07-28 11:09:29 -0400

    Update account_move.py

--- sale_order_vendor_bill_link/models/account_move.py
+++ sale_order_vendor_bill_link/models/account_move.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    sale_order_id = fields.Many2one('sale.order', string="Related Sale Order")
+    sale_order_id = fields.Many2one('sale.order', string="Sale Order")
