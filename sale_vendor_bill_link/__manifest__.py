ce953506f7981c59827fb400956f54b5b265af85
Karl-Aris <areglado291995@gmail.com>
2025-07-29 08:21:38 -0400

    Create __manifest__.py

---
+++ sale_vendor_bill_link/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sale Vendor Bill Link",
+    "version": "1.0",
+    "summary": "Link Vendor Bills to Sale Orders",
+    "category": "Sales",
+    "depends": ["sale_management", "account"],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/account_move_view.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "license": "OEEL-1",
+}
