e90dc5b868b92f670edaa2885336a3af60605ae2
Karl-Aris <areglado291995@gmail.com>
2025-07-31 09:52:13 -0400

    Update account_move.py

--- vendor_to_sale_order/models/account_move.py
+++ vendor_to_sale_order/models/account_move.py
@@ -1,11 +1,9 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     sale_order_id = fields.Many2one('sale.order', string="Related Sale Order")
-    x_po_vb_id = fields.Char(string="PO# from Sale Order")
-
     sale_order_count = fields.Integer(
         string='Related Sale Order Count',
         compute='_compute_sale_order_count',
@@ -20,11 +18,9 @@ class AccountMove(models.Model):
         self.ensure_one()
         if not self.sale_order_id:
             return {'type': 'ir.actions.act_window_close'}
-
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'sale.order',
             'res_id': self.sale_order_id.id,
             'view_mode': 'form',
-            'target': 'current',
         }
