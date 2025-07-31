b39d758f061bdccaf38b711d0c65edce84926362
Karl-Aris <areglado291995@gmail.com>
2025-07-31 16:58:25 -0400

    Update post_init_hook.py

--- mock_authorize_net_provider/post_init_hook.py
+++ mock_authorize_net_provider/post_init_hook.py
@@ -2,8 +2,7 @@ from odoo import api, SUPERUSER_ID
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    provider = env['payment.provider'].search([('code', '=', 'mock_authorize_net')])
-    if not provider:
+    if not env['payment.provider'].search([('code', '=', 'mock_authorize_net')]):
         env['payment.provider'].create({
             'name': 'Mock Authorize.Net',
             'code': 'mock_authorize_net',
