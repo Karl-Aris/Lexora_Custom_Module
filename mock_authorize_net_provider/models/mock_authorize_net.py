6f686db6fbbc23a258e4295fc849c2b143d6cd65
Karl-Aris <areglado291995@gmail.com>
2025-07-31 17:03:34 -0400

    Delete mock_authorize_net_provider directory

--- mock_authorize_net_provider/models/mock_authorize_net.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class PaymentProvider(models.Model):
-    _inherit = 'payment.provider'
-
-    code = fields.Selection(
-        selection_add=[('mock_authorize_net', "Mock Authorize.Net")]
-    )
