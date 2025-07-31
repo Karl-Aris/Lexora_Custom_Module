1631bcee86866e5d9e2c0e79df2330bfe506c684
Karl-Aris <areglado291995@gmail.com>
2025-07-31 17:03:59 -0400

    Add files via upload

---
+++ mock_authorize_net/post_init_hook.py
@@ -0,0 +1,13 @@
+from odoo import api, SUPERUSER_ID
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    provider = env['payment.provider'].search([('code', '=', 'mock_authorize_net')])
+    if not provider:
+        env['payment.provider'].create({
+            'name': 'Mock Authorize.Net',
+            'code': 'mock_authorize_net',
+            'state': 'enabled',
+            'is_published': True,
+            'sequence': 30,
+        })
