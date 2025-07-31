c071dd429dc7946ccfda56a5670dad80db494450
Karl-Aris <areglado291995@gmail.com>
2025-07-31 16:32:23 -0400

    Create __manifest__.py

---
+++ mock_authorize_net_provider/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Mock Authorize.Net Provider",
+    "version": "1.0",
+    "category": "Accounting/Payment Acquirers",
+    "depends": ["payment"],
+    "data": [
+        "data/mock_authorize_net.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
