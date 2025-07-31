589e45cd86b3bdd21ee10dac1292a77e36330bbc
Karl-Aris <areglado291995@gmail.com>
2025-07-31 11:18:40 -0400

    Update __manifest__.py

--- vendor_to_sale_order/__manifest__.py
+++ vendor_to_sale_order/__manifest__.py
@@ -1,13 +1,12 @@
 {
     'name': 'Sale Order Vendor Bill Link',
-    'version': '1.1',
+    'version': '1.0',
     'depends': ['sale', 'account'],
     'data': [
-        'views/account_move_view.xml',
-        'views/sale_order_view.xml',
         'views/sale_order_vendor_bill_button.xml',
+        'views/sale_order_view.xml',
+        'views/account_move_view.xml',
         'views/vendor_bill_action.xml',
-        'views/account_move_mini_form.xml',
     ],
     'installable': True,
     'application': False,
