bee1454c2712f4673fc06c141530a1539c3ec877
Karl-Aris <areglado291995@gmail.com>
2025-07-31 12:08:47 -0400

    Update __manifest__.py

--- vendor_to_sale_order/__manifest__.py
+++ vendor_to_sale_order/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0',
     'depends': ['sale', 'account'],
     'data': [
+        'views/vendor_bill_action.xml', 
         'views/sale_order_vendor_bill_button.xml',
         'views/sale_order_view.xml',
         'views/account_move_view.xml',
