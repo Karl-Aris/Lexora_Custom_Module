814f7b3a42baa73b5907441e933adbca41013eb5
Karl-Aris <areglado291995@gmail.com>
2025-07-31 16:07:02 -0400

    Add files via upload

---
+++ mock_authorize_net_provider/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Mock Authorize.Net Provider",
+    "version": "1.0",
+    "category": "Accounting",
+    "summary": "Mock Authorize.Net payment provider for testing",
+    "depends": ["payment"],
+    "data": [
+        "data/mock_authorize_net.xml"
+    ],
+    "installable": True,
+    "application": False
+}
