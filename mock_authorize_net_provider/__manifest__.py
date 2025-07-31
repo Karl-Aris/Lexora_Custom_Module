9fcf7fddd67ae9c363bc48842257f70f50b77b40
Karl-Aris <areglado291995@gmail.com>
2025-07-31 14:34:28 -0400

    Add files via upload

---
+++ mock_authorize_net_provider/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Mock Authorize.Net Provider",
+    "version": "1.0",
+    "summary": "Creates a mock Authorize.Net payment provider for testing",
+    "category": "Accounting/Payment Acquirers",
+    "author": "ChatGPT",
+    "depends": ["payment"],
+    "data": [
+        "data/mock_authorize_net.xml"
+    ],
+    "installable": True,
+    "application": False
+}
