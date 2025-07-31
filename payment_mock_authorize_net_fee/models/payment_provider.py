38b26a7b5a248a9ecca563d8be565544ad156ed5
Karl-Aris <areglado291995@gmail.com>
2025-07-31 17:33:11 -0400

    Update payment_provider.py

--- payment_mock_authorize_net_fee/models/payment_provider.py
+++ payment_mock_authorize_net_fee/models/payment_provider.py
@@ -1,9 +1,13 @@
+# payment_mock_authorize_net_fee/models/payment_provider.py
 from odoo import models
 
 class PaymentProvider(models.Model):
     _inherit = 'payment.provider'
 
     def _get_compatible_providers(self):
-        providers = super()._get_compatible_providers()
+        return super()._get_compatible_providers() + ['mock_authorize_net']
+
+    def _get_providers(self):
+        providers = super()._get_providers()
         providers.append(('mock_authorize_net', 'Mock Authorize.Net'))
         return providers
