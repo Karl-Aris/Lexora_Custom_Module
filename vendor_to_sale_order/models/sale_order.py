c2bbe03b9af9c39a6d263138f5cc437b53a89379
Karl-Aris <areglado291995@gmail.com>
2025-07-31 11:52:38 -0400

    Update sale_order.py

--- vendor_to_sale_order/models/sale_order.py
+++ vendor_to_sale_order/models/sale_order.py
@@ -1,13 +1,33 @@
-from odoo import fields, models, api
+from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    x_po_so_id = fields.Char(string="PO # (Sale Order)")
-    vendor_bill_ids = fields.One2many('account.move', 'sale_order_id', string='Vendor Bills')
-    vendor_bill_count = fields.Integer(string='Vendor Bill Count', compute='_compute_vendor_bill_count')
+    vendor_bill_count = fields.Integer(compute="_compute_vendor_bill_count", string="Vendor Bill Count")
 
-    @api.depends('vendor_bill_ids')
     def _compute_vendor_bill_count(self):
         for order in self:
-            order.vendor_bill_count = len(order.vendor_bill_ids)
+            order.vendor_bill_count = self.env['account.move'].search_count([
+                ('sale_order_id', '=', order.id),
+                ('move_type', '=', 'in_invoice')
+            ])
+
+    def action_create_vendor_bill(self):
+        self.ensure_one()
+        bill = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'invoice_origin': self.name,
+            'invoice_date': fields.Date.context_today(self),
+            'ref': self.client_order_ref,
+            'sale_order_id': self.id,
+            'x_po_vb_id': self.purchase_order,
+            'partner_id': self.partner_id.id,
+        })
+        return {
+            'name': 'Vendor Bill',
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+            'res_id': bill.id,
+            'view_mode': 'form',
+            'target': 'new',
+        }
