dd128688f9bb4f5aaa2ca4fcf1612d0e39b3d9b6
Karl-Aris <areglado291995@gmail.com>
2025-08-01 09:11:48 -0400

    Delete invoice_sale_order_date/invoice_sale_order_date directory

--- invoice_sale_order_date/invoice_sale_order_date/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Invoice Sale Order Date",
-    "version": "1.0",
-    "depends": ["account", "sale"],
-    "data": [
-        "views/report_invoice_inherit.xml"
-    ],
-    "installable": True
-}
