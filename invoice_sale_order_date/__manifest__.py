4d18714b61014febd0279ace4b64ccdf50213260
Karl-Aris <areglado291995@gmail.com>
2025-08-01 10:28:53 -0400

    Update __manifest__.py

--- invoice_sale_order_date/__manifest__.py
+++ invoice_sale_order_date/__manifest__.py
@@ -1,9 +1,10 @@
 {
-    "name": "Invoice Sale Order Date",
-    "version": "1.0",
-    "depends": ["account", "sale"],
-    "data": [
-        "views/report_invoice_inherit.xml"
-    ],
-    "installable": True
+    'name': 'Invoice Sale Order Date',
+    'version': '1.0',
+    'depends': ['account', 'sale'],
+    'author': 'Your Name or Company',
+    'category': 'Accounting',
+    'summary': 'Adds Sale Order confirmation date to invoices',
+    'installable': True,
+    'auto_install': False,
 }
