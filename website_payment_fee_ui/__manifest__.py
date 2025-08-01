1454e5668e27e7313db9b33f723e08e74a1370ea
Karl-Aris <areglado291995@gmail.com>
2025-08-01 15:41:42 -0400

    Update __manifest__.py

--- website_payment_fee_ui/__manifest__.py
+++ website_payment_fee_ui/__manifest__.py
@@ -2,19 +2,18 @@
     "name": "Website Payment Fee UI",
     "version": "1.0",
     "depends": ["website_sale", "payment"],
-    "author": "Custom",
+    "author": "Carl Custom Dev",
     "category": "Website",
-    "description": "Adds payment provider fees in checkout UI and to Sale Order",
+    "description": "Adds dynamic payment method fees during checkout.",
     "data": [
         "views/payment_provider_views.xml",
         "views/templates.xml"
     ],
     "assets": {
         "web.assets_frontend": [
-            "/website_payment_fee_ui/static/src/js/payment_fee.js"
+            "website_payment_fee_ui/static/src/js/payment_fee.js",
         ]
     },
     "installable": True,
-    "application": False,
-    "auto_install": False
+    "auto_install": False,
 }
