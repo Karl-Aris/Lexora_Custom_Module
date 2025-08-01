c6902d174e05af8ec51f1fb12dc28287c0a9a097
Karl-Aris <areglado291995@gmail.com>
2025-08-01 16:08:46 -0400

    Add files via upload

---
+++ authorize_net_fee_fixed/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Authorize.net Fee on Sale Order",
+    "version": "1.1",
+    "depends": ["sale", "payment", "payment_authorize"],
+    "author": "Carl Custom",
+    "category": "Sales",
+    "description": "Adds a 3.5% Authorize.net fee as a sale order line when the payment method is Authorize.net.",
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False
+}
