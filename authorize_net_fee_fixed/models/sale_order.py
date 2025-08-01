6021889c0600656a0f946d0de93a59a5c88ee912
Karl-Aris <areglado291995@gmail.com>
2025-08-01 16:30:58 -0400

    Delete authorize_net_fee_fixed directory

--- authorize_net_fee_fixed/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    payment_provider_id = fields.Many2one(
-        'payment.provider', string="Payment Provider", readonly=False
-    )
