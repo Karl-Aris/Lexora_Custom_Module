038fb3d2ea848f6cc4abacf2269d87911ebe635d
Karl-Aris <areglado291995@gmail.com>
2025-08-01 10:28:32 -0400

    Update account_move.py

--- invoice_sale_order_date/models/account_move.py
+++ invoice_sale_order_date/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import fields, models
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -6,13 +6,10 @@ class AccountMove(models.Model):
     sale_order_date = fields.Date(
         string='Sale Order Date',
         compute='_compute_sale_order_date',
-        store=True
+        store=True,
     )
 
-    @api.depends('invoice_origin')
     def _compute_sale_order_date(self):
         for move in self:
-            sale_order = self.env['sale.order'].search([
-                ('name', '=', move.invoice_origin)
-            ], limit=1)
-            move.sale_order_date = sale_order.date_order if sale_order else False
+            sale_orders = move.invoice_origin and self.env['sale.order'].search([('name', '=', move.invoice_origin)], limit=1)
+            move.sale_order_date = sale_orders.confirmation_date if sale_orders else False
