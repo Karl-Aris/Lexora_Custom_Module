1f7e67ca2a2da0fb2704ec34b36b177ecb28117c
Karl-Aris <areglado291995@gmail.com>
2025-08-01 15:24:00 -0400

    Update main.py

--- website_payment_fee/controllers/main.py
+++ website_payment_fee/controllers/main.py
@@ -1,17 +1,17 @@
 from odoo import http
 from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
-class WebsitePaymentFeeController(http.Controller):
-
-    @http.route(['/shop/payment'], type='http', auth="public", website=True, sitemap=False)
+class WebsitePaymentFee(WebsiteSale):
+    @http.route(['/shop/payment'], type='http', auth="public", website=True)
     def shop_payment(self, **post):
-        order = request.website.sale_get_order()
-        if not order:
-            return request.redirect("/shop/cart")
+        """Intercept payment method selection and tag the sale.order"""
+        response = super().shop_payment(**post)
 
-        provider_id = post.get('provider_id')
-        if provider_id:
-            provider = request.env['payment.provider'].sudo().browse(int(provider_id))
-            if provider.code == 'authorize':
-                order._add_payment_fee_line()
-        return request.redirect("/shop/payment")
+        order = request.website.sale_get_order()
+        provider = post.get('provider')
+
+        if order and provider:
+            order.sudo().write({'x_payment_provider_code': provider})
+
+        return response
