a6637961caebd49dfe951d6802778e2a426877d7
Karl-Aris <areglado291995@gmail.com>
2025-08-01 15:39:20 -0400

    Update main.py

--- website_payment_fee_ui/controllers/main.py
+++ website_payment_fee_ui/controllers/main.py
@@ -1,11 +1,22 @@
-from odoo import http
+# controllers/main.py
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.http import request
+from odoo import http
 
-class PaymentFeeController(http.Controller):
-    @http.route('/get_payment_fee', type='json', auth='public', website=True)
-    def get_payment_fee(self, provider_id):
-        provider = request.env['payment.provider'].sudo().browse(int(provider_id))
+class WebsiteSaleWithFee(WebsiteSale):
+
+    @http.route(['/shop/payment'], type='http', auth="public", website=True, sitemap=False)
+    def payment(self, **post):
+        response = super().payment(**post)
         order = request.website.sale_get_order()
-        amount = order.amount_total if order else 0
-        fee = amount * (provider.fee_percent / 100.0)
-        return {"fee": fee}
+        if not order:
+            return response
+
+        provider_id = int(post.get('payment_option_id', 0))
+        provider = request.env['payment.provider'].sudo().browse(provider_id)
+
+        if provider and provider.fee_percentage and provider.fee_product_id:
+            order._remove_existing_fee_line(provider.fee_product_id)
+            order._add_payment_fee_line(provider.fee_product_id, provider.fee_percentage)
+
+        return response
