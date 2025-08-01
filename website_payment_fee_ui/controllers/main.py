882270e12c03e6d0618896c1bc4bf0c3bf355366
Karl-Aris <areglado291995@gmail.com>
2025-08-01 15:42:01 -0400

    Update main.py

--- website_payment_fee_ui/controllers/main.py
+++ website_payment_fee_ui/controllers/main.py
@@ -1,22 +1,15 @@
-# controllers/main.py
-from odoo.addons.website_sale.controllers.main import WebsiteSale
-from odoo.http import request
 from odoo import http
+from odoo.http import request
 
-class WebsiteSaleWithFee(WebsiteSale):
+class WebsitePaymentFee(http.Controller):
 
-    @http.route(['/shop/payment'], type='http', auth="public", website=True, sitemap=False)
+    @http.route(['/shop/payment'], type='http', auth='public', website=True, sitemap=False)
     def payment(self, **post):
-        response = super().payment(**post)
         order = request.website.sale_get_order()
-        if not order:
-            return response
+        provider_code = post.get('provider')
 
-        provider_id = int(post.get('payment_option_id', 0))
-        provider = request.env['payment.provider'].sudo().browse(provider_id)
+        if order and provider_code:
+            order = order.sudo()
+            order.add_payment_fee_line(provider_code)
 
-        if provider and provider.fee_percentage and provider.fee_product_id:
-            order._remove_existing_fee_line(provider.fee_product_id)
-            order._add_payment_fee_line(provider.fee_product_id, provider.fee_percentage)
-
-        return response
+        return request.redirect('/shop/payment')
