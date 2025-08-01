f057099816994b284499a857ba6ffbbafdd67e4d
Karl-Aris <areglado291995@gmail.com>
2025-08-01 15:09:38 -0400

    Update payment_provider.py

--- website_payment_fee/models/payment_provider.py
+++ website_payment_fee/models/payment_provider.py
@@ -1,7 +1,41 @@
-from odoo import models, fields
+from odoo import api, fields, models
 
-class PaymentProvider(models.Model):
-    _inherit = 'payment.provider'
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
 
-    fixed_fee = fields.Monetary("Fixed Fee", currency_field='currency_id')
-    percentage_fee = fields.Float("Percentage Fee (%)")
+    def _remove_existing_fee_line(self):
+        fee_product = self.env.ref("product.product_product_consultant", raise_if_not_found=False)
+        if not fee_product:
+            return
+        self.order_line.filtered(lambda l: l.product_id == fee_product).unlink()
+
+    def _add_authorize_net_fee_line(self):
+        fee_product = self.env.ref("product.product_product_consultant", raise_if_not_found=False)
+        if not fee_product:
+            return
+        self._remove_existing_fee_line()
+        self.order_line.create({
+            'order_id': self.id,
+            'product_id': fee_product.id,
+            'name': fee_product.name,
+            'product_uom_qty': 1,
+            'product_uom': fee_product.uom_id.id,
+            'price_unit': fee_product.list_price,
+        })
+
+    @api.model
+    def create(self, vals):
+        order = super().create(vals)
+        if order.payment_provider_id.code == "authorize":
+            order._add_authorize_net_fee_line()
+        return order
+
+    def write(self, vals):
+        res = super().write(vals)
+        if "payment_provider_id" in vals:
+            for order in self:
+                if order.payment_provider_id.code == "authorize":
+                    order._add_authorize_net_fee_line()
+                else:
+                    order._remove_existing_fee_line()
+        return res
