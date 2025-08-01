4966a35353848222163ce2eb21c8d4614dc7e75e
Karl-Aris <areglado291995@gmail.com>
2025-08-01 10:32:11 -0400

    Update __manifest__.py

--- invoice_sale_order_date/__manifest__.py
+++ invoice_sale_order_date/__manifest__.py
@@ -4,6 +4,9 @@
     'depends': ['account', 'sale'],
     'author': 'Your Name or Company',
     'category': 'Accounting',
+    'data': [
+    'views/account_move_views.xml',
+    ],
     'summary': 'Adds Sale Order confirmation date to invoices',
     'installable': True,
     'auto_install': False,
