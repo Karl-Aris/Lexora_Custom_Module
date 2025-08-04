8ca7c08804331c504b4769085548823d1d122526
Karl-Aris <areglado291995@gmail.com>
2025-08-04 08:51:20 -0400

    Update __manifest__.py

--- custom_authorize_net_fee/__manifest__.py
+++ custom_authorize_net_fee/__manifest__.py
@@ -8,14 +8,8 @@
     ],
     "data": [
         "data/fee_product.xml",
-    #],
-    #"assets": {
-    #    "web.assets_frontend": [
-    #        "custom_authorize_net_fee/static/src/js/fee_dynamic.js",
-    #    ],
-    #},
-        
+    ],
     "installable": True,
     "application": False,
-    "license": "LGPL-3",
+    "license": "LGPL-3"
 }
