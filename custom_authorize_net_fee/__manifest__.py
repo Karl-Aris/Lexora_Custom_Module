af7b21d633d7a39dbcf5740bf59943e0600c8c2c
Karl-Aris <areglado291995@gmail.com>
2025-08-04 08:29:38 -0400

    Create __manifest__.py

---
+++ custom_authorize_net_fee/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Authorize.Net Payment Fee",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "sale_management",
+        "website_sale",
+        "payment",
+    ],
+    "data": [
+        "data/fee_product.xml",
+        "views/assets.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "custom_authorize_net_fee/static/src/js/fee_dynamic.js",
+        ],
+    },
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
