ef884749a06d8a2af51a187ea077deca21e67ddd
Karl-Aris <areglado291995@gmail.com>
2025-08-04 11:27:59 -0400

    Update main.py

--- custom_authorize_net_fee/controllers/main.py
+++ custom_authorize_net_fee/controllers/main.py
@@ -16,11 +16,18 @@ class WebsiteSaleAuthorizeFee(http.Controller):
         provider_id = kwargs.get('provider_id')
         if provider_id:
             provider = request.env['payment.provider'].sudo().browse(int(provider_id))
+            if provider and provider.code == 'authorize_net':
+                _logger.info("Authorize.Net selected for order %s", order.name)
 
-            _logger.info("[TEST] Forcing fee application for any provider: %s", provider.code)
+                # Set payment_provider_id on order if missing
+                if not order.payment_provider_id:
+                    order.sudo().payment_provider_id = provider.id
+                    _logger.info("Set payment_provider_id on order %s to %s", order.name, provider.code)
 
-            if not order.authorize_fee_applied:
-                _logger.info("[TEST] Injecting fee into order %s", order.name)
-                order.apply_authorize_net_fee()
+                if not order.authorize_fee_applied:
+                    _logger.info("Injecting Authorize.Net fee into order %s", order.name)
+                    order.sudo().apply_authorize_net_fee()
+                else:
+                    _logger.info("Fee already applied to order %s", order.name)
 
         return request.redirect('/shop/payment/transaction/submit?' + http.url_encode(kwargs))
