df585c3ddeb5f33556f5b48a6d4980d7f85f374a
Karl-Aris <areglado291995@gmail.com>
2025-08-06 10:13:23 -0400

    Create __manifest__.py

---
+++ so_picking_reference/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sales Order Picking Reference',
+    'version': '1.0',
+    'summary': 'Auto-fill Picking IN/OUT from Purchase Order in Sales Order',
+    'depends': ['sale_management', 'stock', 'purchase'],
+    'author': 'Carl',
+    'category': 'Sales',
+    'data': [],
+    'installable': True,
+    'application': False,
+}
