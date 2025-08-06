48e006ea38aeddb0d6b35be85f819b4d5268f8b5
Karl-Aris <areglado291995@gmail.com>
2025-08-06 15:32:39 -0400

    Add files via upload

---
+++ so_picking_reference/stock_internal_search_filter/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Stock: Internal Transfer Search Filter",
+    "version": "1.0.0",
+    "summary": "Add 'Ready' filter to Internal Transfers search view",
+    "category": "Inventory",
+    "author": "You",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_picking_internal_search_inherit.xml",
+    ],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+}
