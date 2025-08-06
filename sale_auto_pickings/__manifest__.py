3df98bea14e3943c83b689d2fee803c970a27e8b
Karl-Aris <areglado291995@gmail.com>
2025-08-06 09:58:54 -0400

    Create __manifest__.py

---
+++ sale_auto_pickings/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Auto Tag Pickings on Sales Order",
+    "version": "1.0",
+    "depends": ["sale", "stock", "purchase"],
+    "author": "Carl Areglado",
+    "description": "Automatically tags x_picking_in and x_delivery_out based on linked Purchase Order.",
+    "category": "Sales",
+    "installable": True,
+    "auto_install": False,
+}
