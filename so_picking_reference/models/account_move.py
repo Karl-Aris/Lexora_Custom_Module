9d91eb8f883dfbd89c0520cde58c1285448fbe47
Karl-Aris <areglado291995@gmail.com>
2025-08-07 08:39:31 -0400

    Create account_move.py

---
+++ so_picking_reference/models/account_move.py
@@ -0,0 +1,27 @@
+from odoo import models, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.model
+    def create(self, vals):
+        record = super().create(vals)
+        record._update_sale_order_from_vendor_bill()
+        return record
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._update_sale_order_from_vendor_bill()
+        return res
+
+    def _update_sale_order_from_vendor_bill(self):
+        for rec in self:
+            if (
+                rec.move_type == 'in_invoice' and
+                rec.sale_order_id and
+                rec.name != '/'
+            ):
+                rec.sale_order_id.write({
+                    'x_vb_number': rec.name,
+                    'x_amount': rec.amount_total_signed,
+                })
