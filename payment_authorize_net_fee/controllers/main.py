8763fd95ce60423379850d9df615c18755031440
Karl-Aris <areglado291995@gmail.com>
2025-08-07 10:52:47 -0400

    Update main.py

--- payment_authorize_net_fee/controllers/main.py
+++ payment_authorize_net_fee/controllers/main.py
@@ -1,32 +1,13 @@
-from odoo import http
-from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.http import request
 
-class WebsiteSaleInherit(WebsiteSale):
+class WebsiteSaleAuthorizeNetFee(WebsiteSale):
 
-    @http.route(['/shop/payment/transaction'], type='http', auth="public", website=True, sitemap=False)
-    def website_payment_transaction(self, **post):
-        response = super().website_payment_transaction(**post)
+    def payment_transaction(self, **post):
         order = request.website.sale_get_order()
-        if not order:
-            return response
+        provider_code = post.get('pm_id') and request.env['payment.provider'].browse(int(post['pm_id'])).code or None
 
-        provider = order.payment_provider_id
-        if provider and provider.code == 'authorize_net':
-            fee_product = request.env['product.product'].sudo().search([('default_code', '=', 'AUTH_NET_FEE')], limit=1)
-            if fee_product:
-                # Remove existing fee lines
-                order.order_line.filtered(lambda l: l.product_id.id == fee_product.id).unlink()
+        if order and provider_code == 'authorize_net':
+            order.with_context(payment_provider_code=provider_code)._add_authorize_net_fee()
 
-                fee_percent = provider.authnet_fee_percent or 0.0
-                if fee_percent > 0:
-                    fee_amount = order.amount_total * (fee_percent / 100.0)
-                    order.order_line.create({
-                        'order_id': order.id,
-                        'product_id': fee_product.id,
-                        'name': fee_product.name,
-                        'product_uom_qty': 1,
-                        'price_unit': fee_amount,
-                    })
-
-        return response
+        return super().payment_transaction(**post)
