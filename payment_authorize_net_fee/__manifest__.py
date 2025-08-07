003435573d753a013e3d6c87a08d19777fa3cc2c
Karl-Aris <areglado291995@gmail.com>
2025-08-07 16:31:24 -0400

    Update __manifest__.py

--- payment_authorize_net_fee/__manifest__.py
+++ payment_authorize_net_fee/__manifest__.py
@@ -6,6 +6,6 @@
     "summary": "Adds 3.5% surcharge when Authorize.Net is selected",
     "installable": True,
     "data": [
-    "views/portal_templates.xml",
+    "authorize_net_portal_note.xml",
     ],
 }
