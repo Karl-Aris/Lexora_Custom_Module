67e34e3f9465088a373a3a391fc928f71101c340
Karl-Aris <areglado291995@gmail.com>
2025-08-07 12:02:04 -0400

    Update __manifest__.py

--- payment_authorize_net_fee/__manifest__.py
+++ payment_authorize_net_fee/__manifest__.py
@@ -3,6 +3,9 @@
     "version": "17.0.1.0.0",
     "depends": ["payment", "sale", "website_sale"],
     "category": "Website",
+    "data": [
+    "views/payment_pending_template.xml",
+    ],
     "summary": "Adds 3.5% surcharge when Authorize.Net is selected",
     "installable": True
 }
