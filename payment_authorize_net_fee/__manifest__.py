b76dbeaefae33b93ecd8bfea7e05ec95b7b0c862
Karl-Aris <areglado291995@gmail.com>
2025-08-07 11:10:10 -0400

    Update __manifest__.py

--- payment_authorize_net_fee/__manifest__.py
+++ payment_authorize_net_fee/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Authorize.Net Payment Fee for Website',
+    'name': 'Authorize.Net Payment Fee',
     'version': '17.0.1.0.0',
-    'depends': ['website_sale', 'payment_authorize_net'],
+    'depends': ['sale', 'website_sale', 'payment'],
     'author': 'Your Company',
     'category': 'Website',
     'data': ['data/auth_net_fee_product.xml'],
-    'summary': 'Adds a surcharge when Authorize.Net is selected as payment method on the website',
+    'summary': 'Add a 3.5% surcharge when Authorize.Net is selected',
     'installable': True,
 }
