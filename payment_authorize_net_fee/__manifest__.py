6d9cc3d300ace67d4d5eff84a67a6376dfc4ae72
Karl-Aris <areglado291995@gmail.com>
2025-08-07 11:18:12 -0400

    Add files via upload

---
+++ payment_authorize_net_fee/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Authorize.Net Payment Fee",
+    "version": "17.0.1.0.0",
+    "depends": ["payment", "sale", "website_sale"],
+    "category": "Website",
+    "summary": "Adds 3.5% surcharge when Authorize.Net is selected",
+    "installable": True
+}
