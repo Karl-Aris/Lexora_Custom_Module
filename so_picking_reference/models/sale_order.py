327ce6e25dc55fcd054369879602600c98daf88d
Karl-Aris <areglado291995@gmail.com>
2025-08-07 08:28:46 -0400

    Update sale_order.py

--- so_picking_reference/models/sale_order.py
+++ so_picking_reference/models/sale_order.py
@@ -7,11 +7,13 @@ class SaleOrder(models.Model):
     def create(self, vals):
         record = super().create(vals)
         record._update_pickings_fast()
+        record._match_invoice_number()
         return record
 
     def write(self, vals):
         res = super().write(vals)
         self._update_pickings_fast()
+        self._match_invoice_number()
         return res
 
     def _update_pickings_fast(self):
@@ -19,10 +21,10 @@ class SaleOrder(models.Model):
         for rec in self:
             if not rec.purchase_order:
                 continue
-    
+
             vals = {}
             domain_base = [('purchase_order', '=', rec.purchase_order)]
-    
+
             if not rec.x_picking_in:
                 picking_in = Picking.search(
                     domain_base + [('name', '=like', 'WH/PICK%')],
@@ -30,7 +32,7 @@ class SaleOrder(models.Model):
                 )
                 if picking_in:
                     vals['x_picking_in'] = picking_in.name
-    
+
             if not rec.x_delivery_out:
                 picking_out = Picking.search(
                     domain_base + [('name', '=like', 'WH/OUT%')],
@@ -38,6 +40,16 @@ class SaleOrder(models.Model):
                 )
                 if picking_out:
                     vals['x_delivery_out'] = picking_out.name
-    
+
             if vals:
                 rec.write(vals)
+
+    def _match_invoice_number(self):
+        Move = self.env['account.move']
+        for rec in self:
+            if rec.purchase_order and not rec.x_invoice_number:
+                invoice = Move.search([
+                    ('x_po_so_id', '=', rec.purchase_order),
+                ], limit=1)
+                if invoice:
+                    rec.x_invoice_number = invoice.name
