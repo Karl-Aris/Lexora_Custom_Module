f12e2befff4371e4ab0db0a5352ee49dd2b37607
Karl-Aris <areglado291995@gmail.com>
2025-08-08 17:03:09 -0400

    Update payment_transaction.py

--- payment_authorize_net_fee/models/payment_transaction.py
+++ payment_authorize_net_fee/models/payment_transaction.py
@@ -3,15 +3,24 @@ from odoo import models, api
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
+    def _extract_ids(self, commands):
+        ids = []
+        for command in commands:
+            if isinstance(command, int):
+                ids.append(command)
+            elif isinstance(command, (list, tuple)) and len(command) == 3:
+                if command[0] == 6:
+                    ids.extend(command[2])
+                elif command[0] == 4:
+                    ids.append(command[1])
+        return ids
+
     @api.model_create_multi
     def create(self, vals_list):
-        # Before creating transaction, add surcharge lines to related SOs
         for vals in vals_list:
-            # You must adjust this depending on your data model
-            # Common way: the transaction may have reference to sale order via 'reference' or 'sale_order_ids'
-            # Here we assume sale_order_ids is populated (adjust if necessary)
             if 'sale_order_ids' in vals:
-                so_ids = vals['sale_order_ids']
+                so_commands = vals['sale_order_ids']
+                so_ids = self._extract_ids(so_commands)
                 if so_ids:
                     sale_orders = self.env['sale.order'].browse(so_ids)
                     sale_orders._add_authorize_net_fee()
