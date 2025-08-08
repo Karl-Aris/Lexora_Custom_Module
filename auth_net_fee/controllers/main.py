a421c90d462676f581c2f0135742d795f9b1fbb6
Karl-Aris <areglado291995@gmail.com>
2025-08-08 13:34:04 -0400

    Update main.py

--- auth_net_fee/controllers/main.py
+++ auth_net_fee/controllers/main.py
@@ -1,37 +1,34 @@
-from odoo import http
-from odoo.http import request
-from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo import models, api
 
-class WebsiteSaleAuthorizeNetFee(WebsiteSale):
+class WebsiteSaleAuthorizeFee(models.Model):
+    _inherit = 'website.sale'
 
-    @http.route(['/shop/payment/transaction'], type='http', auth='public', website=True, csrf=False)
-    def shop_payment_transaction(self, **kwargs):
-        """
-        Override to add Authorize.Net fee before transaction is created.
-        """
-        order = request.website.sale_get_order()
-        if order and order.payment_provider_id and order.payment_provider_id.code == 'authorize':
-            fee_product = request.env['product.product'].sudo().search([
-                ('default_code', '=', 'AUTH_NET_FEE')
-            ], limit=1)
+    @api.model
+    def _create_payment_transaction(self, order, **kwargs):
+        """Intercept before payment transaction is created."""
+        provider_id = int(kwargs.get('payment_provider_id') or 0)
+        if provider_id:
+            provider = self.env['payment.provider'].browse(provider_id)
+            if provider and provider.code == 'authorize':
+                fee_product = self.env['product.product'].search([
+                    ('default_code', '=', 'AUTH_NET_FEE')
+                ], limit=1)
+                if fee_product:
+                    # Remove old fee lines to avoid duplicates
+                    order.order_line.filtered(
+                        lambda l: l.product_id.id == fee_product.id
+                    ).unlink()
 
-            if fee_product:
-                # Remove any previous fee lines
-                order.sudo().order_line.filtered(
-                    lambda l: l.product_id.id == fee_product.id
-                ).unlink()
+                    # Calculate fee (3.5% of untaxed amount)
+                    fee = round(order.amount_untaxed * 0.035, 2)
+                    if fee > 0:
+                        self.env['sale.order.line'].create({
+                            'order_id': order.id,
+                            'product_id': fee_product.id,
+                            'name': fee_product.name,
+                            'price_unit': fee,
+                            'product_uom_qty': 1,
+                        })
 
-                # Calculate fee (3.5% of untaxed amount)
-                fee_amount = round(order.amount_untaxed * 0.035, 2)
-                if fee_amount > 0:
-                    request.env['sale.order.line'].sudo().create({
-                        'order_id': order.id,
-                        'product_id': fee_product.id,
-                        'name': fee_product.name,
-                        'price_unit': fee_amount,
-                        'product_uom_qty': 1,
-                    })
-                    order.sudo()._amount_all()
-
-        # Continue with normal payment transaction creation
-        return super(WebsiteSaleAuthorizeNetFee, self).shop_payment_transaction(**kwargs)
+        # Now proceed with normal transaction creation
+        return super()._create_payment_transaction(order, **kwargs)
