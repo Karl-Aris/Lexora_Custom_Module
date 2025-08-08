b651a1199419113e84d72829fdda6223ddc36573
Karl-Aris <areglado291995@gmail.com>
2025-08-08 08:21:11 -0400

    Delete payment_authorize_net_fee/payment_authorize_net_fee_so directory

--- payment_authorize_net_fee/payment_authorize_net_fee_so/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Authorize.Net Payment Fee',
-    'version': '1.0',
-    'summary': 'Adds a 3.5% surcharge for Authorize.Net payments',
-    'depends': ['sale_management', 'payment'],
-    'data': [],
-    'installable': True,
-    'application': False,
-}
