b11ec033714e6bbaf8526e9b7de22ae39fcd9616
Karl-Aris <areglado291995@gmail.com>
2025-08-12 14:43:05 -0400

    Update __manifest__.py

--- payment_authorize_net_fee/__manifest__.py
+++ payment_authorize_net_fee/__manifest__.py
@@ -3,10 +3,8 @@
     'version': '1.0',
     'summary': 'Add 3.5% surcharge for Authorize.Net payments',
     'category': 'Accounting',
-    'depends': ['payment', 'sale', 'account'],
-    'data': [
-        'views/assets.xml'
-    ],
+    'depends': ['payment', 'sale', 'account', 'website_sale'],
+    'data': [],
     'assets': {
         'web.assets_frontend': [
             'payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js',
