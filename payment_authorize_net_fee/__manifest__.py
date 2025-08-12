bc79dc16f27daa0e200ea507ec23d97d71f5c341
Karl-Aris <areglado291995@gmail.com>
2025-08-12 14:51:16 -0400

    Update __manifest__.py

--- payment_authorize_net_fee/__manifest__.py
+++ payment_authorize_net_fee/__manifest__.py
@@ -5,11 +5,6 @@
     'category': 'Accounting',
     'depends': ['payment', 'sale', 'account', 'website_sale'],
     'data': [],
-    'assets': {
-        'web.assets_frontend': [
-            'payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js',
-        ],
-    },
     'installable': True,
     'application': False,
 }
