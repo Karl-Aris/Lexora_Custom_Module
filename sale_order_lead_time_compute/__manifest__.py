cf70208bc11bb56e0b9514aae50f184759462549
Karl-Aris <areglado291995@gmail.com>
2025-08-12 13:56:05 -0400

    Create __manifest__.py

---
+++ sale_order_lead_time_compute/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sale Order Lead Time Compute',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Compute lead time in days between order date and commitment date',
+    'author': 'Your Name',
+    'depends': ['sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
