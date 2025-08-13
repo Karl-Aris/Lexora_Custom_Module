407a8efd2dc8ce8206c30caf8976bceb8500668d
Karl-Aris <areglado291995@gmail.com>
2025-08-13 12:53:58 -0400

    Update authorize_net_fee_notice.js

--- payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
+++ payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
@@ -3,18 +3,17 @@
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 publicWidget.registry.AuthorizeNetFeeNotice = publicWidget.Widget.extend({
-    selector: '#o_payment_form',
+    selector: '.o_payment_form',
 
     start: function () {
-        this.el.addEventListener('submit', (ev) => {
-            const selectedOption = this.el.querySelector('input[name="pm_id"]:checked');
-            if (selectedOption && selectedOption.dataset.provider === 'authorize') {
-                if (!confirm("⚠ You will be charged an additional 3.5% fee for payments made via Authorize.Net.\n\nDo you want to continue?")) {
-                    ev.preventDefault();
-                    ev.stopPropagation();
-                    return false;
+        const radios = this.el.querySelectorAll('input[name="pm_id"]');
+        radios.forEach((radio) => {
+            radio.addEventListener('change', (ev) => {
+                const selected = ev.target;
+                if (selected && selected.dataset.provider === 'authorize') {
+                    alert("⚠ You will be charged an additional 3.5% fee for payments made via Authorize.Net.");
                 }
-            }
+            });
         });
         return this._super.apply(this, arguments);
     },
