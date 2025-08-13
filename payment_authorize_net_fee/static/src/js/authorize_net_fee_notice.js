90164f94f3c258e425e3ba8b376c4631cae7f4b9
Karl-Aris <areglado291995@gmail.com>
2025-08-13 13:15:45 -0400

    Update authorize_net_fee_notice.js

--- payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
+++ payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
@@ -10,25 +10,15 @@ publicWidget.registry.AuthorizeNetFeeNotice = publicWidget.Widget.extend({
 
     init() {
         this._super(...arguments);
-        this.seenAuthorizeNetAlert = false; // Prevent multiple alerts
-        this.selectedPaymentId = null; // Track selected method
+        this.seenAuthorizeNetAlert = false;
     },
 
     _onPaymentMethodClick: function (ev) {
         const methodText = ev.currentTarget.innerText.toLowerCase();
         const isAuthorizeNet = methodText.includes("authorize.net");
 
-        // Find currently selected payment method
-        const currentSelected = document.querySelector("li[name='o_payment_option'].o_selected");
-        const currentSelectedId = currentSelected ? currentSelected.dataset.id : null;
-
-        // Show alert only if:
-        // 1) It's Authorize.Net
-        // 2) It's a new selection
-        // 3) User hasn't already seen it this session
-        if (isAuthorizeNet && currentSelectedId !== this.selectedPaymentId && !this.seenAuthorizeNetAlert) {
+        if (isAuthorizeNet && !this.seenAuthorizeNetAlert) {
             this.seenAuthorizeNetAlert = true;
-            this.selectedPaymentId = currentSelectedId;
             alert("You will be charged an additional 3.5% fee for payments made via Authorize.Net.");
         }
     },
