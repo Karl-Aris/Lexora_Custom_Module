c6b49a3ccae953c5ff4184207e66e04c3157c845
Karl-Aris <areglado291995@gmail.com>
2025-08-13 11:51:23 -0400

    Update authorize_net_fee_notice.js

--- payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
+++ payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
@@ -1,30 +1,20 @@
 /** @odoo-module **/
 
-import { patch } from "@web/core/utils/patch";
-import { PaymentForm } from "@payment/js/payment_form";
-import { Dialog } from "@web/core/dialog/dialog";
+import publicWidget from "@web/legacy/js/public/public_widget";
 
-patch(PaymentForm.prototype, "authorize_net_fee_notice", {
-    async pay(ev) {
-        if (this.paymentProviderCode === "authorize") {
-            ev.preventDefault();
-            ev.stopPropagation();
+publicWidget.registry.AuthorizeNetFeeNotice = publicWidget.Widget.extend({
+    selector: 'form[name="o_payment_form"]',
+    events: {
+        'change input[name="payment_method_id"]': '_onPaymentMethodChange',
+    },
 
-            const confirmed = await new Promise((resolve) => {
-                this.dialogService.add(Dialog, {
-                    title: "Authorize.Net Fee",
-                    body: "You will be charged an additional 3.5% fee for payments made via Authorize.Net. Continue?",
-                    buttons: [
-                        { text: "Cancel", close: true, click: () => resolve(false) },
-                        { text: "Continue", primary: true, close: true, click: () => resolve(true) },
-                    ],
-                });
-            });
-
-            if (!confirmed) {
-                return;
-            }
+    _onPaymentMethodChange: function (ev) {
+        const selected = this.$(ev.currentTarget);
+        // Adjust the data-code or value depending on your payment method DOM
+        if (selected.data('provider') === 'authorize') {
+            alert('You will be charged an additional 3.5% fee for payments made via Authorize.Net.');
         }
-        return await super.pay(ev);
     },
 });
+
+export default publicWidget.registry.AuthorizeNetFeeNotice;
