8b31d1467763060b6608cf2ae18d6922e7cce2dc
Karl-Aris <areglado291995@gmail.com>
2025-08-13 11:18:05 -0400

    Update authorize_net_fee_notice.js

--- payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
+++ payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const paymentRadios = document.querySelectorAll('input[name="payment_method"]');
+/** @odoo-module **/
 
-    paymentRadios.forEach(radio => {
-        radio.addEventListener('change', function () {
-            if (this.value === 'authorize_net') {
-                alert('You will be charged an additional 3.5% fee for payments made via Authorize.Net.');
-            }
-        });
-    });
+import publicWidget from "@web/legacy/js/public/public_widget";
+
+publicWidget.registry.AuthorizeNetFeeNotice = publicWidget.Widget.extend({
+    selector: '.o_payment_form', // This is the payment form container
+    events: {
+        'change input[name="provider"]': '_onProviderChange',
+    },
+
+    _onProviderChange: function (ev) {
+        const selectedValue = ev.currentTarget.value;
+        if (selectedValue === 'authorize') { // must match your provider_id.code
+            alert('You will be charged an additional 3.5% fee for payments made via Authorize.Net.');
+        }
+    },
 });
+
+export default publicWidget.registry.AuthorizeNetFeeNotice;
