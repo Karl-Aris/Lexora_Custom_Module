d8f2cd639d8288ddf37a2b8f5389d2f75dfe6b47
Karl-Aris <areglado291995@gmail.com>
2025-08-13 13:00:49 -0400

    Update authorize_net_fee_notice.js

--- payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
+++ payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
@@ -3,20 +3,17 @@
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 publicWidget.registry.AuthorizeNetFeeNotice = publicWidget.Widget.extend({
-    selector: 'body', // bind to body so it also works when modal is loaded dynamically
+    selector: "#o_payment_methods",
+    events: {
+        "click li[name='o_payment_option']": "_onPaymentMethodClick",
+    },
 
-    start: function () {
-        // Listen for clicks on any payment method option
-        this.el.addEventListener('click', (ev) => {
-            const target = ev.target.closest('.o_payment_option_card');
-            if (target) {
-                // Check if this is the Authorize.Net payment option
-                const providerName = target.dataset.provider;
-                if (providerName && providerName.toLowerCase().includes('authorize')) {
-                    alert("⚠ You will be charged an additional 3.5% fee for payments made via Authorize.Net.");
-                }
-            }
-        });
-        return this._super.apply(this, arguments);
+    _onPaymentMethodClick: function (ev) {
+        const methodText = ev.currentTarget.innerText.toLowerCase();
+        if (methodText.includes("authorize.net")) {
+            alert("You will be charged an additional 3.5% fee for payments made via Authorize.Net.");
+        }
     },
 });
+
+export default publicWidget.registry.AuthorizeNetFeeNotice;
