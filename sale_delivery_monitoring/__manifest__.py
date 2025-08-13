d0bbec01a9f4ae023388eb7a38da0ff4bc5f03df
Karl-Aris <areglado291995@gmail.com>
2025-08-13 15:12:32 -0400

    Create __manifest__.py

---
+++ sale_delivery_monitoring/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sales: Delivery Monitoring (EDD & Status Buckets)",
+    "version": "17.0.1.0",
+    "summary": "Track shipment EDD & delivery status on Sales Orders with daily auto-bucketing",
+    "author": "Your Company",
+    "license": "LGPL-3",
+    "depends": ["sale_management"],  # minimum: sale; using sale_management for standard views/actions
+    "data": [
+        "data/ir_cron.xml",
+        "views/sale_order_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
