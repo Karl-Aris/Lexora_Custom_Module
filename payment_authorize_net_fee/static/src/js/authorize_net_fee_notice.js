2c7c5483a8e1c9f3919265f28d391ffcd846a41c
Karl-Aris <areglado291995@gmail.com>
2025-08-13 11:46:41 -0400

    Update authorize_net_fee_notice.js

--- payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
+++ payment_authorize_net_fee/static/src/js/authorize_net_fee_notice.js
@@ -6,15 +6,14 @@ import { Dialog } from "@web/core/dialog/dialog";
 
 patch(PaymentForm.prototype, "authorize_net_fee_notice", {
     async pay(ev) {
-        const selectedProvider = this.paymentProviderId;
-        if (selectedProvider && this.paymentProviderCode === "authorize") { // <-- must match your provider code
+        if (this.paymentProviderCode === "authorize") {
             ev.preventDefault();
             ev.stopPropagation();
 
             const confirmed = await new Promise((resolve) => {
                 this.dialogService.add(Dialog, {
-                    title: "Payment Fee Notice",
-                    body: "You will be charged an additional 3.5% fee for payments made via Authorize.Net. Do you want to continue?",
+                    title: "Authorize.Net Fee",
+                    body: "You will be charged an additional 3.5% fee for payments made via Authorize.Net. Continue?",
                     buttons: [
                         { text: "Cancel", close: true, click: () => resolve(false) },
                         { text: "Continue", primary: true, close: true, click: () => resolve(true) },
