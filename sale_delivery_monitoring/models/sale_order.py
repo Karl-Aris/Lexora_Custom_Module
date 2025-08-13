bd265641a2c10600ded2ebb16c7e54bfb1f5c23a
Karl-Aris <areglado291995@gmail.com>
2025-08-13 16:27:06 -0400

    Delete sale_delivery_monitoring directory

--- sale_delivery_monitoring/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import models, fields
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    tracking_number = fields.Char(string='Tracking Number')
-    estimated_delivery_date = fields.Date(string='Estimated Delivery Date')
