cfcad95c935c81b0700a980349cc40cdb9a9c446
Karl-Aris <areglado291995@gmail.com>
2025-08-13 16:51:51 -0400

    Delete sale_order_pending_status directory

--- sale_order_pending_status/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    delivery_status = fields.Selection(
-        selection_add=[('pending', 'Pending')],
-        ondelete={'pending': 'set default'}
-    )
