c7d6e62ad7558f543ea449cc29206b02c5c206c3
Karl-Aris <areglado291995@gmail.com>
2025-08-14 16:39:42 -0400

    Create __manifest__.py

---
+++ delivery_fedex_rest/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "FedEx REST Shipping Provider",
+    "summary": "Adds a FedEx REST (OAuth2) shipping provider that supports rating, shipping, and return labels.",
+    "version": "17.0.1.0.0",
+    "category": "Inventory/Delivery",
+    "author": "Your Company",
+    "license": "LGPL-3",
+    "website": "",
+    "depends": ["delivery", "stock", "sale_management"],
+    "data": [
+        "views/delivery_carrier_views.xml",
+    ],
+    "installable": True,
+}
