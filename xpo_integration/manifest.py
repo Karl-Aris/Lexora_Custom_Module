af95d0bd22dc0e1a42fe57b0449ec77974c26a42
Karl-Aris <areglado291995@gmail.com>
2025-08-18 16:18:46 -0400

    Create manifest.py

---
+++ xpo_integration/manifest.py
@@ -0,0 +1,14 @@
+{
+    "name": "XPO Shipment Integration",
+    "version": "1.0",
+    "category": "Warehouse",
+    "summary": "Integrate XPO shipment tracking into Odoo",
+    "description": "Track XPO shipments from Odoo Delivery Orders",
+    "author": "Custom",
+    "depends": ["base", "stock"],
+    "data": [
+        "views/xpo_shipment_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
