04cc1cdced2193d3f99a071e41a0b1c8a5a3fad1
Karl-Aris <areglado291995@gmail.com>
2025-08-18 09:48:10 -0400

    Update __manifest__.py

--- sale_order_delivery_tracking/__manifest__.py
+++ sale_order_delivery_tracking/__manifest__.py
@@ -1,13 +1,19 @@
 {
     'name': 'Sale Order Delivery Tracking',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Sales',
-    'summary': 'Adds delivery tracking fields to Sales Orders under Other Information tab',
+    'summary': 'Adds delivery tracking fields and grouped filters',
     'author': 'Carl',
     'depends': ['sale'],
     'data': [
         'views/sale_order_views.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'sale_order_delivery_tracking/static/src/js/search_favorites_menu.js',
+            'sale_order_delivery_tracking/static/src/xml/search_favorites_menu.xml',
+        ],
+    },
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
