6da1bb0c988bb90f4251939a60f3c1729426ee6e
Karl-Aris <areglado291995@gmail.com>
2025-08-22 15:27:24 -0400

    Delete shipping_api_fedex-master/shipping_api_fedex-master directory

--- shipping_api_fedex-master/shipping_api_fedex-master/helpers/settings.py
+++
@@ -1,9 +0,0 @@
-from .fedex.config import FedexConfig
-
-FEDEX_CONFIG = FedexConfig(
-    'key', 'password',
-    account_number="your account number",
-    meter_number="your meter number",
-    integrator_id="your integrator ID",
-    use_test_server=True
-)
