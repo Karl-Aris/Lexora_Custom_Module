9affe9ebe34b32b74a7726ba6dbb29dd685409ce
Karl-Aris <areglado291995@gmail.com>
2025-08-25 15:50:42 -0400

    Create __manifest__.py

---
+++ delivery_fedex_rest/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "FedEx (REST) Carrier",
+    "summary": "FedEx REST integration for Delivery Carrier (no meter number)",
+    "version": "17.0.1.0.0",
+    "author": "Karl Areglado",
+    "license": "LGPL-3",
+    "depends": ["delivery", "stock", "sale"],
+    "data": [
+        "views/delivery_carrier_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
