fabfb5d52942ad0c4bcea825bacc19134253d0b2
Karl-Aris <areglado291995@gmail.com>
2025-08-27 09:20:43 -0400

    Add files via upload

---
+++ sale_order_multi_po_search/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Sale Order Multi PO Search',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Enable multiple PO# search in Sale Orders',
+    'author': 'Custom',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_search.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
