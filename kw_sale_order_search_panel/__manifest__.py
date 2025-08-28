6e7fe85f395aa0a438b39ef574690b9682e6e99d
Karl-Aris <areglado291995@gmail.com>
2025-08-28 08:36:43 -0400

    Add files via upload

---
+++ kw_sale_order_search_panel/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'SO search panel',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Sales',
+    'license': 'OPL-1',
+    'version': '17.0.0.0.3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+}
