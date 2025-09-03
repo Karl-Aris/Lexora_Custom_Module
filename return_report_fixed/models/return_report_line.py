542cdfb9248d50e9f4a3ceb285a5fbb380fc68fe
Karl-Aris <areglado291995@gmail.com>
2025-09-03 09:14:02 -0400

    Update return_report_line.py

--- return_report_fixed/models/return_report_line.py
+++ return_report_fixed/models/return_report_line.py
@@ -4,12 +4,7 @@ class ReturnReportLine(models.Model):
     _name = 'return.report.line'
     _description = 'Return Report Line'
 
-    report_id = fields.Many2one(
-        'return.report',
-        string="Return Report",
-        required=True,
-        ondelete="cascade"
-    )
-    
+    report_id = fields.Many2one('return.report', string="Return Report", required=True, ondelete="cascade")
+    x_return_product = fields.Many2one('product.template', string="Return Product", required=True)
     quantity = fields.Float(string="Quantity", default=1.0)
     reason = fields.Char(string="Reason for Return")
