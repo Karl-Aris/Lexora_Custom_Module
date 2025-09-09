995a6f1fecb69f146d29ccf070692bdf717b74f4
Karl-Aris <areglado291995@gmail.com>
2025-09-09 16:47:36 -0400

    Create __manifest__.py

---
+++ badge/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Returns Display',
+    'version': '1.0',
+    'category': 'Sales',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
