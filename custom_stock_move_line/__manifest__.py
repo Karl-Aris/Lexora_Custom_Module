8f59300016e078f0fe56257a4422d234c71ca1d6
Karl-Aris <areglado291995@gmail.com>
2025-09-11 12:18:34 -0400

    Update __manifest__.py

--- custom_stock_move_line/__manifest__.py
+++ custom_stock_move_line/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Custom Stock Move Line',
+    'name': 'Custom Stock Move Line Independent',
     'version': '17.0.1.0.0',
     'author': 'Carl Areglado',
     'depends': ['stock'],
     'data': [
-        'views/stock_move_line_menu.xml',
+        'views/x_stock_move_line_custom_views.xml',
     ],
     'installable': True,
     'application': False,
