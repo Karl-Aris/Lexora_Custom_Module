a8f22bd302f32f0e35be182342ac2fc3d4be6283
Karl-Aris <areglado291995@gmail.com>
2025-09-11 12:35:57 -0400

    Create manifest.py

---
+++ custom_stock_move_line/manifest.py
@@ -0,0 +1,11 @@
+{
+    "name": "Custom Stock Move Line",
+    "version": "1.0",
+    "category": "Inventory",
+    "summary": "Custom Stock Move Line using _inherits",
+    "author": "Carl",
+    "depends": ["stock"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
