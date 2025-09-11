4cdcffeb883cdd051bc7f2d33a78613881ab7c4d
Karl-Aris <areglado291995@gmail.com>
2025-09-11 10:10:28 -0400

    Add files via upload

---
+++ custom_sales_move_history/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Custom Sales Move History",
+    "version": "17.0.1.0.0",
+    "depends": ["sale_management", "stock"],
+    "data": [
+        "views/sales_move_history_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
