e96b968d9119b8990218eccea5191202a8e88356
Karl-Aris <areglado291995@gmail.com>
2025-09-11 09:05:55 -0400

    Delete return_order_auto_create directory

--- return_order_auto_create/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import return_order
-from . import stock_picking
-from . import sale_order
--- return_order_auto_create/models/sale_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    return_order_ids = fields.One2many('return.order', 'sale_order_id', string='Return Orders')
