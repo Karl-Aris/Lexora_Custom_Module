870517e2ca398aab231d1be1d3930daf919f24cd
Karl-Aris <areglado291995@gmail.com>
2025-09-11 13:04:31 -0400

    Update custom_move_line.py

--- custom_stock_move_line/models/custom_move_line.py
+++ custom_stock_move_line/models/custom_move_line.py
@@ -7,14 +7,11 @@ class CustomStockMoveLine(models.Model):
 
     move_line_id = fields.Many2one('stock.move.line', required=True, ondelete='cascade', string="Stock Move Line")
 
-    # Custom fields
     x_custom_field = fields.Char(string="Custom Field")
     x_custom_note = fields.Text(string="Custom Note")
-
-    # Computed field example
     x_computed_field = fields.Float(string="Computed Field", compute='_compute_custom_value')
 
-    @api.depends('product_uom_qty', 'quantity_done')
+    @api.depends('move_line_id.product_uom_qty', 'move_line_id.quantity_done')
     def _compute_custom_value(self):
         for record in self:
-            record.x_computed_field = record.product_uom_qty - record.quantity_done
+            record.x_computed_field = record.move_line_id.product_uom_qty - record.move_line_id.quantity_done
