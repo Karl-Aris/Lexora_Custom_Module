8d231dd513fe15ca8498f418039a01700eb8d794
Karl-Aris <areglado291995@gmail.com>
2025-09-12 08:49:43 -0400

    Update __manifest__.py

--- badge/__manifest__.py
+++ badge/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    'name': 'Returns Display',
-    'version': '1.0',
-    'category': 'Sales',
-    'depends': ['sale'],
-    'data': [
-        'views/quality_check_view.xml',
+    "name": "Quality Check Condition Badge",
+    "version": "17.0.1.0.0",
+    "summary": "Show badge colors for quality check conditions",
+    "author": "Karl Areglado",
+    "depends": ["quality_control"],  # depends on Quality module
+    "data": [
+        "views/quality_check_views.xml",
     ],
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
 }
