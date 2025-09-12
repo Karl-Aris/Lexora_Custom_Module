3efc0589193034fa5232bfa8b8a16268cedf4dd3
Karl-Aris <areglado291995@gmail.com>
2025-09-12 09:04:16 -0400

    Update __manifest__.py

--- badge/__manifest__.py
+++ badge/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "17.0.1.0.0",
     "depends": ["quality_control"],
     "data": [
-        "views/quality_check_views.xml",
+        "views/quality_check_view.xml",
     ],
     "application": False,
     "installable": True,
