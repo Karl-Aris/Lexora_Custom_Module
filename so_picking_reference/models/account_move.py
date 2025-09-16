ef61b20a47a22c22b910b22299c67cc1da6d7f4b
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-17 02:13:11 +0800

    Update account_move.py

--- so_picking_reference/models/account_move.py
+++ so_picking_reference/models/account_move.py
@@ -15,6 +15,7 @@ class AccountMove(models.Model):
         return res
 
     def _update_sale_order_from_vendor_bill(self):
+        SaleOrder = self.env['sale.order']
         for rec in self:
             if rec.move_type == 'in_invoice' and rec.sale_order_id and rec.name != '/':
                 vals = {
@@ -22,7 +23,8 @@ class AccountMove(models.Model):
                     'x_amount': rec.amount_total_signed,
                     'x_bol': rec.ref,
                 }
-                # Update invoice date if available
                 if rec.invoice_date:
                     vals['x_vb_date'] = rec.invoice_date
-                rec.sale_order_id.write(vals)
+
+                # Prevent recursion: bypass SaleOrder.write override
+                super(SaleOrder, rec.sale_order_id).write(vals)
